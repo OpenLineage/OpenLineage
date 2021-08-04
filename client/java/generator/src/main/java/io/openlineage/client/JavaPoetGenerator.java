abc8695cd723f133c0e38094728ed701c083029f
Julien Le Dem <julien@apache.org>
2021-08-04 16:11:29 -0700

    remove dead code
    Signed-off-by: Julien Le Dem <julien@apache.org>

--- client/java/generator/src/main/java/io/openlineage/client/JavaPoetGenerator.java
+++ client/java/generator/src/main/java/io/openlineage/client/JavaPoetGenerator.java
@@ -331,8 +331,6 @@ public class JavaPoetGenerator {
             }
           }
           return ClassName.get(String.class);
-        } else if (primitiveType.getName().equals("integer")) {
-          return TypeName.LONG;
         }
         throw new RuntimeException("Unknown primitive: " + primitiveType.getName());
       }
