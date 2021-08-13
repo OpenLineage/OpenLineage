95f8b779cfbe1bbeb4187612558888731d5e997f
wslulciuc <willy@datakin.com>
2021-08-12 17:46:10 -0700

    Prepare for release 0.1.0-rc.3
    Signed-off-by: wslulciuc <willy@datakin.com>

--- integration/common/setup.py
+++ integration/common/setup.py
@@ -19,7 +19,7 @@ from setuptools import find_namespace_packages, setup
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-__version__ = "0.1.0rc2"
+__version__ = "0.1.0rc3"
 
 requirements = [
     "attrs>=19.3.0",
