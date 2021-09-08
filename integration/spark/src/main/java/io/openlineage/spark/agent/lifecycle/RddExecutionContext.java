b96daafc566ed999f7fcb16b070bc9c5556efca4
olek <oleksandr.dvornik@gmail.com>
2021-09-08 16:18:00 +0200

    spark3# Resolves #257 - fix tests
    Signed-off-by: olek <oleksandr.dvornik@gmail.com>

--- integration/spark/src/main/java/io/openlineage/spark/agent/lifecycle/RddExecutionContext.java
+++ integration/spark/src/main/java/io/openlineage/spark/agent/lifecycle/RddExecutionContext.java
@@ -93,7 +93,11 @@ public class RddExecutionContext implements ExecutionContext {
         Field f = getConfigField(fn);
         f.setAccessible(true);
 
-        HadoopMapRedWriteConfigUtil configUtil = (HadoopMapRedWriteConfigUtil) f.get(fn);
+        HadoopMapRedWriteConfigUtil configUtil = Optional.of(f.get(fn))
+            .filter(HadoopMapRedWriteConfigUtil.class::isInstance)
+            .map(HadoopMapRedWriteConfigUtil.class::cast)
+            .orElseThrow(() -> new NoSuchFieldException("Field is not instance of HadoopMapRedWriteConfigUtil"));
+
         Field confField = HadoopMapRedWriteConfigUtil.class.getDeclaredField("conf");
         confField.setAccessible(true);
         SerializableJobConf conf = (SerializableJobConf) confField.get(configUtil);
