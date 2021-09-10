1a80d1e7c8b24afeec09b1f42ded1fd0cd509c33
Michael Collado <mike@datakin.com>
2021-09-10 12:27:45 -0700

    Fix LogicalRelationVisitor to stop publishing unknown relations, as that's covered by the UnknownEntryFacet
    Signed-off-by: Michael Collado <mike@datakin.com>

--- integration/spark/src/test/java/io/openlineage/spark/agent/lifecycle/plan/LogicalRelationVisitorTest.java
+++ integration/spark/src/test/java/io/openlineage/spark/agent/lifecycle/plan/LogicalRelationVisitorTest.java
@@ -1,33 +1,23 @@
 package io.openlineage.spark.agent.lifecycle.plan;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.openlineage.client.OpenLineage;
 import io.openlineage.spark.agent.SparkAgentTestExtension;
 import java.net.URI;
-import java.util.Arrays;
 import java.util.List;
 import org.apache.spark.Partition;
-import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.SparkSession$;
 import org.apache.spark.sql.catalyst.expressions.AttributeReference;
 import org.apache.spark.sql.catalyst.expressions.ExprId;
-import org.apache.spark.sql.catalyst.expressions.GenericRow;
 import org.apache.spark.sql.execution.datasources.LogicalRelation;
 import org.apache.spark.sql.execution.datasources.jdbc.JDBCOptions;
 import org.apache.spark.sql.execution.datasources.jdbc.JDBCRelation;
-import org.apache.spark.sql.execution.streaming.ConsoleRelation;
-import org.apache.spark.sql.types.FloatType$;
-import org.apache.spark.sql.types.IntegerType$;
-import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StringType$;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -35,7 +25,6 @@ import org.postgresql.Driver;
 import scala.Option;
 import scala.Tuple2;
 import scala.collection.Seq$;
-import scala.collection.immutable.HashMap;
 import scala.collection.immutable.Map$;
 
 @ExtendWith(SparkAgentTestExtension.class)
@@ -95,39 +84,5 @@ class LogicalRelationVisitorTest {
     assertEquals(sparkTableName, ds.getName());
     assertEquals(URI.create(connectionUri), ds.getFacets().getDataSource().getUri());
     assertEquals(connectionUri, ds.getFacets().getDataSource().getName());
-  }
-
-  @Test
-  void testApplyUnknownRelation() {
-    SparkSession session = SparkSession.builder().master("local").getOrCreate();
-    LogicalRelationVisitor visitor =
-        new LogicalRelationVisitor(session.sparkContext(), "testnamespace");
-    org.apache.spark.sql.Dataset<Row> dataFrame =
-        session.createDataFrame(
-            Arrays.asList(new GenericRow(new Object[] {1, "hello", 5.4})),
-            new StructType(
-                new StructField[] {
-                  new StructField(
-                      "num", IntegerType$.MODULE$, false, new Metadata(new HashMap<>())),
-                  new StructField(
-                      "word", StringType$.MODULE$, false, new Metadata(new HashMap<>())),
-                  new StructField("fl", FloatType$.MODULE$, false, new Metadata(new HashMap<>()))
-                }));
-    ConsoleRelation consoleRelation = new ConsoleRelation(session.sqlContext(), dataFrame);
-    LogicalRelation logicalRelation =
-        new LogicalRelation(
-            consoleRelation, consoleRelation.schema().toAttributes(), Option.empty(), false);
-    assertTrue(visitor.isDefinedAt(logicalRelation));
-    List<OpenLineage.Dataset> datasets = visitor.apply(logicalRelation);
-    Assertions.assertThat(datasets)
-        .isNotEmpty()
-        .hasSize(1)
-        .first()
-        .hasFieldOrPropertyWithValue("name", "ConsoleRelation_struct<num:int,word:string,fl:float>")
-        .extracting("facets")
-        .extracting("documentation")
-        .extracting("description")
-        .asString()
-        .startsWith("Relation[num#6,word#7,fl#8] ConsoleRelation(");
   }
 }
