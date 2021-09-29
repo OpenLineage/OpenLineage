15414b1cf6450241b2ae7b854f6597fc035937a3
olek <oleksandr.dvornik@gmail.com>
2021-09-29 10:33:52 +0200

    spark# clean-up
    Signed-off-by: olek <oleksandr.dvornik@gmail.com>

--- integration/spark/src/test/java/io/openlineage/spark/agent/lifecycle/LogicalPlanSerializerTest.java
+++ integration/spark/src/test/java/io/openlineage/spark/agent/lifecycle/LogicalPlanSerializerTest.java
@@ -210,7 +210,6 @@ class LogicalPlanSerializerTest {
     assertThat(hadoopFSActualNode).satisfies(new MatchesMapRecursively(expectedHadoopFSNode));
   }
 
-  // TODO: (OD) reflection approach would be changed on splitting source tree task
   private CatalogTable getCatalogTable() throws InvocationTargetException, IllegalAccessException {
     Method applyMethod =
         Arrays.stream(CatalogTable.class.getDeclaredMethods())
