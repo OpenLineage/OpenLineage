5e3e0f8bc93eaeb49d175c195a325ec7e7cdd083
Maciej Obuchowski <maciej.obuchowski@getindata.com>
2021-10-20 16:11:37 +0200

    airflow: add tests that check for order of received events
    Signed-off-by: Maciej Obuchowski <maciej.obuchowski@getindata.com>

--- integration/airflow/tests/integration/test_integration.py
+++ integration/airflow/tests/integration/test_integration.py
@@ -11,6 +11,7 @@
 # limitations under the License.
 import json
 import logging
+import os
 import sys
 
 import psycopg2
@@ -60,15 +61,16 @@ def wait_for_dag(dag_id):
         raise Exception('Retry!')
 
 
-def check_matches(expected_requests, received_requests):
-    for expected in expected_requests:
+def check_matches(expected_events, actual_events) -> bool:
+    for expected in expected_events:
         is_compared = False
-        for request in received_requests:
-            if expected['eventType'] == request['eventType'] and \
-                    expected['job']['name'] == request['job']['name']:
+        for actual in actual_events:
+            # Try to find matching event by eventType and job name
+            if expected['eventType'] == actual['eventType'] and \
+                    expected['job']['name'] == actual['job']['name']:
                 is_compared = True
-                if not match(expected, request):
-                    log.info(f"failed to compare expected {expected}\nwith request {request}")
+                if not match(expected, actual):
+                    log.info(f"failed to compare expected {expected}\nwith actual {actual}")
                     return False
                 break
         if not is_compared:
@@ -78,13 +80,40 @@ def check_matches(expected_requests, received_requests):
     return True
 
 
-def check_events_emitted(expected_requests):
+def check_matches_ordered(expected_events, actual_events) -> bool:
+    for index, expected in enumerate(expected_events):
+        # Actual events have to be in the same order as expected events
+        actual = actual_events[index]
+        if expected['eventType'] == actual['eventType'] and \
+                expected['job']['name'] == actual['job']['name']:
+            if not match(expected, actual):
+                log.info(f"failed to compare expected {expected}\nwith actual {actual}")
+                return False
+            break
+        log.info(f"Wrong order of events: expected {expected['eventType']} - "
+                 f"{expected['job']['name']}\ngot {actual['eventType']} - {actual['job']['name']}")
+        return False
+    return True
+
+
+def get_events(job_name: str = None):
     time.sleep(5)
+    params = {}
+
+    if job_name:
+        params = {
+            "job_name": job_name
+        }
+
     # Service in ./server captures requests and serves them
-    r = requests.get('http://backend:5000/api/v1/lineage', timeout=5)
+    r = requests.get(
+        'http://backend:5000/api/v1/lineage',
+        params=params,
+        timeout=5
+    )
     r.raise_for_status()
     received_requests = r.json()
-    return check_matches(expected_requests, received_requests)
+    return received_requests
 
 
 def setup_db():
@@ -105,10 +134,37 @@ def test_integration(dag_id, request_path):
     wait_for_dag(dag_id)
     # (2) Read expected events
     with open(request_path, 'r') as f:
-        expected_requests = json.load(f)
+        expected_events = json.load(f)
+
+    # (3) Get actual events
+    actual_events = get_events()
 
     # (3) Verify events emitted
-    if not check_events_emitted(expected_requests):
+    if not check_matches(expected_events, actual_events):
+        log.info(f"failed to compare events!")
+        sys.exit(1)
+
+
+def test_integration_ordered(dag_id, request_dir: str):
+    log.info(f"Checking dag {dag_id}")
+    # (1) Wait for DAG to complete
+    wait_for_dag(dag_id)
+
+    # (2) Find and read events in given directory on order of file names.
+    #     The events have to arrive at the server in the same order.
+    event_files = sorted(
+        os.listdir(request_dir),
+        key=lambda x: int(x.split('.')[0])
+    )
+    expected_events = []
+    for file in event_files:
+        with open(os.path.join(request_dir, file), 'r') as f:
+            expected_events.append(json.load(f))
+
+    # (3) Get actual events with job names starting with dag_id
+    actual_events = get_events(dag_id)
+
+    if not check_matches_ordered(expected_events, actual_events):
         log.info(f"failed to compare events!")
         sys.exit(1)
 
@@ -120,3 +176,4 @@ if __name__ == '__main__':
     test_integration('bigquery_orders_popular_day_of_week', 'requests/bigquery.json')
     test_integration('dbt_dag', 'requests/dbt.json')
     test_integration('custom_extractor', 'requests/custom_extractor.json')
+    test_integration_ordered('event_order', 'requests/order')
