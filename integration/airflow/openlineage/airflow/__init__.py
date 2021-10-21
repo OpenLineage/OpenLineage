daf6312cc96e09e0a561a5c743ff19c9d4a294d5
Michael Collado <mike@datakin.com>
2021-10-21 15:45:00 -0700

    Prepare for release 0.3.0
    Signed-off-by: Michael Collado <mike@datakin.com>

--- integration/airflow/openlineage/airflow/__init__.py
+++ integration/airflow/openlineage/airflow/__init__.py
@@ -15,7 +15,7 @@ from pkg_resources import parse_version
 from airflow.version import version as AIRFLOW_VERSION
 
 __author__ = """OpenLineage"""
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 
 if parse_version(AIRFLOW_VERSION) < parse_version("2.0.0"):
     from openlineage.airflow.dag import DAG
