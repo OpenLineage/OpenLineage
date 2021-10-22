e2c386a9685e974e5ed7c31dfc7a94da4d914ba0
Michael Collado <mike@datakin.com>
2021-10-21 17:39:21 -0700

    Prepare for release 0.3.1
    Signed-off-by: Michael Collado <mike@datakin.com>

--- integration/common/setup.py
+++ integration/common/setup.py
@@ -19,7 +19,7 @@ from setuptools import find_namespace_packages, setup
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-__version__ = "0.4.0"
+__version__ = "0.3.1"
 
 requirements = [
     "attrs>=19.3.0",
