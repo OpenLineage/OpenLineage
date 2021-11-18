177d570aa253d6b7546d27e1b3ebfc6741ba3351
Michael Collado <collado.mike@gmail.com>
2021-11-18 10:35:26 -0800

    Added String only constructor to HttpError

--- integration/spark/src/main/common/java/io/openlineage/spark/agent/client/HttpError.java
+++ integration/spark/src/main/common/java/io/openlineage/spark/agent/client/HttpError.java
@@ -1,18 +1,15 @@
 package io.openlineage.spark.agent.client;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class HttpError {
   protected Integer code;
-  protected String message;
+  @NonNull protected String message;
   protected String details;
 }
