0ac5f3bfa0b42dd903e0eddbb8402309a60cdb12
OleksandrDvornik <oleksandr.dvornik@gmail.com>
2021-11-20 01:41:22 +0100

    [spark] support read/write to kafka (#387)
    * adding initial test for spark kafka integration (#279)
    
    * adding initial test for spark kafka integration
    
    Signed-off-by: tomassatka <satka.tomas@gmail.com>
    
    * [spark] Resolves: #280 kafka support
    
    Signed-off-by: olek <oleksandr.dvornik@gmail.com>
    
    * [spark] Resolves: #280 kafka support
    
    Signed-off-by: olek <oleksandr.dvornik@gmail.com>
    
    * [spark] Resolves: #280 remove debug line
    
    Signed-off-by: olek <oleksandr.dvornik@gmail.com>
    
    * [spark] Resolves: #280 javadoc
    
    Signed-off-by: olek <oleksandr.dvornik@gmail.com>
    
    * [spark] Resolves: #280 review
    
    Signed-off-by: olek <oleksandr.dvornik@gmail.com>
    
    * [spark] Resolves: #280 review
    
    Signed-off-by: olek <oleksandr.dvornik@gmail.com>
    
    * Moved KafkaWriter handling into KafkaRelationVisitor and added support for assign in kafka consumer parsing
    
    Signed-off-by: Michael Collado <mike@datakin.com>
    
    * Add check for hive classes to avoid NoClassDefFoundErrors
    
    Signed-off-by: Michael Collado <mike@datakin.com>
    
    * Fix build for kafka integration (#411)
    
    * Fix integration tests step to run on raw machine to enable docker tests
    
    Signed-off-by: Michael Collado <mike@datakin.com>
    
    * Updated kafka tests to use common version and common reference to kafka package
    
    Signed-off-by: Michael Collado <collado.mike@gmail.com>
    
    * Added reset() call to mock server to avoid results across tests
    
    Signed-off-by: Michael Collado <collado.mike@gmail.com>
    
    * Added String only constructor to HttpError
    
    Signed-off-by: Michael Collado <collado.mike@gmail.com>
    
    * Changed build to copy dependencies and use downloaded jars in Spark containers
    
    Signed-off-by: Michael Collado <collado.mike@gmail.com>
    
    * Fix spark integration test build to use explicit 3.1.2 version
    
    Signed-off-by: Michael Collado <collado.mike@gmail.com>
    
    Co-authored-by: Tomas Satka <61469727+tomassatka@users.noreply.github.com>
    Co-authored-by: Michael Collado <mike@datakin.com>
    Co-authored-by: Michael Collado <40346148+collado-mike@users.noreply.github.com>
    Signed-off-by: Will Johnson <will@willj.co>

--- integration/spark/src/main/common/java/io/openlineage/spark/agent/lifecycle/BaseVisitorFactory.java
+++ integration/spark/src/main/common/java/io/openlineage/spark/agent/lifecycle/BaseVisitorFactory.java
@@ -10,6 +10,7 @@ import io.openlineage.spark.agent.lifecycle.plan.InsertIntoDirVisitor;
 import io.openlineage.spark.agent.lifecycle.plan.InsertIntoHadoopFsRelationVisitor;
 import io.openlineage.spark.agent.lifecycle.plan.InsertIntoHiveDirVisitor;
 import io.openlineage.spark.agent.lifecycle.plan.InsertIntoHiveTableVisitor;
+import io.openlineage.spark.agent.lifecycle.plan.KafkaRelationVisitor;
 import io.openlineage.spark.agent.lifecycle.plan.LogicalRDDVisitor;
 import io.openlineage.spark.agent.lifecycle.plan.LogicalRelationVisitor;
 import io.openlineage.spark.agent.lifecycle.plan.QueryPlanVisitor;
@@ -33,6 +34,9 @@ abstract class BaseVisitorFactory implements VisitorFactory {
     list.add(new CommandPlanVisitor(new ArrayList<>(list)));
     if (BigQueryNodeVisitor.hasBigQueryClasses()) {
       list.add(new BigQueryNodeVisitor(sqlContext));
+    }
+    if (KafkaRelationVisitor.hasKafkaClasses()) {
+      list.add(new KafkaRelationVisitor());
     }
     return list;
   }
@@ -64,10 +68,12 @@ abstract class BaseVisitorFactory implements VisitorFactory {
             new SaveIntoDataSourceCommandVisitor(sqlContext, allCommonVisitors)));
     list.add(new OutputDatasetVisitor(new AppendDataVisitor(allCommonVisitors)));
     list.add(new OutputDatasetVisitor(new InsertIntoDirVisitor(sqlContext)));
-    list.add(
-        new OutputDatasetWithMetadataVisitor(
-            new InsertIntoHiveTableVisitor(sqlContext.sparkContext())));
-    list.add(new OutputDatasetVisitor(new InsertIntoHiveDirVisitor()));
+    if (InsertIntoHiveTableVisitor.hasHiveClasses()) {
+      list.add(
+          new OutputDatasetWithMetadataVisitor(
+              new InsertIntoHiveTableVisitor(sqlContext.sparkContext())));
+      list.add(new OutputDatasetVisitor(new InsertIntoHiveDirVisitor()));
+    }
     return list;
   }
 }
