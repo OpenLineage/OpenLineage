0a107a0625455db83fff9f4805bd056d6f779ce7
Michael Collado <collado.mike@gmail.com>
2021-12-20 22:57:54 -0800

    Implemented OpenLineageRunEventBuilder and defined OpenLineageEventHandlerFactory with all accompanying types
    Signed-off-by: Michael Collado <collado.mike@gmail.com>

---
+++ integration/spark/src/main/common/java/io/openlineage/spark/api/CustomFacetBuilder.java
@@ -0,0 +1,38 @@
+package io.openlineage.spark.api;
+
+import java.util.function.BiConsumer;
+
+/**
+ * Defines the base class for an event consumer that can generate custom facets from those events. A
+ * facet builder is like a partial function, which defines an {@link #isDefinedAt(Object)} method
+ * that determines whether the function is defined for a given input. If the function is defined,
+ * the {@link #build(Object, BiConsumer)} method is invoked with the input object and a {@link
+ * BiConsumer} that accepts both the name of the facet and the facet itself. A {@link
+ * CustomFacetBuilder} is itself a {@link BiConsumer} whose {@link #accept(Object, BiConsumer)}
+ * method implements the logic described.
+ *
+ * <p>The event types that can be consumed to generate facets have no common supertype, so the
+ * generic argument T isn't bounded. The types of arguments that may be found include
+ *
+ * <ul>
+ *   <li>{@link org.apache.spark.scheduler.StageInfo}
+ *   <li>{@link org.apache.spark.scheduler.Stage}
+ *   <li>{@link org.apache.spark.rdd.RDD}
+ *   <li>{@link org.apache.spark.scheduler.ActiveJob}
+ *   <li>{@link org.apache.spark.sql.execution.QueryExecution}
+ * </ul>
+ *
+ * @param <T>
+ * @param <F>
+ */
+public abstract class CustomFacetBuilder<T, F>
+    implements AbstractPartial<T>, BiConsumer<T, BiConsumer<String, ? super F>> {
+
+  public final void accept(T event, BiConsumer<String, ? super F> consumer) {
+    if (isDefinedAt(event)) {
+      build(event, consumer);
+    }
+  }
+
+  protected abstract void build(T event, BiConsumer<String, ? super F> consumer);
+}
