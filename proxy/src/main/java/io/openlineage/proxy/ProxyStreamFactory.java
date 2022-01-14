6153c0186ad09dacdb5673fcb1fb5ec686f98880
wslulciuc <willy@datakin.com>
2022-01-14 12:17:37 -0800

    Use List when registering proxy streams
    Signed-off-by: wslulciuc <willy@datakin.com>

--- proxy/src/main/java/io/openlineage/proxy/ProxyStreamFactory.java
+++ proxy/src/main/java/io/openlineage/proxy/ProxyStreamFactory.java
@@ -1,13 +1,27 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.openlineage.proxy;
 
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Sets;
+import com.google.common.collect.Lists;
 import io.openlineage.proxy.api.models.ConsoleConfig;
 import io.openlineage.proxy.api.models.ConsoleLineageStream;
 import io.openlineage.proxy.api.models.KafkaConfig;
 import io.openlineage.proxy.api.models.KafkaLineageStream;
 import io.openlineage.proxy.api.models.LineageStream;
-import java.util.Set;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -15,11 +29,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public final class ProxyStreamFactory {
   private static final String DEFAULT_PROXY_LINEAGE_SOURCE = "openLineageProxyBackend";
-  private static final Set<ProxyStreamConfig> DEFAULT_STREAMS =
-      Sets.newHashSet(new ConsoleConfig());
+  private static final List<ProxyStreamConfig> DEFAULT_STREAMS =
+      Lists.newArrayList(new ConsoleConfig());
 
   @Getter @Setter private String source = DEFAULT_PROXY_LINEAGE_SOURCE;
-  @Getter @Setter private Set<ProxyStreamConfig> streams = DEFAULT_STREAMS;
+  @Getter @Setter private List<ProxyStreamConfig> streams = DEFAULT_STREAMS;
 
   public ImmutableSet<LineageStream> build() {
     final ImmutableSet.Builder lineageStreams = ImmutableSet.builder();
