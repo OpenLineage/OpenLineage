959e5daf5d2f30b0dcfa6dd977b8a4ef7b7b8226
wslulciuc <willy@datakin.com>
2022-01-22 13:43:37 -0800

    Add javadocs to client/Utils.java
    Signed-off-by: wslulciuc <willy@datakin.com>

--- client/java/src/main/java/io/openlineage/client/Utils.java
+++ client/java/src/main/java/io/openlineage/client/Utils.java
@@ -31,11 +31,13 @@ import java.net.URL;
 import lombok.NonNull;
 import org.apache.http.client.methods.HttpRequestBase;
 
+/** Utilities class for {@link OpenLineageClient}. */
 public final class Utils {
   private Utils() {}
 
   private static final ObjectMapper MAPPER = newObjectMapper();
 
+  /** Returns a new {@link ObjectMapper} instance. */
   public static ObjectMapper newObjectMapper() {
     final ObjectMapper mapper = new ObjectMapper();
     mapper.registerModule(new Jdk8Module());
@@ -46,6 +48,7 @@ public final class Utils {
     return mapper;
   }
 
+  /** Converts the provided {@code value} to a Json {@code string}. */
   public static String toJson(@NonNull final Object value) {
     try {
       return MAPPER.writeValueAsString(value);
@@ -54,6 +57,7 @@ public final class Utils {
     }
   }
 
+  /** Converts the provided Json {@code string} to the specified {@code type}. */
   public static <T> T fromJson(@NonNull final String json, @NonNull final TypeReference<T> type) {
     try {
       return MAPPER.readValue(json, type);
@@ -62,13 +66,12 @@ public final class Utils {
     }
   }
 
+  /** Converts the provided {@code urlString} to an {@link URL} object. */
   public static URL toUrl(@NonNull final String urlString) {
     try {
-      String url = urlString;
-      if (url.endsWith("/")) {
-        url = url.substring(0, url.length() - 1);
-      }
-      return new URL(url);
+      final String urlStringWithNoTrailingSlash =
+          (urlString.endsWith("/") ? urlString.substring(0, urlString.length() - 1) : urlString);
+      return new URL(urlStringWithNoTrailingSlash);
     } catch (MalformedURLException e) {
       final AssertionError error = new AssertionError("Malformed URL: " + urlString);
       error.initCause(e);
@@ -76,6 +79,10 @@ public final class Utils {
     }
   }
 
+  /**
+   * Adds the provided {@code apiKey} as an {@code Authorization} HTTP header to the specified
+   * {@link HttpRequestBase} object.
+   */
   public static void addAuthTo(
       @NonNull final HttpRequestBase request, @NonNull final String apiKey) {
     request.addHeader(AUTHORIZATION, "Bearer " + apiKey);
