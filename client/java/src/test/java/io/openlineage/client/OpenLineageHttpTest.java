a150b5ac827e04a7e3d7946512835bb66c3f85eb
wslulciuc <willy@datakin.com>
2022-02-01 15:43:20 -0800

    continued: Add docs to configure client with static query params
    Signed-off-by: wslulciuc <willy@datakin.com>

--- client/java/src/test/java/io/openlineage/client/OpenLineageHttpTest.java
+++ client/java/src/test/java/io/openlineage/client/OpenLineageHttpTest.java
@@ -7,28 +7,26 @@ import java.net.URL;
 import java.util.Map;
 import org.apache.http.client.utils.URIBuilder;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 /** Unit tests for {@link OpenLineageHttp}. */
 public class OpenLineageHttpTest {
   @Test
   public void testHttp_overrideUrlWithQueryParams() throws Exception {
-    final URL expected =
+    final URI uri =
         new URIBuilder("http://localhost:5000/api/v1/test")
             .addParameter("param0", "value0")
             .addParameter("param1", "value1")
             .addParameter("param2", "value2")
-            .build()
-            .toURL();
+            .build();
+    final URL expected = uri.toURL();
 
     // URL with query params appended on each HTTP request
-    final URI uri =
+    final URI uriWithQueryParams =
         new URIBuilder("http://localhost:5000")
             .addParameter("param0", "value0")
             .addParameter("param1", "value1")
             .build();
-    final URL urlWithQueryParams = uri.toURL();
+    final URL urlWithQueryParams = uriWithQueryParams.toURL();
     final OpenLineageHttp http = OpenLineageHttp.create(urlWithQueryParams, null);
 
     // URL with query params combined
