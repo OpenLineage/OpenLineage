8d4725de0b1701cd7497275168eaa5140314ae28
Ziyoiddin Yusupov <ziyoiddin.yusupov@gmail.com>
2022-02-08 16:35:32 +0100

    [INTEGRATION][Flink] Prepare baseline for Flink integration which incluses local Flink/Kafka environment
    Signed-off-by: mr-yusupov <ziyoiddin.yusupov@gmail.com>

---
+++ integration/flink/examples/stateful/src/test/groovy/io/openlineage/kafka/SimpleKafkaProducer.groovy
@@ -0,0 +1,38 @@
+package io.openlineage.kafka
+
+
+import io.confluent.kafka.serializers.KafkaAvroSerializer
+import org.apache.avro.specific.SpecificRecord
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
+
+class SimpleKafkaProducer implements AutoCloseable {
+    private final KafkaProducer<String, SpecificRecord> producer
+
+    SimpleKafkaProducer(Kafka kafka) {
+        final Properties properties = new Properties()
+        properties.put('bootstrap.servers', kafka.bootstrapServers)
+        properties.put('acks', 'all')
+        properties.put('retries', 2)
+        properties.put('key.serializer', StringSerializer.class)
+        properties.put('value.serializer', KafkaAvroSerializer.class)
+        properties.put('schema.registry.url', kafka.schemaRegistryUrl)
+        properties.put('specific.avro.reader', true)
+        properties.put('auto.register.schemas', true)
+        properties.put('value.subject.name.strategy', 'io.confluent.kafka.serializers.subject.TopicNameStrategy')
+        producer = new KafkaProducer<>(properties)
+    }
+
+    @Override
+    void close() throws Exception {
+        if (producer != null) {
+            producer.close()
+        }
+    }
+
+    void produce(String topic, SpecificRecord event, String key) {
+        println "Sending to topic=$topic, key=$key, value=$event"
+        producer.send(new ProducerRecord<String, SpecificRecord>(topic, key, event)).get()
+    }
+}
