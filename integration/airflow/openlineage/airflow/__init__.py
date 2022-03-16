43b88fe1e5d08ff9dc5f3a6c553806a3017318d9
Michael Robinson <merobi@gmail.com>
2022-03-16 14:12:32 -0400

    Prepare for release 0.6.2
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- integration/airflow/openlineage/airflow/__init__.py
+++ integration/airflow/openlineage/airflow/__init__.py
@@ -5,7 +5,7 @@ from pkg_resources import parse_version
 from airflow.version import version as AIRFLOW_VERSION
 
 __author__ = """OpenLineage"""
-__version__ = "0.7.0"
+__version__ = "0.6.2"
 
 if parse_version(AIRFLOW_VERSION) < parse_version("2.0.0"):
     from openlineage.airflow.dag import DAG
