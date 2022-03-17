554c6f49f8022157f32dc25c46ca4cd1929d4d19
Maciej Obuchowski <obuchowski.maciej@gmail.com>
2022-03-17 11:12:15 +0100

    flink: extract sources/sinks from transformation tree (#591)
    * [INTEGRATION][Flink] Cascade Transaformations into Datasets
    
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>
    
    * flink: process tree to look only at list of sources and sink
    
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>
    
    Co-authored-by: mr-yusupov <ziyoiddin.yusupov@gmail.com>

--- integration/flink/src/main/java/io/openlineage/flink/OpenLineageFlinkJobListener.java
+++ integration/flink/src/main/java/io/openlineage/flink/OpenLineageFlinkJobListener.java
@@ -1,116 +1,151 @@
 package io.openlineage.flink;
 
-import org.apache.commons.lang3.reflect.FieldUtils;
-import org.apache.flink.api.common.JobExecutionResult;
-import org.apache.flink.api.dag.Transformation;
-import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
-import org.apache.flink.connector.kafka.sink.KafkaSink;
-import org.apache.flink.connector.kafka.source.KafkaSource;
-import org.apache.flink.connector.kafka.source.enumerator.subscriber.KafkaSubscriber;
-import org.apache.flink.core.execution.JobClient;
-import org.apache.flink.core.execution.JobListener;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.transformations.OneInputTransformation;
-import org.apache.flink.streaming.api.transformations.PartitionTransformation;
-import org.apache.flink.streaming.api.transformations.SinkTransformation;
-import org.apache.flink.streaming.api.transformations.SourceTransformation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.annotation.Nullable;
+import io.openlineage.client.OpenLineage;
+import io.openlineage.flink.api.OpenLineageContext;
+import io.openlineage.flink.visitor.Visitor;
+import io.openlineage.flink.visitor.VisitorFactory;
+import io.openlineage.flink.visitor.VisitorFactoryImpl;
 import java.lang.reflect.Field;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.function.Function;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import javax.annotation.Nullable;
 
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.dag.Transformation;
+import org.apache.flink.core.execution.JobClient;
+import org.apache.flink.core.execution.JobListener;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+@Slf4j
 public class OpenLineageFlinkJobListener implements JobListener {
 
-    private final StreamExecutionEnvironment executionEnvironment;
-    private static final Logger LOGGER = LoggerFactory.getLogger(OpenLineageFlinkJobListener.class);
+  private final StreamExecutionEnvironment executionEnvironment;
+  private final OpenLineageContext openLineageContext;
 
+  public OpenLineageFlinkJobListener(StreamExecutionEnvironment executionEnvironment) {
+    makeTransformationsArchivedList(executionEnvironment);
+    this.executionEnvironment = executionEnvironment;
+    this.openLineageContext =
+        OpenLineageContext.builder()
+            .streamExecutionEnvironment(Optional.of(executionEnvironment))
+            .openLineage(new OpenLineage(URI.create("")))
+            .build();
+  }
 
-    public OpenLineageFlinkJobListener(StreamExecutionEnvironment executionEnvironment) {
-        this.executionEnvironment = executionEnvironment;
-        try {
-            Field transformations = FieldUtils.getField(StreamExecutionEnvironment.class, "transformations", true);
-            ArrayList<Transformation<?>> previousTransformationList = (ArrayList<Transformation<?>>) FieldUtils.readField(transformations, executionEnvironment, true);
-            List<Transformation<?>> transformationList = new UnclearableList<>(previousTransformationList);
-            FieldUtils.writeField(transformations, executionEnvironment, transformationList, true);
-        } catch (IllegalAccessException e) {
-            LOGGER.error("Failed to rewrite transforamtions");
+  @Override
+  public void onJobSubmitted(@Nullable JobClient jobClient, @Nullable Throwable throwable) {
+    if (jobClient != null) {
+      log.debug("JobId: {}", jobClient.getJobID());
+      Field transformationsField =
+          FieldUtils.getField(StreamExecutionEnvironment.class, "transformations", true);
+      try {
+        List<Transformation<?>> transformations =
+          ((ArchivedList<Transformation<?>>) transformationsField.get(executionEnvironment)).getArchive();
+        TransformationUtils converter = new TransformationUtils();
+
+        List<SinkLineage> sinkLineages = converter.convertToVisitable(transformations);
+        VisitorFactory visitorFactory = new VisitorFactoryImpl();
+
+        for (var lineage : sinkLineages) {
+          List<OpenLineage.InputDataset> inputDatasets =
+              getInputDatasets(visitorFactory, lineage.getSources());
+          List<OpenLineage.OutputDataset> outputDatasets =
+              getOutputDatasets(visitorFactory, lineage.getSink());
+          createRunEventAndEmit(inputDatasets, outputDatasets);
         }
+
+      } catch (IllegalAccessException e) {
+        log.error("Can't access the field. ", e);
+      }
+    }
+  }
+
+  private void createRunEventAndEmit(
+      List<OpenLineage.InputDataset> inputDatasets,
+      List<OpenLineage.OutputDataset> outputDatasets) {
+    OpenLineage.RunEvent runEvent =
+        openLineageContext
+            .getOpenLineage()
+            .newRunEventBuilder()
+            .inputs(inputDatasets)
+            .outputs(outputDatasets)
+            .build();
+
+    log.debug("RunEvent: {}", runEvent);
+  }
+
+  private List<OpenLineage.InputDataset> getInputDatasets(
+      VisitorFactory visitorFactory, List<Object> sources) {
+    List<OpenLineage.InputDataset> inputDatasets = new ArrayList<>();
+    List<Visitor<OpenLineage.InputDataset>> inputVisitors =
+        visitorFactory.getInputVisitors(openLineageContext);
+
+    for (var transformation : sources) {
+      inputDatasets.addAll(
+          inputVisitors.stream()
+              .filter(inputVisitor -> inputVisitor.isDefinedAt(transformation))
+              .map(inputVisitor -> inputVisitor.apply(transformation))
+              .flatMap(List::stream)
+              .collect(Collectors.toList()));
+    }
+    return inputDatasets;
+  }
+
+  private List<OpenLineage.OutputDataset> getOutputDatasets(
+      VisitorFactory visitorFactory, Object sink) {
+    List<OpenLineage.OutputDataset> outputDatasets = new ArrayList<>();
+    List<Visitor<OpenLineage.OutputDataset>> outputVisitors =
+        visitorFactory.getOutputVisitors(openLineageContext);
+
+    outputDatasets.addAll(
+        outputVisitors.stream()
+            .filter(inputVisitor -> inputVisitor.isDefinedAt(sink))
+            .map(inputVisitor -> inputVisitor.apply(sink))
+            .flatMap(List::stream)
+            .collect(Collectors.toList()));
+    return outputDatasets;
+  }
+
+  @Override
+  public void onJobExecuted(
+      @Nullable JobExecutionResult jobExecutionResult, @Nullable Throwable throwable) {
+    log.debug("JobExecutionResult: {}", jobExecutionResult);
+  }
+
+  private void makeTransformationsArchivedList(StreamExecutionEnvironment executionEnvironment) {
+    try {
+      Field transformations =
+          FieldUtils.getField(StreamExecutionEnvironment.class, "transformations", true);
+      ArrayList<Transformation<?>> previousTransformationList =
+          (ArrayList<Transformation<?>>)
+              FieldUtils.readField(transformations, executionEnvironment, true);
+      List<Transformation<?>> transformationList =
+          new ArchivedList<>(previousTransformationList);
+      FieldUtils.writeField(transformations, executionEnvironment, transformationList, true);
+    } catch (IllegalAccessException e) {
+      log.error("Failed to rewrite transformations");
+    }
+  }
+
+  static class ArchivedList<T> extends ArrayList<T> {
+    @Getter
+    List<T> archive;
+
+    public ArchivedList(Collection<T> collection) {
+      super(collection);
     }
 
     @Override
-    public void onJobSubmitted(@Nullable JobClient jobClient, @Nullable Throwable throwable) {
-        if (jobClient != null) {
-            LOGGER.info("JobId: {}", jobClient.getJobID());
-            Field transformationsField = FieldUtils.getField(StreamExecutionEnvironment.class, "transformations", true);
-            try {
-                List<Transformation> transformations = (List<Transformation>) transformationsField.get(executionEnvironment);
-                transformations.forEach(transformation -> {
-                    if (transformation instanceof OneInputTransformation) {
-                        extractKafkaSourceTopics(transformation);
-                    } else if (transformation instanceof SinkTransformation) {
-                        extractKafkaSinkTopics(transformation);
-                    }
-                });
-            } catch (IllegalAccessException e) {
-                LOGGER.error("Can't access the field. ", e);
-            }
-        }
+    public void clear() {
+      archive = new ArrayList<>(this);
+      clear();
     }
-
-    private void extractKafkaSourceTopics(Transformation transformation) {
-        transformation.getInputs().stream()
-                .map(partitionTransformation -> ((PartitionTransformation) partitionTransformation).getInputs())
-                .forEach(inputs -> ((List<SourceTransformation>) inputs).forEach(sourceTransformation -> {
-                            KafkaSource kafkaSource = (KafkaSource) ((SourceTransformation) sourceTransformation).getSource();
-                            Field subscriberField = FieldUtils.getField(KafkaSource.class, "subscriber", true);
-                            try {
-                                KafkaSubscriber kafkaSubscriber = (KafkaSubscriber) subscriberField.get(kafkaSource);
-                                Field topicsField = FieldUtils.getField(kafkaSubscriber.getClass().asSubclass(kafkaSubscriber.getClass()), "topics", true);
-                                List<String> topics = (List<String>) topicsField.get(kafkaSubscriber);
-                                topics.forEach(topic -> LOGGER.info("Kafka input topic: {}", topic));
-                            } catch (IllegalAccessException e) {
-                                LOGGER.error("Can't access the field. ", e);
-                            }
-                        })
-                );
-    }
-
-    private void extractKafkaSinkTopics(Transformation transformation) {
-        KafkaSink kafkaSink = (KafkaSink) ((SinkTransformation)transformation).getSink();
-        Field recordSerializerField = FieldUtils.getField(KafkaSink.class, "recordSerializer", true);
-        try {
-            KafkaRecordSerializationSchema serializationSchema = (KafkaRecordSerializationSchema) recordSerializerField.get(kafkaSink);
-            Field topicSelectorField = FieldUtils.getField(serializationSchema.getClass().asSubclass(serializationSchema.getClass()), "topicSelector", true);
-            Field topicSelectorFunctionField = FieldUtils.getField(topicSelectorField.get(serializationSchema).getClass().asSubclass(topicSelectorField.get(serializationSchema).getClass()), "topicSelector", true);
-            Function function = (Function) topicSelectorFunctionField.get(topicSelectorField.get(serializationSchema));
-            LOGGER.error("Kafka output topic: {}", function.apply(null));
-        } catch (IllegalAccessException e) {
-            LOGGER.error("Can't access the field. ", e);
-        }
-    }
-
-
-
-    @Override
-    public void onJobExecuted(@Nullable JobExecutionResult jobExecutionResult, @Nullable Throwable throwable) {
-        LOGGER.info("JobExecutionResult: {}", jobExecutionResult);
-    }
-
-    static class UnclearableList<T> extends ArrayList<T> {
-
-        public UnclearableList(Collection<T> collection) {
-            super(collection);
-            LOGGER.warn("Created unclearableList");
-        }
-
-        @Override
-        public void clear() {
-            LOGGER.info("Trying to clear unclearable list");
-        }
-    }
+  }
 }
---
+++ integration/flink/src/main/java/io/openlineage/flink/SinkLineage.java
@@ -0,0 +1,15 @@
+package io.openlineage.flink;
+
+import java.util.List;
+import lombok.Value;
+
+@Value
+public class SinkLineage {
+  // Can be implementation of org.apache.flink.api.connector.source.Source
+  // or org.apache.flink.streaming.api.functions.source.SourceFunction
+  List<Object> sources;
+
+  // Can be implementation of org.apache.flink.api.connector.sink2.Sink
+  // or org.apache.flink.streaming.api.functions.sink.SinkFunction
+  Object sink;
+}
