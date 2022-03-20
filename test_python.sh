7929d2c8ff7924f3bd3d9ea3041f81d85a78b50d
Maciej Obuchowski <obuchowski.maciej@gmail.com>
2022-03-21 00:42:59 +0100

    beginning of a build script
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>

--- test_python.sh
+++ test_python.sh
@@ -6,3 +6,6 @@ pip install target/wheels/*.whl
 pip install pytest
 
 pytest -vv tests/python
+
+
+#docker run --rm -it --platform linux/arm64/v8 --entrypoint bash -v $PWD:/code $(docker build -q .)
