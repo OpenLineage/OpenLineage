3d9a2cd24de82270301169179d4630686b0e6f5d
Michael Robinson <merobi@gmail.com>
2022-04-29 18:10:38 -0400

    Prepare next development version 0.9.0-SNAPSHOT
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- integration/airflow/openlineage/airflow/__init__.py
+++ integration/airflow/openlineage/airflow/__init__.py
@@ -5,7 +5,7 @@ from pkg_resources import parse_version
 from airflow.version import version as AIRFLOW_VERSION
 
 __author__ = """OpenLineage"""
-__version__ = "0.8.1"
+__version__ = "0.9.0"
 
 if parse_version(AIRFLOW_VERSION) < parse_version("2.0.0"):
     from openlineage.airflow.dag import DAG
--- integration/common/openlineage/common/__init__.py
+++ integration/common/openlineage/common/__init__.py
@@ -1,3 +1,3 @@
 # SPDX-License-Identifier: Apache-2.0
 
-__version__ = "0.8.1"
+__version__ = "0.9.0"
