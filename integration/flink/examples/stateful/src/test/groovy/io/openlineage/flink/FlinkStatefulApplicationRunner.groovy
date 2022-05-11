2be46f15eab40ac5a19ea35c3ae516b519f36b89
pawel.leszczynski <leszczynski.pawel@gmail.com>
2022-05-11 08:24:26 +0200

    multi input topic integration test (#730)
    Signed-off-by: Pawel Leszczynski <leszczynski.pawel@gmail.com>

--- integration/flink/examples/stateful/src/test/groovy/io/openlineage/flink/FlinkStatefulApplicationRunner.groovy
+++ integration/flink/examples/stateful/src/test/groovy/io/openlineage/flink/FlinkStatefulApplicationRunner.groovy
@@ -1,7 +1,7 @@
 package io.openlineage.flink
 
 class FlinkStatefulApplicationRunner {
-    private static String[] PARAMETERS = ["--input-topic", "io.openlineage.flink.kafka.input",
+    private static String[] PARAMETERS = ["--input-topics", "io.openlineage.flink.kafka.input",
                                           "--output-topic", "io.openlineage.flink.kafka.output",
                                           "--flink.openlineage.url", "http://localhost:5000/api/v1/namespaces/flink_integration/"]
 
