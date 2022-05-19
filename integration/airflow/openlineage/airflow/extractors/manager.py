851e5b7437078b51aaeedcd4e25e098f2d2ca965
Maciej Obuchowski <obuchowski.maciej@gmail.com>
2022-05-19 17:58:01 +0200

    airflow: bug: unknown source attribute always set (#762)
    * Only set unknownSourceAttribute facet when no extractor is present.
    
    Signed-off-by: Conor Beverland <conorbev@gmail.com>
    
    * airflow: fix tests for unknown source attribute
    
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>
    
    Co-authored-by: Conor Beverland <conorbev@gmail.com>

--- integration/airflow/openlineage/airflow/extractors/manager.py
+++ integration/airflow/openlineage/airflow/extractors/manager.py
@@ -53,19 +53,24 @@ class ExtractorManager:
             self.log.warning(
                 f'Unable to find an extractor. {task_info}')
 
-        return TaskMetadata(
-            name=get_job_name(task),
-            run_facets={
-                "unknownSourceAttribute": UnknownOperatorAttributeRunFacet(
-                    unknownItems=[
-                        UnknownOperatorInstance(
-                            name=task.__class__.__name__,
-                            properties={attr: value for attr, value in task.__dict__.items()}
-                        )
-                    ]
-                )
-            }
-        )
+            # Only include the unkonwnSourceAttribute facet if there is no extractor
+            return TaskMetadata(
+                name=get_job_name(task),
+                run_facets={
+                    "unknownSourceAttribute": UnknownOperatorAttributeRunFacet(
+                        unknownItems=[
+                            UnknownOperatorInstance(
+                                name=task.__class__.__name__,
+                                properties={
+                                    attr: value for attr, value in task.__dict__.items()
+                                },
+                            )
+                        ]
+                    )
+                },
+            )
+
+        return TaskMetadata(name=get_job_name(task))
 
     def _get_extractor(self, task) -> Optional[BaseExtractor]:
         if task.task_id in self.extractors:
