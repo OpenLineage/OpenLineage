b910c126cbd1178e90b7e53a69c795d0470768b9
howardyoo <howardyoo@gmail.com>
2022-05-26 16:52:02 -0500

    fixed incorrect indentation and trailing spaces.
    Signed-off-by: howardyoo <howardyoo@gmail.com>

--- integration/airflow/openlineage/airflow/__init__.py
+++ integration/airflow/openlineage/airflow/__init__.py
@@ -9,13 +9,13 @@ __author__ = """OpenLineage"""
 __version__ = "0.9.0"
 
 if parse_version(AIRFLOW_VERSION) < parse_version("2.0.0"):
-  from openlineage.airflow.dag import DAG
-  __all__ = ["DAG"]
-  logging.warning(
-  f'''
-  OpenLineage supprot for Airflow version {AIRFLOW_VERSION}
-  is DEPRECATED, and will be desupported on September 30, 2022.
-  Please make sure to upgrade your Airflow version to minimum of 2.0.0
-  in order to continue using OpenLineage.
-  '''
-  )  
+    from openlineage.airflow.dag import DAG
+    __all__ = ["DAG"]
+    logging.warning(
+    f'''
+    OpenLineage supprot for Airflow version {AIRFLOW_VERSION}
+    is DEPRECATED, and will be desupported on September 30, 2022.
+    Please make sure to upgrade your Airflow version to minimum of 2.0.0
+    in order to continue using OpenLineage.
+    '''
+    )
