6ffa7f8475389c5bccecf531a0a936bc61881213
Howard Yoo <32691630+howardyoo@users.noreply.github.com>
2022-05-27 06:37:16 -0500

    [INTEGRATION][AIRFLOW] Warning message when using OpenLineage with Airflow v1.x (#791)
    * Added warning message in logging when using OpenLineage with Airflow v1.*,
    to notify users that the library is scheduled to be deprecating support for older versions.
    
    Signed-off-by: howardyoo <howardyoo@gmail.com>
    
    * fixed incorrect indentation and trailing spaces.
    
    Signed-off-by: howardyoo <howardyoo@gmail.com>
    
    * indentation fixes
    
    Signed-off-by: howardyoo <howardyoo@gmail.com>
    
    * fixed no newline at the end of file.
    
    Signed-off-by: howardyoo <howardyoo@gmail.com>
    Signed-off-by: Tyler Farris <TylerpFarris@gmail.com>

--- integration/airflow/openlineage/airflow/__init__.py
+++ integration/airflow/openlineage/airflow/__init__.py
@@ -3,6 +3,7 @@
 # -*- coding: utf-8 -*-
 from pkg_resources import parse_version
 from airflow.version import version as AIRFLOW_VERSION
+import logging
 
 __author__ = """OpenLineage"""
 __version__ = "0.9.0"
@@ -10,3 +11,11 @@ __version__ = "0.9.0"
 if parse_version(AIRFLOW_VERSION) < parse_version("2.0.0"):
     from openlineage.airflow.dag import DAG
     __all__ = ["DAG"]
+    logging.warning(
+        f'''
+        OpenLineage supprot for Airflow version {AIRFLOW_VERSION}
+        is DEPRECATED, and will be desupported on September 30, 2022.
+        Please make sure to upgrade your Airflow version to minimum of 2.0.0
+        in order to continue using OpenLineage.
+        '''
+    )
