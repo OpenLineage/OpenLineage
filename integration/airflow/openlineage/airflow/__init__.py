0602c6fd6da91b5192d809171ac668c65e9d2d26
Howard Yoo <32691630+howardyoo@users.noreply.github.com>
2022-05-27 06:37:16 -0500

    * rebase Signed-off-by: Tyler Farris <TylerpFarris@gmail.com>

--- integration/airflow/openlineage/airflow/__init__.py
+++ integration/airflow/openlineage/airflow/__init__.py
@@ -8,15 +8,16 @@ import logging
 __author__ = """OpenLineage"""
 __version__ = "0.10.0"
 
-if parse_version(AIRFLOW_VERSION) < parse_version("2.0.0"):     # type: ignore
+if parse_version(AIRFLOW_VERSION) < parse_version("2.0.0"):  # type: ignore
     from openlineage.airflow.dag import DAG
+
     logging.warning(
-        f'''
+        f"""
         OpenLineage supprot for Airflow version {AIRFLOW_VERSION}
         is DEPRECATED, and will be desupported on September 30, 2022.
         Please make sure to upgrade your Airflow version to minimum of 2.0.0
         in order to continue using OpenLineage.
-        '''
+        """
     )
 else:
     from airflow.models import DAG
