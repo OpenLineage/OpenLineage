25897f113114829f3b44f37d7d1dc21aa6df7e8f
Maciej Obuchowski <obuchowski.maciej@gmail.com>
2022-05-31 14:01:31 +0200

    flink: add iceberg source (#765)
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>

--- integration/flink/src/test/resources/generate_events.sh
+++ integration/flink/src/test/resources/generate_events.sh
@@ -1,11 +1,11 @@
 kafka-avro-console-producer \
   --topic io.openlineage.flink.kafka.input1 \
-  --bootstrap-server kafka:9092 \
+  --broker-list kafka:9092 \
   --property schema.registry.url=http://schema-registry:8081 \
   --property value.schema="$(< /tmp/InputEvent.avsc)" < /tmp/events.json &&
   kafka-avro-console-producer \
     --topic io.openlineage.flink.kafka.input2 \
-    --bootstrap-server kafka:9092 \
+    --broker-list kafka:9092 \
     --property schema.registry.url=http://schema-registry:8081 \
     --property value.schema="$(< /tmp/InputEvent.avsc)" < /tmp/events.json &&
 echo 'Events emitted'
