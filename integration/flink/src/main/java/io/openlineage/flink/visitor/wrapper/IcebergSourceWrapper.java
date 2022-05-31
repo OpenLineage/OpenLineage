ca9e05a2e0c3e4250b9136ec08898871767782a4
Maciej Obuchowski <obuchowski.maciej@gmail.com>
2022-05-31 14:01:31 +0200

    flink: add iceberg source (#765)
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>

---
+++ integration/flink/src/main/java/io/openlineage/flink/visitor/wrapper/IcebergSourceWrapper.java
@@ -0,0 +1,25 @@
+package io.openlineage.flink.visitor.wrapper;
+
+import org.apache.iceberg.Table;
+import org.apache.iceberg.flink.TableLoader;
+import org.apache.iceberg.flink.source.StreamingMonitorFunction;
+
+public class IcebergSourceWrapper {
+
+  private final StreamingMonitorFunction source;
+
+  public IcebergSourceWrapper(StreamingMonitorFunction streamingMonitorFunction) {
+    this.source = streamingMonitorFunction;
+  }
+
+  public static IcebergSourceWrapper of(StreamingMonitorFunction streamingMonitorFunction) {
+    return new IcebergSourceWrapper(streamingMonitorFunction);
+  }
+
+  public Table getTable() {
+    return WrapperUtils.<TableLoader>getFieldValue(
+            StreamingMonitorFunction.class, source, "tableLoader")
+        .map(TableLoader::loadTable)
+        .get();
+  }
+}
