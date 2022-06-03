7edea0b20ce60f4074f215ee7a6fbedf66a5f294
Michael Collado <40346148+collado-mike@users.noreply.github.com>
2022-06-03 15:01:20 -0700

    Made listener thread use a queue rather than spinning up many new thr… (#820)
    * Made listener thread use a queue rather than spinning up many new threads
    
    Signed-off-by: Michael Collado <collado.mike@gmail.com>
    
    * Added test for hanging extractor that sleeps for 30 seconds before returning
    
    Signed-off-by: Michael Collado <collado.mike@gmail.com>
    Signed-off-by: Tyler Farris <TylerpFarris@gmail.com>

---
+++ integration/airflow/tests/integration/failures/airflow/dags/hanging_extractor.py
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: Apache-2.0
+from time import sleep
+from typing import Union, Optional, List
+
+from openlineage.airflow.extractors import TaskMetadata
+from openlineage.airflow.extractors.base import BaseExtractor
+from openlineage.client.run import Dataset
+
+
+class HangingExtractor(BaseExtractor):
+    """
+    Custom extractor that hangs for 30 seconds. The listener module should terminate the thread that executes
+    this extractor after waiting for the timeout to complete.
+    """
+    @classmethod
+    def get_operator_classnames(cls) -> List[str]:
+        return ['CustomOperator']
+
+    def extract(self) -> Union[Optional[TaskMetadata], List[TaskMetadata]]:
+        sleep(30)
+        return TaskMetadata(
+            "test",
+            inputs=[
+                Dataset(
+                    namespace="test",
+                    name="dataset",
+                    facets={}
+                )
+            ]
+        )
