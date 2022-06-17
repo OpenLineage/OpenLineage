d7e175b4ad57f5a229e5b5625b23b583f6559866
tomasznazarewicz <t.nazarewicz94@gmail.com>
2022-06-17 16:54:50 +0200

    remove one newline
    Signed-off-by: tomasznazarewicz <t.nazarewicz94@gmail.com>

--- integration/spark/app/src/main/java/io/openlineage/spark/agent/ArgumentParser.java
+++ integration/spark/app/src/main/java/io/openlineage/spark/agent/ArgumentParser.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
