4e61a6640d2d54f19b6a02f05a48f646758c720c
Benji Lampel <benjamin@astronomer.io>
2022-06-29 14:06:24 -0400

    Revert postgres extractor as Snowflake extractor no longer inherits from it.
    Signed-off-by: Benji Lampel <benjamin@astronomer.io>

--- integration/airflow/openlineage/airflow/extractors/sql_check_extractors.py
+++ integration/airflow/openlineage/airflow/extractors/sql_check_extractors.py
@@ -1,27 +1,86 @@
 import logging
 from abc import abstractmethod
-from typing import List
+from typing import List, Optional, Dict
+from sqlalchemy import MetaData, Table
 
-from openlineage.airflow.extractors.postgres_extractor import PostgresExtractor
-from openlineage.common.dataset import Dataset, Field
-from facet_builders import (
-    build_check_facets,
-    build_value_check_facets,
-    build_interval_check_facets,
-    build_threshold_check_facets,
+from openlineage.airflow.extractors.sql_extractor import SqlExtractor
+from openlineage.airflow.extractors.dbapi_utils import get_table_schemas
+from openlineage.airflow.extractors.base import TaskMetadata
+from openlineage.client.facet import SqlJobFacet
+from openlineage.common.sql import SqlMeta, parse
+from openlineage.common.dataset import Dataset, Source, Field
+from openlineage.airflow.utils import (
     build_column_check_facets,
     build_table_check_facets
 )
-from sqlalchemy import MetaData, Table
 
 logger = logging.getLogger(__name__)
 
 
-class BaseSqlCheckExtractor(PostgresExtractor):
+class BaseSqlCheckExtractor(SqlExtractor):
     default_schema = 'public'
 
     def __init__(self, operator):
         super().__init__(operator)
+
+    def extract_on_complete(self, task_instance) -> Optional[TaskMetadata]:
+        task_name = f"{self.operator.dag_id}.{self.operator.task_id}"
+        job_facets = {"sql": SqlJobFacet(self.operator.sql)}
+        run_facets: Dict = {}
+
+        # (1) Parse sql statement to obtain input / output tables.
+        logger.debug(f"Sending SQL to parser: {self.operator.sql}")
+        sql_meta: Optional[SqlMeta] = parse(self.operator.sql, self.default_schema)
+        logger.debug(f"Got meta {sql_meta}")
+
+        if not sql_meta:
+            return TaskMetadata(
+                name=task_name,
+                inputs=[],
+                outputs=[],
+                run_facets=run_facets,
+                job_facets=job_facets,
+            )
+
+        # (2) Construct source object
+        source = Source(
+            scheme=self._scheme,
+            authority=self._get_authority(),
+            connection_url=self._get_connection_uri(),
+        )
+
+        database = getattr(self.operator, "database", None)
+        if not database:
+            database = self._get_database()
+
+        # (3) Map input / output tables to dataset objects with source set
+        # as the current connection. We need to also fetch the schema for the
+        # input tables to format the dataset name as:
+        # {schema_name}.{table_name}
+        inputs, outputs = get_table_schemas(
+            self.hook,
+            source,
+            database,
+            self._get_in_query(sql_meta.in_tables) if sql_meta.in_tables else None,
+            self._get_out_query(sql_meta.out_tables) if sql_meta.out_tables else None,
+        )
+
+        for ds in inputs:
+            ds.input_facets = self._build_facets()
+
+        db_specific_run_facets = self._get_db_specific_run_facets(
+            source, inputs, outputs
+        )
+
+        run_facets = {**run_facets, **db_specific_run_facets}
+
+        return TaskMetadata(
+            name=task_name,
+            inputs=[ds.to_openlineage_dataset() for ds in inputs],
+            outputs=[ds.to_openlineage_dataset() for ds in outputs],
+            run_facets=run_facets,
+            job_facets=job_facets,
+        )
 
     def _get_input_tables(self, source, database, sql_meta):
         inputs = []
@@ -68,7 +127,7 @@ class SqlCheckExtractor(BaseSqlCheckExtractor):
         return ['SQLCheckOperator']
 
     def _build_facets(self) -> dict:
-        return build_check_facets()
+        return
 
 
 class SqlValueCheckExtractor(BaseSqlCheckExtractor):
@@ -80,7 +139,7 @@ class SqlValueCheckExtractor(BaseSqlCheckExtractor):
         return ['SQLValueCheckOperator']
 
     def _build_facets(self) -> dict:
-        return build_value_check_facets()
+        return
 
 
 class SqlThresholdCheckExtractor(BaseSqlCheckExtractor):
@@ -92,7 +151,7 @@ class SqlThresholdCheckExtractor(BaseSqlCheckExtractor):
         return ['SQLThresholdCheckOperator']
 
     def _build_facets(self) -> dict:
-        return build_threshold_check_facets()
+        return
 
 
 class SqlIntervalCheckExtractor(BaseSqlCheckExtractor):
@@ -104,7 +163,7 @@ class SqlIntervalCheckExtractor(BaseSqlCheckExtractor):
         return ['SQLIntervalCheckOperator']
 
     def _build_facets(self) -> dict:
-        return build_interval_check_facets()
+        return
 
 
 class SqlColumnCheckExtractor(BaseSqlCheckExtractor):
