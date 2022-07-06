c74410d609a4001102cc1729d62b8777338901e3
tomasznazarewicz <t.nazarewicz94@gmail.com>
2022-07-06 16:48:33 +0200

    testing integration tests on circle CI
    Signed-off-by: tomasznazarewicz <t.nazarewicz94@gmail.com>

--- integration/spark/app/src/main/java/io/openlineage/spark/agent/lifecycle/VisitorFactoryProvider.java
+++ integration/spark/app/src/main/java/io/openlineage/spark/agent/lifecycle/VisitorFactoryProvider.java
@@ -14,6 +14,9 @@ class VisitorFactoryProvider {
   private static final String SPARK3_FACTORY_NAME =
       "io.openlineage.spark.agent.lifecycle.Spark3VisitorFactoryImpl";
 
+  private static final String SPARK32_FACTORY_NAME =
+      "io.openlineage.spark.agent.lifecycle.Spark32VisitorFactoryImpl";
+
   static VisitorFactory getInstance() {
     String version = package$.MODULE$.SPARK_VERSION();
     try {
@@ -28,7 +31,11 @@ class VisitorFactoryProvider {
     if (version.startsWith("2.")) {
       return SPARK2_FACTORY_NAME;
     } else {
-      return SPARK3_FACTORY_NAME;
+      if (version.startsWith("3.2")) {
+        return SPARK32_FACTORY_NAME;
+      } else {
+        return SPARK3_FACTORY_NAME;
+      }
     }
   }
 }
