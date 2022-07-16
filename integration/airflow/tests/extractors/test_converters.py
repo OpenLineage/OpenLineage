68a35255f5c2901979317f765e0652ffb286f6d6
Fenil Doshi <fdoshi@slack-corp.com>
2022-07-15 18:46:24 -0700

    moved importing the function inside the test
    Signed-off-by: Fenil Doshi <fdoshi@slack-corp.com>

--- integration/airflow/tests/extractors/test_converters.py
+++ integration/airflow/tests/extractors/test_converters.py
@@ -8,14 +8,13 @@ from pkg_resources import parse_version
 if parse_version(AIRFLOW_VERSION) < parse_version("2.0.0"):
     pytestmark = pytest.mark.skip("Skipping tests for Airflow version < 2")
 
-from openlineage.airflow.extractors.converters import table_to_dataset
-
 
 @pytest.mark.skipif(
     parse_version(AIRFLOW_VERSION) < parse_version("2.0.0"),
     reason="requires AIRFLOW_VERSION to be higher than 2.0",
 )
 def test_table_to_dataset_conversion():
+    from openlineage.airflow.extractors.converters import table_to_dataset
     from airflow.lineage.entities import Table
     t = Table(
         database="db",
