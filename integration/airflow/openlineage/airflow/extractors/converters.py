2f421b67673b76524c61eec3b73d24e889d3146c
Fenil Doshi <fdoshi@slack-corp.com>
2022-07-15 18:22:29 -0700

    moved import to resolve errors for conflicting airflow versions
    Signed-off-by: Fenil Doshi <fdoshi@slack-corp.com>

--- integration/airflow/openlineage/airflow/extractors/converters.py
+++ integration/airflow/openlineage/airflow/extractors/converters.py
@@ -1,8 +1,9 @@
 from openlineage.client.run import Dataset
-from airflow.lineage.entities import Table
 
 
 def table_to_dataset(table: Table):
+    from airflow.lineage.entities import Table
+
     return Dataset(
         namespace=f"{table.cluster}",
         name=f"{table.database}.{table.name}",
