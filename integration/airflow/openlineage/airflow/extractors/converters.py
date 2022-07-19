13b1b952601f677a1dab7da8ecba68c662686de8
Fenil Doshi <fdoshi@umass.edu>
2022-07-19 09:57:27 -0700

    Create converters.py
    Signed-off-by: Fenil Doshi <fdoshi@slack-corp.com>

--- integration/airflow/openlineage/airflow/extractors/converters.py
+++ integration/airflow/openlineage/airflow/extractors/converters.py
@@ -6,11 +6,5 @@ def table_to_dataset(table: Table):
     return Dataset(
         namespace=f"{table.cluster}",
         name=f"{table.database}.{table.name}",
-        facets={
-            "tags": table.tags,
-            "description": table.description,
-            "columns": table.columns,
-            "owners": table.owners,
-            "extra": table.extra,
-        }
+        facets={},
     )
