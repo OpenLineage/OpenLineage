3d00a1a944ff6b65c90b775e0b144200b4b7822b
tomasznazarewicz <t.nazarewicz94@gmail.com>
2022-07-29 10:20:05 +0200

    spotless
    Signed-off-by: tomasznazarewicz <t.nazarewicz94@gmail.com>

--- integration/spark/app/src/test/java/io/openlineage/spark/agent/lifecycle/LibraryTest.java
+++ integration/spark/app/src/test/java/io/openlineage/spark/agent/lifecycle/LibraryTest.java
@@ -14,7 +14,6 @@ import static org.mockito.internal.verification.VerificationModeFactory.times;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
