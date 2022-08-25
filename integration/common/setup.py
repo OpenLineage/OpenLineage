d8a8d3ef0ec32a2d02f109412b111d52f74e263c
Michael Robinson <merobi@gmail.com>
2022-08-25 18:36:53 -0400

    Prepare for release 0.13.1
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- integration/common/setup.py
+++ integration/common/setup.py
@@ -10,7 +10,7 @@ from setuptools import find_namespace_packages, setup
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-__version__ = "0.14.0"
+__version__ = "0.13.1"
 
 requirements = [
     "attrs>=19.3.0",
