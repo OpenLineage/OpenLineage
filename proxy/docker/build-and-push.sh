6bcbb25f45c8d69d12ee4250ab5dd08bde58e838
wslulciuc <willy@datakin.com>
2022-09-12 14:04:29 -0700

    Add build-and-push.sh for proxy and CI job
    Signed-off-by: wslulciuc <willy@datakin.com>

---
+++ proxy/docker/build-and-push.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Copyright 2018-2022 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+#
+# Usage: $ ./build-and-push.sh <version>
+
+set -eu
+
+readonly SEMVER_REGEX="^[0-9]+(\.[0-9]+){2}(-rc\.[0-9]+)?$" # X.Y.Z
+readonly ORG="openlineage"
+
+# Change working directory to proxy module
+project_root=$(git rev-parse --show-toplevel)
+cd "${project_root}/proxy"
+
+# Version X.Y.Z of proxy image to build
+version="${1}"
+
+# Ensure valid version
+if [[ ! "${version}" =~ ${SEMVER_REGEX} ]]; then
+  echo "Version must match ${SEMVER_REGEX}"
+  exit 1
+fi
+
+echo "Building image (tag: ${version})..."
+
+# Build, tag and push proxy image
+docker build --no-cache --tag "${ORG}/proxy:${version}" .
+docker tag "${ORG}/proxy:${version}" "${ORG}/proxy:latest"
+
+docker push "${ORG}/proxy:${version}"
+docker push "${ORG}/proxy:latest"
+
+echo "DONE!"
+
