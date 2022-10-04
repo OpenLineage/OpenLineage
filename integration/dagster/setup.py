255c808e41ce495dfb32ff499b094d5ca3dd25f2
Michael Robinson <merobi@gmail.com>
2022-10-04 11:20:10 -0400

    Prepare next development version 0.16.0-SNAPSHOT
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- integration/dagster/setup.py
+++ integration/dagster/setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_namespace_packages
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-__version__ = "0.15.0"
+__version__ = "0.16.0"
 
 DAGSTER_VERSION = "0.13.8"
 
--- integration/dbt/setup.py
+++ integration/dbt/setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 with open("README.md") as readme_file:
      readme = readme_file.read()
 
-__version__ = "0.15.0"
+__version__ = "0.16.0"
 
 requirements = [
     f"tqdm>=4.62.0",
