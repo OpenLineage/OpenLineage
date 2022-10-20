af44b5d6d61ea6d04601c11345adf2a59da04043
Maciej Obuchowski <obuchowski.maciej@gmail.com>
2022-10-20 11:09:26 +0200

    airflow: add default extractor (#1162)
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>
    
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>

---
+++ integration/airflow/tests/extractors/test_default_extractor.py
@@ -0,0 +1,72 @@
+# Copyright 2018-2022 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+from typing import Any
+
+from airflow.models import BaseOperator
+from airflow.operators.python import PythonOperator
+
+from openlineage.airflow.extractors import Extractors
+from openlineage.airflow.extractors.base import OperatorLineage, DefaultExtractor, TaskMetadata
+from openlineage.airflow.extractors.python_extractor import PythonExtractor
+from openlineage.client.facet import ParentRunFacet, SqlJobFacet
+from openlineage.client.run import Dataset
+
+
+INPUTS = [Dataset(namespace="database://host:port", name="inputtable")]
+OUTPUTS = [Dataset(namespace="database://host:port", name="inputtable")]
+RUN_FACETS = {
+    "parent": ParentRunFacet.create(
+        "3bb703d1-09c1-4a42-8da5-35a0b3216072", "namespace", "parentjob"
+    )
+}
+JOB_FACETS = {"sql": SqlJobFacet(query="SELECT * FROM inputtable")}
+
+
+class ExampleOperator(BaseOperator):
+    def execute(self, context) -> Any:
+        pass
+
+    def get_openlineage_facets(self) -> OperatorLineage:
+        return OperatorLineage(
+            inputs=INPUTS,
+            outputs=OUTPUTS,
+            run_facets=RUN_FACETS,
+            job_facets=JOB_FACETS
+        )
+
+
+class BrokenOperator(BaseOperator):
+    get_openlineage_facets = []
+
+    def execute(self, context) -> Any:
+        pass
+
+
+def test_default_extraction():
+    extractor = Extractors().get_extractor_class(ExampleOperator)
+    assert extractor is DefaultExtractor
+
+    metadata = extractor(ExampleOperator(task_id="test")).extract()
+
+    assert metadata == TaskMetadata(
+        name="adhoc_airflow.test",
+        inputs=INPUTS,
+        outputs=OUTPUTS,
+        run_facets=RUN_FACETS,
+        job_facets=JOB_FACETS
+    )
+
+
+def test_does_not_use_default_extractor_when_not_a_method():
+    extractor = Extractors().get_extractor_class(BrokenOperator)
+    assert extractor is None
+
+
+def test_does_not_use_default_extractor_when_no_get_openlineage_facets():
+    extractor = Extractors().get_extractor_class(BaseOperator)
+    assert extractor is None
+
+
+def test_does_not_use_default_extractor_when_explicite_extractor():
+    extractor = Extractors().get_extractor_class(PythonOperator)
+    assert extractor is PythonExtractor
