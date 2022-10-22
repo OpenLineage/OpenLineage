c55fe15be4b1a45d2052a386aa7acb7d2ec13a42
Piotr Wojtczak <piotr.m.wojtczak@gmail.com>
2022-10-22 16:12:38 +0000

    Increase test coverage
    Signed-off-by: Piotr Wojtczak <piotr.m.wojtczak@gmail.com>

--- integration/sql/impl/tests/column_lineage/tests_aliases.rs
+++ integration/sql/impl/tests/column_lineage/tests_aliases.rs
@@ -73,3 +73,74 @@ fn test_nested_renaming_with_join() {
         ]
     );
 }
+
+#[test]
+fn test_multiple_tables() {
+    let output = test_sql(
+        "SELECT t1.a, t2.b, t3.c
+         FROM table1 t1, table2 t2, table3 t3",
+    )
+    .unwrap();
+    assert_eq!(
+        output.column_lineage,
+        vec![
+            ColumnLineage {
+                descendant: ColumnMeta {
+                    origin: None,
+                    name: "a".to_string()
+                },
+                lineage: vec![ColumnMeta {
+                    origin: Some(table("table1")),
+                    name: "a".to_string()
+                }]
+            },
+            ColumnLineage {
+                descendant: ColumnMeta {
+                    origin: None,
+                    name: "b".to_string()
+                },
+                lineage: vec![ColumnMeta {
+                    origin: Some(table("table2")),
+                    name: "b".to_string()
+                }]
+            },
+            ColumnLineage {
+                descendant: ColumnMeta {
+                    origin: None,
+                    name: "c".to_string()
+                },
+                lineage: vec![ColumnMeta {
+                    origin: Some(table("table3")),
+                    name: "c".to_string()
+                }]
+            },
+        ]
+    );
+}
+
+#[test]
+fn test_deeply_nested_alias_chain() {
+    let output = test_sql(
+        "SELECT d as e FROM (
+            SELECT c as d FROM (
+                SELECT b as c FROM (
+                    SELECT a as b FROM table1 t
+                )
+            )
+         )",
+    )
+    .unwrap();
+    assert_eq!(
+        output.column_lineage,
+        vec![ColumnLineage {
+            descendant: ColumnMeta {
+                origin: None,
+                name: "e".to_string()
+            },
+            lineage: vec![ColumnMeta {
+                origin: Some(table("table1")),
+                name: "a".to_string()
+            }]
+        },]
+    );
+}
