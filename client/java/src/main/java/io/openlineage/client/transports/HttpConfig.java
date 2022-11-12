c6100e26ea0ae439d67404881b6624aef57ebbaf
Michael Collado <40346148+collado-mike@users.noreply.github.com>
2022-11-11 16:42:38 -0800

    Deprecate HttpTransport.Builder in favor of HttpConfig only (#1287)
    * Deprecate HttpTransport.Builder in favor of HttpConfig only
    
    Signed-off-by: Michael Collado <collado.mike@gmail.com>
    
    * Update changelog
    
    Signed-off-by: Michael Collado <collado.mike@gmail.com>
    
    Signed-off-by: Michael Collado <collado.mike@gmail.com>

--- client/java/src/main/java/io/openlineage/client/transports/HttpConfig.java
+++ client/java/src/main/java/io/openlineage/client/transports/HttpConfig.java
@@ -7,13 +7,17 @@ package io.openlineage.client.transports;
 
 import java.net.URI;
 import javax.annotation.Nullable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.With;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @ToString
+@With
 public final class HttpConfig implements TransportConfig {
   @Getter @Setter private URI url;
   @Getter @Setter private @Nullable String endpoint;
