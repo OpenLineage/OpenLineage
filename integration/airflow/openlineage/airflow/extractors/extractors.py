d9230dc94de18bfaa85065345b70634aab4ff3a2
Faisal <fhoda@users.noreply.github.com>
2022-11-28 16:47:20 -0600

    fix rebase conflict
    Signed-off-by: Faisal <fhoda@users.noreply.github.com>

--- integration/airflow/openlineage/airflow/extractors/extractors.py
+++ integration/airflow/openlineage/airflow/extractors/extractors.py
@@ -4,7 +4,7 @@ import os
 
 from typing import Type, Optional
 
-from openlineage.airflow.extractors.base import BaseExtractor
+from openlineage.airflow.extractors.base import BaseExtractor, DefaultExtractor
 from openlineage.airflow.utils import import_from_string, try_import_from_string
 from openlineage.airflow.extractors.sql_check_extractors import get_check_extractors
 
