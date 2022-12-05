0a1ae4560a33bcccb20645f42dcd3d92befa780c
Faisal <fhoda@users.noreply.github.com>
2022-12-05 10:22:52 -0600

    fix test ignore version
    Signed-off-by: Faisal <fhoda@users.noreply.github.com>

--- integration/airflow/tests/conftest.py
+++ integration/airflow/tests/conftest.py
@@ -17,11 +17,9 @@ collect_ignore = []
 if parse_version(AIRFLOW_VERSION) < parse_version("2.3.0"):
     collect_ignore.append("test_listener.py")
 
-if parse_version(AIRFLOW_VERSION) < parse_version("2.1.4"):
-    collect_ignore.append("extractors/test_s3_extractor.py")
-
 if parse_version(AIRFLOW_VERSION) < parse_version("2.2.4"):
     collect_ignore.append("extractors/test_redshift_sql_extractor.py")
+    collect_ignore.append("extractors/test_s3_extractor.py")
 
 if parse_version(AIRFLOW_VERSION) < parse_version("2.3.0"):
     collect_ignore.append("extractors/test_redshift_data_extractor.py")
