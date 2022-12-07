9564dd8644b28dd17fb1aff61fad6a2315048f8d
tomasznazarewicz <tomasz.nazarewicz@getindata.com>
2022-12-07 16:33:57 +0100

    rerun integration tests on ci server
    Signed-off-by: tomasznazarewicz <tomasz.nazarewicz@getindata.com>

--- integration/spark/app/src/test/java/io/openlineage/spark/agent/EventEmitterTest.java
+++ integration/spark/app/src/test/java/io/openlineage/spark/agent/EventEmitterTest.java
@@ -8,30 +8,5 @@ package io.openlineage.spark.agent;
 class EventEmitterTest {
 
   ArgumentParser.ArgumentParserBuilder builder = new ArgumentParser.ArgumentParserBuilder();
-
-  //  @Test
-  //  void testLineageUri() throws URISyntaxException {
-  //    SparkConf conf = new SparkConf()
-  //            .set(ArgumentParser.SPARK_CONF_HTTP_URL,
-  // "https://localhost:5000/api/v1/namespaces/ns_name/jobs/job_name/runs/"
-  //            + "ea445b5c-22eb-457a-8007-01c7c52b6e54?api_key=abc")
-  //            .set("spark.openlineage.transport.type", "http");
-  //    ArgumentParser argPars = ArgumentParser.parse(conf);
-  //    EventEmitter ctx = new EventEmitter(argPars);
-  ////    ctx.getClient()
-  ////    assertEquals(URI.create("https://localhost:5000/api/v1/lineage"), ctx.getLineageURI());
-  //  }
-  //
-  //  @Test
-  //  void testLineageUriWithExtraParams() throws URISyntaxException {
-  //    SparkConf conf = new SparkConf().set(ArgumentParser.SPARK_CONF_HTTP_URL,
-  // "https://localhost:5000/api/v1/namespaces/ns_name/jobs/job_name/runs/"
-  //            + "ea445b5c-22eb-457a-8007-01c7c52b6e54?api_key=abc&code=123&foo=bar")
-  //            .set("spark.openlineage.transport.type", "http");
-  //    ArgumentParser argPars = ArgumentParser.parse(conf);
-  //    EventEmitter ctx = new EventEmitter(argPars);
-  ////    assertEquals(
-  ////        URI.create("https://localhost:5000/api/v1/lineage?code=123&foo=bar"),
-  // ctx.getLineageURI());
-  //  }
+  
 }
