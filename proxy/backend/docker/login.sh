01893bbaad8d7a2c6ff3112c3208cc080e8c0e9d
Willy Lulciuc <willy@datakin.com>
2023-01-04 15:36:39 -0800

    Add `login.sh` for proxy docker release (#1463)
    * Add login.sh for proxy docker release
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    * continued: Add login.sh for proxy docker release
    
    Signed-off-by: wslulciuc <willy@datakin.com>
    
    Signed-off-by: wslulciuc <willy@datakin.com>

---
+++ proxy/backend/docker/login.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Copyright 2018-2022 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+#
+# Usage: $ ./login.sh
+
+set -eu
+
+docker login --username "${DOCKER_LOGIN}" --password "${DOCKER_PASSWORD}"
