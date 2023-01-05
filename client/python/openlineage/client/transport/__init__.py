db0faae62e85da6897bb1473741284ca871c3983
Michael Robinson <merobi@gmail.com>
2023-01-05 16:48:58 -0500

    update spdx headers in python client
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- client/python/openlineage/client/transport/__init__.py
+++ client/python/openlineage/client/transport/__init__.py
@@ -1,3 +1,6 @@
+# Copyright 2018-2023 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+
 # flake8: noqa
 from typing import Type
 
--- client/python/openlineage/client/transport/http.py
+++ client/python/openlineage/client/transport/http.py
@@ -1,5 +1,6 @@
-# Copyright 2018-2022 contributors to the OpenLineage project
-# SPDX-License-Identifier: Apache-2.0.
+# Copyright 2018-2023 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+
 import logging
 from urllib.parse import urljoin
 
