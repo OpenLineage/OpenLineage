68ee93a8cc61e921fcdbcb6f869e152b98f55d6a
Michael Robinson <merobi@gmail.com>
2023-01-05 17:26:54 -0500

    update spdx headers in common integration
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- integration/common/README.md
+++ integration/common/README.md
@@ -1,1 +1,5 @@
 Common modules for OpenLineage integrations.
+
+----
+SPDX-License-Identifier: Apache-2.0\
+Copyright 2018-2023 contributors to the OpenLineage project
--- integration/common/openlineage/common/schema/__init__.py
+++ integration/common/openlineage/common/schema/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2018-2022 contributors to the OpenLineage project
+# Copyright 2018-2023 contributors to the OpenLineage project
 # SPDX-License-Identifier: Apache-2.0
 
 GITHUB_LOCATION = "https://github.com/OpenLineage/OpenLineage/tree/main/integration/common/openlineage/schema/"  # noqa: E501
