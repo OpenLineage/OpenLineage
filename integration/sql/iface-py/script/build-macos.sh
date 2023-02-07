5747a102db7589516e7d76d00f9555aa5f370ac7
Maciej Obuchowski <obuchowski.maciej@gmail.com>
2023-02-07 20:34:48 +0100

    ci: fix another part of release failures (#1593)
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>

--- integration/sql/iface-py/script/build-macos.sh
+++ integration/sql/iface-py/script/build-macos.sh
@@ -8,7 +8,7 @@
 set -e
 
 if [ -x "$(command -v /usr/local/opt/python@3.7/bin/python3)" ]; then
-  /usr/local/opt/python@3.7/bin/python3 -m pip install maturin
+  /usr/local/opt/python@3.7/bin/python3 -m venv .env
 elif [ -x "$(command -v /usr/local/bin/python3.7)" ]; then
   /usr/local/bin/python3.7 -m venv .env
 elif [ -x "$(command -v python3.7)" ]; then
