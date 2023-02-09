93cf0d1295e9896b49d894c6c5a605f0914b7615
Michael Robinson <merobi@gmail.com>
2023-02-09 12:52:06 -0500

    Prepare next development version 0.21.0-SNAPSHOT
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- integration/common/setup.py
+++ integration/common/setup.py
@@ -10,7 +10,7 @@ from setuptools import find_namespace_packages, setup
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-__version__ = "0.20.5"
+__version__ = "0.21.0"
 
 requirements = [
     "attrs>=19.3.0",
