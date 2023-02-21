00b40335095ce584da5991c5a8953b01e7c00e4d
Natalie Zeller <natalie.zeller@naturalint.com>
2023-02-21 15:54:08 +0200

    Introduce new output visitors: AlterTableAddPartitionCommandVisitor and AlterTableSetLocationCommandVisitor (#1629)
    * Introduce new output visitors: AlterTableAddPartitionCommandVisitor, AlterTableSetLocationCommandVisitor
    
    Signed-off-by: natalie.zeller <natalie.zeller@naturalint.com>
    
    * fix format
    
    Signed-off-by: natalie.zeller <natalie.zeller@naturalint.com>
    
    * fix format
    
    Signed-off-by: natalie.zeller <natalie.zeller@naturalint.com>
    
    * add comments - new output visitors do not include location and partition info
    
    Signed-off-by: natalie.zeller <natalie.zeller@naturalint.com>
    
    * fix comment in md file
    
    Signed-off-by: natalie.zeller <natalie.zeller@naturalint.com>
    
    ---------
    
    Signed-off-by: natalie.zeller <natalie.zeller@naturalint.com>

---
+++ integration/spark/app/src/test/java/io/openlineage/spark/agent/lifecycle/plan/AlterTableSetLocationCommandVisitorTest.java
@@ -0,0 +1,91 @@
+/*
+/* Copyright 2018-2022 contributors to the OpenLineage project
+/* SPDX-License-Identifier: Apache-2.0
+*/
+
+package io.openlineage.spark.agent.lifecycle.plan;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+
+import io.openlineage.client.OpenLineage;
+import io.openlineage.spark.agent.SparkAgentTestExtension;
+import java.util.List;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.catalyst.TableIdentifier;
+import org.apache.spark.sql.execution.command.AlterTableSetLocationCommand;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StringType$;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import scala.Option;
+import scala.collection.Map$;
+import scala.collection.immutable.HashMap;
+
+@ExtendWith(SparkAgentTestExtension.class)
+class AlterTableSetLocationCommandVisitorTest {
+
+  private static final String TABLE_1 = "table1";
+  SparkSession session;
+  AlterTableSetLocationCommandVisitor visitor;
+  String database;
+
+  @AfterEach
+  public void afterEach() {
+    dropTables();
+  }
+
+  private void dropTables() {
+    session
+        .sessionState()
+        .catalog()
+        .dropTable(new TableIdentifier(TABLE_1, Option.apply(database)), true, true);
+  }
+
+  @BeforeEach
+  public void setup() {
+    session =
+        SparkSession.builder()
+            .config("spark.sql.warehouse.dir", "/tmp/warehouse")
+            .master("local")
+            .getOrCreate();
+
+    dropTables();
+
+    StructType schema =
+        new StructType(
+            new StructField[] {
+              new StructField("col1", StringType$.MODULE$, false, new Metadata(new HashMap<>()))
+            });
+
+    session.catalog().createTable(TABLE_1, "csv", schema, Map$.MODULE$.empty());
+    database = session.catalog().currentDatabase();
+    visitor = new AlterTableSetLocationCommandVisitor(SparkAgentTestExtension.newContext(session));
+  }
+
+  @Test
+  void testAlterTableSetLocation() {
+    AlterTableSetLocationCommand command =
+        new AlterTableSetLocationCommand(
+            new TableIdentifier(TABLE_1, Option.apply(database)),
+            Option.empty(),
+            "file:///tmp/dir");
+
+    command.run(session);
+
+    assertThat(visitor.isDefinedAt(command)).isTrue();
+    List<OpenLineage.OutputDataset> datasets = visitor.apply(command);
+    assertEquals(1, datasets.get(0).getFacets().getSchema().getFields().size());
+    assertEquals(
+        "default.table1",
+        datasets.get(0).getFacets().getSymlinks().getIdentifiers().get(0).getName());
+    assertThat(datasets)
+        .singleElement()
+        .hasFieldOrPropertyWithValue("name", "/tmp/dir")
+        .hasFieldOrPropertyWithValue("namespace", "file");
+  }
+}
