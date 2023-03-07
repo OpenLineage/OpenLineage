8f38c4cd89fbf0820385d9dd63ac2cfb766e5ddf
pawel.leszczynski <leszczynski.pawel@gmail.com>
2023-03-07 14:19:25 +0100

    SQLParser supports alter, truncate and drop statements (#1695)
    Signed-off-by: Pawel Leszczynski <leszczynski.pawel@gmail.com>

--- integration/sql/impl/tests/table_lineage/mod.rs
+++ integration/sql/impl/tests/table_lineage/mod.rs
@@ -1,9 +1,11 @@
 // Copyright 2018-2023 contributors to the OpenLineage project
 // SPDX-License-Identifier: Apache-2.0
 
+pub mod tests_alter;
 pub mod tests_copy;
 pub mod tests_create;
 pub mod tests_cte;
+pub mod tests_drop;
 pub mod tests_delete;
 pub mod tests_error_handling;
 pub mod tests_insert;
@@ -11,3 +13,4 @@ pub mod tests_merge;
 pub mod tests_select;
 pub mod tests_tpcds;
 pub mod tests_update;
+pub mod tests_truncate;
