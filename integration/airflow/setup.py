d0d89636c1d668dfc16c28dd8941da2e33a3cea6
Michael Robinson <merobi@gmail.com>
2023-06-12 09:56:14 -0400

    Prepare next development version 0.29.0-SNAPSHOT
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- integration/airflow/setup.py
+++ integration/airflow/setup.py
@@ -10,7 +10,7 @@ from setuptools import find_namespace_packages, setup
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-__version__ = "0.28.0"
+__version__ = "0.29.0"
 
 requirements = [
     "attrs>=20.3",
