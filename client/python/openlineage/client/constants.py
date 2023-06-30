222f36fd0f046942e42e01e6d0f205b62babcc54
Michael Robinson <merobi@gmail.com>
2023-06-30 11:45:44 -0400

    Prepare next development version 0.30.0-SNAPSHOT
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- client/python/openlineage/client/constants.py
+++ client/python/openlineage/client/constants.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 from __future__ import annotations
 
-__version__ = "0.29.1"
+__version__ = "0.30.0"
 
 DEFAULT_TIMEOUT_MS = 5000
 DEFAULT_NAMESPACE_NAME = "default"
