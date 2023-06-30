2942e7650cc30026a3df3a157f158bde9433bc3d
Maciej Obuchowski <obuchowski.maciej@gmail.com>
2023-06-30 17:54:31 +0200

    remove 3.7 from few more places
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>

--- integration/airflow/setup.py
+++ integration/airflow/setup.py
@@ -62,7 +62,7 @@ setup(
     include_package_data=True,
     install_requires=requirements,
     extras_require=extras_require,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     zip_safe=False,
     keywords="openlineage",
     entry_points={
