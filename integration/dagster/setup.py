f83edcf8a2bc0e7eded459bd3cbcefb156fb1142
Michael Robinson <merobi@gmail.com>
2023-07-25 12:24:38 -0400

    Prepare next development version 1.0.0-SNAPSHOT
    Signed-off-by: Michael Robinson <merobi@gmail.com>
    Signed-off-by: Sheeri K. Cabral <me@sheeri.com>

--- integration/dagster/setup.py
+++ integration/dagster/setup.py
@@ -10,7 +10,7 @@ from setuptools import find_namespace_packages, setup
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-__version__ = "0.30.1"
+__version__ = "1.0.0"
 
 DAGSTER_VERSION = "0.13.8"
 
