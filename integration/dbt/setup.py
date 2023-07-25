3ce04d33fe4dd71c4d67b341c159ad2e3f0f15fc
Michael Robinson <merobi@gmail.com>
2023-07-25 12:02:38 -0400

    Prepare next development version 1.0.0-SNAPSHOT
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- integration/dbt/setup.py
+++ integration/dbt/setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 with open("README.md") as readme_file:
      readme = readme_file.read()
 
-__version__ = "0.30.0"
+__version__ = "1.0.0"
 
 requirements = [
     "tqdm>=4.62.0",
