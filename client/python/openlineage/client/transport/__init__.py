52f7281a3fb86f9c6cf5a8089f49fc2f6327bfdd
Alex <alexandre.pro.bergere@gmail.com>
2023-07-25 12:13:40 +0200

    Adding a file transport (#1891)
    * feat: add file transport
    
    Signed-off-by: Alexandre Bergere <alexandre.bergere@datagalaxy.com>
    
    * fix: python client
    
    Signed-off-by: Alexandre Bergere <alexandre.bergere@datagalaxy.com>
    
    ---------
    
    Signed-off-by: Alexandre Bergere <alexandre.bergere@datagalaxy.com>
    Co-authored-by: Alexandre Bergere <alexandre.bergere@datagalaxy.com>

--- client/python/openlineage/client/transport/__init__.py
+++ client/python/openlineage/client/transport/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from openlineage.client.transport.console import ConsoleTransport
 from openlineage.client.transport.factory import DefaultTransportFactory
+from openlineage.client.transport.file import FileTransport
 from openlineage.client.transport.http import HttpConfig, HttpTransport
 from openlineage.client.transport.kafka import KafkaConfig, KafkaTransport
 from openlineage.client.transport.noop import NoopTransport
@@ -14,6 +15,7 @@ _factory.register_transport(HttpTransport.kind, HttpTransport)
 _factory.register_transport(KafkaTransport.kind, KafkaTransport)
 _factory.register_transport(ConsoleTransport.kind, ConsoleTransport)
 _factory.register_transport(NoopTransport.kind, NoopTransport)
+_factory.register_transport(FileTransport.kind, FileTransport)
 
 
 def get_default_factory() -> DefaultTransportFactory:
