3eacfe424a1af28704a9b8aa74265cf117764d21
Maciej Obuchowski <obuchowski.maciej@gmail.com>
2023-07-26 16:36:56 +0200

    airflow: convert lineage from legacy file definition (#2006)
    * airflow: convert lineage from legacy file definition
    
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>
    
    * Update integration/airflow/openlineage/airflow/extractors/converters.py
    
    Co-authored-by: JDarDagran <kuba0221@gmail.com>
    
    ---------
    
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>
    Co-authored-by: JDarDagran <kuba0221@gmail.com>
    Signed-off-by: Sheeri K. Cabral <me@sheeri.com>

--- integration/airflow/tests/extractors/test_converters.py
+++ integration/airflow/tests/extractors/test_converters.py
@@ -1,9 +1,11 @@
 # Copyright 2018-2023 contributors to the OpenLineage project
 # SPDX-License-Identifier: Apache-2.0
+import pytest
+from openlineage.airflow.extractors.converters import convert_to_dataset
+from openlineage.client.run import Dataset
+
 
 def test_table_to_dataset_conversion():
-    from openlineage.airflow.extractors.converters import convert_to_dataset
-
     from airflow.lineage.entities import Table
     t = Table(
         database="db",
@@ -18,8 +20,6 @@ def test_table_to_dataset_conversion():
 
 
 def test_dataset_to_dataset_conversion():
-    from openlineage.airflow.extractors.converters import convert_to_dataset
-    from openlineage.client.run import Dataset
     t = Dataset(
         namespace="c",
         name="db.table1",
@@ -30,3 +30,24 @@ def test_dataset_to_dataset_conversion():
 
     assert d.namespace == "c"
     assert d.name == "db.table1"
+
+
+@pytest.mark.parametrize("uri, dataset", [
+    ("s3://my-bucket/my-file", Dataset("s3://my-bucket", "/my-file")),
+    ("gcs://my-bucket/some/path/to/file", Dataset("gs://my-bucket", "/some/path/to/file")),
+    ("gs://my-bucket/some/path/to/file", Dataset("gs://my-bucket", "/some/path/to/file")),
+    ("something://asdf", Dataset("something", "/asdf")),
+    ("file://path/to/something", Dataset("file", "/path/to/something")),
+    ("not|a|uri", None)
+])
+def test_gcs_file_to_dataset_conversion(uri, dataset):
+    from airflow.lineage.entities import File
+
+    f = File(url=uri)
+    d = convert_to_dataset(f)
+
+    if dataset is None:
+        assert d is None
+    else:
+        assert d.namespace == dataset.namespace
+        assert d.name == dataset.name
