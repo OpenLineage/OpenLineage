2c6b8667a02113e157f3675b1f9257f192fe06e4
pentium3 <celerond@msn.com>
2023-08-17 03:52:15 -0400

    fix a bug when getting schema for KafkaSink
    Signed-off-by: pentium3 <celerond@msn.com>

--- integration/flink/src/main/java/io/openlineage/flink/visitor/wrapper/AvroUtils.java
+++ integration/flink/src/main/java/io/openlineage/flink/visitor/wrapper/AvroUtils.java
@@ -12,29 +12,36 @@ import org.apache.flink.formats.avro.AvroSerializationSchema;
 import org.apache.flink.formats.avro.RegistryAvroSerializationSchema;
 
 public class AvroUtils {
-  public static Optional<Schema> getRegistryAvroSchema(
-      Optional<SerializationSchema> serializationSchema) {
-    return serializationSchema
-        .filter(schema -> schema instanceof RegistryAvroSerializationSchema)
-        .map(schema -> (RegistryAvroSerializationSchema) schema)
-        .flatMap(
-            schema -> {
-              WrapperUtils.invoke(
-                  RegistryAvroSerializationSchema.class, schema, "checkAvroInitialized");
-              return WrapperUtils.invoke(AvroSerializationSchema.class, schema, "getDatumWriter");
-            })
-        .flatMap(writer -> WrapperUtils.<Schema>getFieldValue(writer.getClass(), writer, "root"));
-  }
-
   public static Optional<Schema> getAvroSchema(Optional<SerializationSchema> serializationSchema) {
-    return serializationSchema
-        .filter(schema -> schema instanceof AvroSerializationSchema)
-        .map(schema -> (AvroSerializationSchema) schema)
-        .flatMap(
-            schema -> {
-              WrapperUtils.invoke(AvroSerializationSchema.class, schema, "checkAvroInitialized");
-              return WrapperUtils.invoke(AvroSerializationSchema.class, schema, "getDatumWriter");
-            })
-        .flatMap(writer -> WrapperUtils.<Schema>getFieldValue(writer.getClass(), writer, "root"));
+    // First try to get the RegistryAvroSerializationSchema
+    Optional<Schema> registryAvroSchema =
+        serializationSchema
+            .filter(schema -> schema instanceof RegistryAvroSerializationSchema)
+            .map(schema -> (RegistryAvroSerializationSchema) schema)
+            .flatMap(
+                schema -> {
+                  WrapperUtils.invoke(
+                      RegistryAvroSerializationSchema.class, schema, "checkAvroInitialized");
+                  return WrapperUtils.invoke(
+                      AvroSerializationSchema.class, schema, "getDatumWriter");
+                })
+            .flatMap(
+                writer -> WrapperUtils.<Schema>getFieldValue(writer.getClass(), writer, "root"));
+
+    // If not present, try to get the AvroSerializationSchema
+    return registryAvroSchema.isPresent()
+        ? registryAvroSchema
+        : serializationSchema
+            .filter(schema -> schema instanceof AvroSerializationSchema)
+            .map(schema -> (AvroSerializationSchema) schema)
+            .flatMap(
+                schema -> {
+                  WrapperUtils.invoke(
+                      AvroSerializationSchema.class, schema, "checkAvroInitialized");
+                  return WrapperUtils.invoke(
+                      AvroSerializationSchema.class, schema, "getDatumWriter");
+                })
+            .flatMap(
+                writer -> WrapperUtils.<Schema>getFieldValue(writer.getClass(), writer, "root"));
   }
 }
