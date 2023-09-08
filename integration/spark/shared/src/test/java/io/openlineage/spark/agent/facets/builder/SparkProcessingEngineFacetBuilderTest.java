2f171e99bda77eb54d4dd9ebe1640defc417fcc2
Damien Hawes <d-m-h@users.noreply.github.com>
2023-09-08 13:20:01 +0200

    spark: publish ProcessingEngineRunFacet (#2089)
    * spark: publish ProcessingEngineRunFacet
    
    Previously, the Spark integration published a custom facet named
    'SparkVersion'. As it is custom, it isn't defined in the OpenLineage
    spec. However, the OpenLineage spec defines a ProcessingEngineRunFacet
    that is meant to capture details about the things that runs a job.
    
    This change introduces this in the form of the
    'SparkProcessingEngineRunFacetBuilder' and
    'SparkProcessingEngineRunFacetBuilderDelegate'.
    
    As the names suggest, these classes are meant to create and populate
    ProcessingEngineRunFacet.
    
    The reason for the existence of the delegate is because there are two
    code paths that interact with the run facets, namely the code path
    within the RddExecutionContext and the other in the
    SparkSqlExecutionContext, albeit via a very roundabout way.
    
    The delegate is the object that actually constructs the facet, whilst
    the builder provides an adapter that uses the CustomFacetBuilder
    interface.
    
    Yes, it's a hacky way of doing it and may need to be changed in the
    future. For now though, its good enough.
    
    Closes: https://github.com/OpenLineage/OpenLineage/issues/2086
    Signed-off-by: Damien Hawes <d-m-h@users.noreply.github.com>
    
    * spark: Deprecated the SparkVersionFacet, alongside the version-facet.json
    
    Signed-off-by: Damien Hawes <d-m-h@users.noreply.github.com>
    
    * spark: Used a mocked SparkContext instead inside SparkProcessingEngineFacetBuilderTest
    
    Signed-off-by: Damien Hawes <d-m-h@users.noreply.github.com>
    
    * changelog: Updated the changelog indicating that the SparkVersionFacet will be removed in 1.4.0
    
    Signed-off-by: Damien Hawes <d-m-h@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Damien Hawes <d-m-h@users.noreply.github.com>

---
+++ integration/spark/shared/src/test/java/io/openlineage/spark/agent/facets/builder/SparkProcessingEngineFacetBuilderTest.java
@@ -0,0 +1,82 @@
+/*
+/* Copyright 2018-2022 contributors to the OpenLineage project
+/* SPDX-License-Identifier: Apache-2.0
+*/
+
+package io.openlineage.spark.agent.facets.builder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.openlineage.client.OpenLineage;
+import io.openlineage.spark.agent.Versions;
+import io.openlineage.spark.api.OpenLineageContext;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import org.apache.spark.SparkContext;
+import org.apache.spark.scheduler.JobSucceeded$;
+import org.apache.spark.scheduler.SparkListenerJobEnd;
+import org.apache.spark.scheduler.SparkListenerJobStart;
+import org.apache.spark.scheduler.SparkListenerStageCompleted;
+import org.apache.spark.scheduler.SparkListenerStageSubmitted;
+import org.apache.spark.sql.execution.ui.SparkListenerSQLExecutionEnd;
+import org.apache.spark.sql.execution.ui.SparkListenerSQLExecutionStart;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import scala.collection.Seq$;
+
+class SparkProcessingEngineFacetBuilderTest {
+  private static SparkContext sparkContext;
+
+  @BeforeAll
+  public static void setupSparkContext() {
+    sparkContext = Mockito.mock(SparkContext.class);
+    Mockito.when(sparkContext.appName()).thenReturn("SparkProcessingEngineFacetBuilderTest");
+    Mockito.when(sparkContext.version()).thenReturn("3.3.0");
+  }
+
+  @Test
+  void testIsDefinedForSparkListenerEvents() {
+    SparkProcessingEngineRunFacetBuilder builder =
+        new SparkProcessingEngineRunFacetBuilder(
+            OpenLineageContext.builder()
+                .sparkContext(sparkContext)
+                .openLineage(new OpenLineage(Versions.OPEN_LINEAGE_PRODUCER_URI))
+                .build());
+    assertThat(builder.isDefinedAt(new SparkListenerSQLExecutionEnd(1, 1L))).isTrue();
+    assertThat(
+            builder.isDefinedAt(
+                new SparkListenerSQLExecutionStart(1L, "abc", "abc", "abc", null, 1L)))
+        .isTrue();
+    assertThat(
+            builder.isDefinedAt(
+                new SparkListenerJobStart(1, 1L, Seq$.MODULE$.empty(), new Properties())))
+        .isTrue();
+    assertThat(builder.isDefinedAt(new SparkListenerJobEnd(1, 1L, JobSucceeded$.MODULE$))).isTrue();
+    assertThat(builder.isDefinedAt(new SparkListenerStageSubmitted(null, new Properties())))
+        .isTrue();
+    assertThat(builder.isDefinedAt(new SparkListenerStageCompleted(null))).isTrue();
+  }
+
+  @Test
+  void testBuild() {
+    SparkProcessingEngineRunFacetBuilder builder =
+        new SparkProcessingEngineRunFacetBuilder(
+            OpenLineageContext.builder()
+                .sparkContext(sparkContext)
+                .openLineage(new OpenLineage(Versions.OPEN_LINEAGE_PRODUCER_URI))
+                .build());
+
+    Map<String, OpenLineage.RunFacet> runFacetMap = new HashMap<>();
+    builder.build(new SparkListenerSQLExecutionEnd(1, 1L), runFacetMap::put);
+    assertThat(runFacetMap)
+        .hasEntrySatisfying(
+            "processing_engine",
+            facet ->
+                assertThat(facet)
+                    .isInstanceOf(OpenLineage.ProcessingEngineRunFacet.class)
+                    .hasFieldOrPropertyWithValue("name", "spark")
+                    .hasFieldOrPropertyWithValue("version", sparkContext.version()));
+  }
+}
