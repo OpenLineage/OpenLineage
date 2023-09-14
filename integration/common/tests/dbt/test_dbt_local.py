28c35f47bdbbb334ebb8e1b2eeba580701e24f1b
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2023-09-14 16:02:44 +0100

    fix: Support parsing dbt dbt_project.yml without target-path (#2106)
    As of dbt v1.5, usage of target-path in the dbt_project.yml file has been deprecated, now preferring a CLI flag or env var. It will be removed in a future version. See dbt-labs/dbt-core#6882
    
    Docs: https://docs.getdbt.com/reference/project-configs/target-path
    
    This change allows users to run DbtLocalArtifactProcessor in dbt projects that don't declare target-path
    
    Fix: #2093
    
    Signed-off-by: Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
    Signed-off-by: Sheeri K. Cabral <me@sheeri.com>

--- integration/common/tests/dbt/test_dbt_local.py
+++ integration/common/tests/dbt/test_dbt_local.py
@@ -238,3 +238,41 @@ def test_logging_handler_does_not_warn():
     DbtLocalArtifactProcessor.load_metadata(path, [2], logger)
 
     logger.warning.assert_not_called()
+
+
+@mock.patch.dict(os.environ, {"DBT_TARGET_PATH": "target-from-envvar"}, clear=True)
+def test_build_target_path_with_user_defined():
+    processor = DbtLocalArtifactProcessor(
+        producer='https://github.com/OpenLineage/OpenLineage/tree/0.0.1/integration/dbt',
+        project_dir='tests/dbt/env_vars',
+        target='prod',
+        target_path="arg-target-name",
+        job_namespace="ol-namespace"
+    )
+    assert processor.build_target_path({}) == "arg-target-name"
+
+@mock.patch.dict(os.environ, {"DBT_TARGET_PATH": "target-from-envvar"}, clear=True)
+def test_build_target_path_with_envvar():
+    processor = DbtLocalArtifactProcessor(
+        producer='https://github.com/OpenLineage/OpenLineage/tree/0.0.1/integration/dbt',
+        project_dir='tests/dbt/env_vars',
+        target='prod',
+        job_namespace="ol-namespace"
+    )
+    assert processor.build_target_path({}) == "target-from-envvar"
+
+@pytest.mark.parametrize(
+    "test_name,dbt_project,expected",
+    [
+        ("with_dbt_project", {"target-path": "from-dbt-project"}, "from-dbt-project"),
+        ("with_default", {}, "target")
+    ]
+)
+def test_build_target_path(test_name, dbt_project, expected):
+    processor = DbtLocalArtifactProcessor(
+        producer='https://github.com/OpenLineage/OpenLineage/tree/0.0.1/integration/dbt',
+        project_dir='tests/dbt/env_vars',
+        target='prod',
+        job_namespace="ol-namespace"
+    )
+    assert processor.build_target_path(dbt_project) == expected
