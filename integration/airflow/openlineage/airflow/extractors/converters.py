414ce9c33faddfe31f395dc72738a7cecdc1583a
Erik Alfthan <erik.alfthan@kommuninvest.se>
2023-09-28 11:17:27 +0200

    #2130 Format import - revert black formatting on untouched function
    Signed-off-by: Erik Alfthan <erik.alfthan@kommuninvest.se>

--- integration/airflow/openlineage/airflow/extractors/converters.py
+++ integration/airflow/openlineage/airflow/extractors/converters.py
@@ -4,27 +4,35 @@
 from typing import Optional, Tuple, cast
 from urllib.parse import urlparse
 
-from openlineage.client.run import Dataset
-
 from airflow.lineage.entities import File, Table
-
 from openlineage.client.facet import SchemaDatasetFacet, SchemaField
+from openlineage.client.run import Dataset
 
 
 def convert_from_object_storage_uri(uri: str) -> Optional[Dataset]:
     try:
         scheme, netloc, path, params, _, _ = cast(
-            Tuple[str, str, str, str, str, str], urlparse(uri)
+            Tuple[str, str, str, str, str, str],
+            urlparse(uri)
         )
     except Exception:
         return None
     if scheme.startswith("s3"):
-        return Dataset(namespace=f"s3://{netloc}", name=path)
+        return Dataset(
+            namespace=f"s3://{netloc}",
+            name=path
+        )
     elif scheme.startswith(("gcs", "gs")):
-        return Dataset(namespace=f"gs://{netloc}", name=path)
+        return Dataset(
+            namespace=f"gs://{netloc}",
+            name=path
+        )
     elif "/" not in uri:
         return None
-    return Dataset(namespace=scheme, name=f"/{netloc}{path}")
+    return Dataset(
+        namespace=scheme,
+        name=f"/{netloc}{path}"
+    )
 
 
 def convert_to_dataset(obj):
