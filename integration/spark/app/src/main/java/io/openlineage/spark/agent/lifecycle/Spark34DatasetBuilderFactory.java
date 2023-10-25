024c9484681262747ef645c767fa96302fd71079
pawel.leszczynski <leszczynski.pawel@gmail.com>
2023-10-25 09:34:19 +0200

    [SPARK] support databricks 13.3. (#2185)
    Signed-off-by: Pawel Leszczynski <leszczynski.pawel@gmail.com>

--- integration/spark/app/src/main/java/io/openlineage/spark/agent/lifecycle/Spark34DatasetBuilderFactory.java
+++ integration/spark/app/src/main/java/io/openlineage/spark/agent/lifecycle/Spark34DatasetBuilderFactory.java
@@ -6,28 +6,61 @@
 package io.openlineage.spark.agent.lifecycle;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
 import io.openlineage.client.OpenLineage;
+import io.openlineage.client.OpenLineage.InputDataset;
+import io.openlineage.spark.agent.lifecycle.plan.CommandPlanVisitor;
 import io.openlineage.spark.agent.lifecycle.plan.SaveIntoDataSourceCommandVisitor;
 import io.openlineage.spark.agent.util.DeltaUtils;
+import io.openlineage.spark.api.AbstractQueryPlanOutputDatasetBuilder;
 import io.openlineage.spark.api.DatasetFactory;
 import io.openlineage.spark.api.OpenLineageContext;
 import io.openlineage.spark3.agent.lifecycle.plan.AppendDataDatasetBuilder;
+import io.openlineage.spark3.agent.lifecycle.plan.DataSourceV2RelationInputDatasetBuilder;
 import io.openlineage.spark3.agent.lifecycle.plan.DataSourceV2RelationOutputDatasetBuilder;
+import io.openlineage.spark3.agent.lifecycle.plan.DataSourceV2ScanRelationInputDatasetBuilder;
+import io.openlineage.spark3.agent.lifecycle.plan.InMemoryRelationInputDatasetBuilder;
 import io.openlineage.spark3.agent.lifecycle.plan.LogicalRelationDatasetBuilder;
+import io.openlineage.spark3.agent.lifecycle.plan.MergeIntoCommandInputDatasetBuilder;
 import io.openlineage.spark3.agent.lifecycle.plan.MergeIntoCommandOutputDatasetBuilder;
+import io.openlineage.spark3.agent.lifecycle.plan.SubqueryAliasInputDatasetBuilder;
 import io.openlineage.spark3.agent.lifecycle.plan.SubqueryAliasOutputDatasetBuilder;
 import io.openlineage.spark3.agent.lifecycle.plan.TableContentChangeDatasetBuilder;
 import io.openlineage.spark32.agent.lifecycle.plan.AlterTableCommandDatasetBuilder;
-import io.openlineage.spark33.agent.lifecycle.plan.CreateReplaceDatasetBuilder;
 import io.openlineage.spark33.agent.lifecycle.plan.ReplaceIcebergDataDatasetBuilder;
+import io.openlineage.spark34.agent.lifecycle.plan.column.CreateReplaceInputDatasetBuilder;
+import io.openlineage.spark35.agent.lifecycle.plan.CreateReplaceOutputDatasetBuilder;
 import java.util.Collection;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.reflect.MethodUtils;
+import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan;
 import scala.PartialFunction;
 
 @Slf4j
 public class Spark34DatasetBuilderFactory extends Spark32DatasetBuilderFactory
     implements DatasetBuilderFactory {
+
+  @Override
+  public Collection<PartialFunction<Object, List<OpenLineage.InputDataset>>> getInputBuilders(
+      OpenLineageContext context) {
+    DatasetFactory<OpenLineage.InputDataset> datasetFactory = DatasetFactory.input(context);
+    Builder builder =
+        ImmutableList.<PartialFunction<Object, List<InputDataset>>>builder()
+            .add(new LogicalRelationDatasetBuilder(context, datasetFactory, true))
+            .add(new InMemoryRelationInputDatasetBuilder(context))
+            .add(new CommandPlanVisitor(context))
+            .add(new DataSourceV2ScanRelationInputDatasetBuilder(context, datasetFactory))
+            .add(new SubqueryAliasInputDatasetBuilder(context))
+            .add(new CreateReplaceInputDatasetBuilder(context))
+            .add(new DataSourceV2RelationInputDatasetBuilder(context, datasetFactory));
+
+    if (DeltaUtils.hasMergeIntoCommandClass()) {
+      builder.add(new MergeIntoCommandInputDatasetBuilder(context));
+    }
+
+    return builder.build();
+  }
 
   @Override
   public Collection<PartialFunction<Object, List<OpenLineage.OutputDataset>>> getOutputBuilders(
@@ -40,7 +73,7 @@ public class Spark34DatasetBuilderFactory extends Spark32DatasetBuilderFactory
             .add(new AppendDataDatasetBuilder(context, datasetFactory))
             .add(new DataSourceV2RelationOutputDatasetBuilder(context, datasetFactory))
             .add(new TableContentChangeDatasetBuilder(context))
-            .add(new CreateReplaceDatasetBuilder(context))
+            .add(getCreateReplaceDatasetBuilder(context))
             .add(new SubqueryAliasOutputDatasetBuilder(context))
             .add(new AlterTableCommandDatasetBuilder(context));
 
@@ -53,5 +86,28 @@ public class Spark34DatasetBuilderFactory extends Spark32DatasetBuilderFactory
     }
 
     return builder.build();
+  }
+
+  private AbstractQueryPlanOutputDatasetBuilder<LogicalPlan> getCreateReplaceDatasetBuilder(
+      OpenLineageContext context) {
+    // On Databricks platform Spark 3.4 needs to have the version of `CreateReplaceDatasetBuilder`
+    // from Spark 3.5. Distinction has to be done by sub object fields
+    String tableSpecClass = null;
+    try {
+      tableSpecClass =
+          MethodUtils.getAccessibleMethod(
+                  Class.forName("org.apache.spark.sql.catalyst.plans.logical.ReplaceTable"),
+                  "tableSpec")
+              .getReturnType()
+              .getCanonicalName();
+    } catch (ClassNotFoundException e) {
+      log.error("Could not find `ReplaceTable` class", e);
+    }
+
+    if (tableSpecClass.endsWith("TableSpecBase")) {
+      return new CreateReplaceOutputDatasetBuilder(context);
+    } else {
+      return new io.openlineage.spark33.agent.lifecycle.plan.CreateReplaceDatasetBuilder(context);
+    }
   }
 }
