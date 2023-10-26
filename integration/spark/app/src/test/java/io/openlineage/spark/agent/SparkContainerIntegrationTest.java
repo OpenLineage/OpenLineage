48bcdb7439aafa49b1c6326c9e4b3e2214fc9bd5
savan navalgi <savan.navalgi@gmail.com>
2023-10-26 16:40:46 +0530

    indentation
    Signed-off-by: savan navalgi <savan.navalgi@gmail.com>

--- integration/spark/app/src/test/java/io/openlineage/spark/agent/SparkContainerIntegrationTest.java
+++ integration/spark/app/src/test/java/io/openlineage/spark/agent/SparkContainerIntegrationTest.java
@@ -266,7 +266,10 @@ class SparkContainerIntegrationTest {
   void testDropTable() {
     SparkContainerUtils.runPysparkContainerWithDefaultConf(
         network, openLineageClientMockContainer, "testDropTable", "spark_drop_table.py");
-    verifyEvents(mockServerClient,"pysparkDropTableStartEvent.json","pysparkDropTableCompleteEvent.json");
+    verifyEvents(
+            mockServerClient,
+            "pysparkDropTableStartEvent.json",
+            "pysparkDropTableCompleteEvent.json");
   }
 
   @Test
