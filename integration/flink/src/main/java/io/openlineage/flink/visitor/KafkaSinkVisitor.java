24fe87979e8ddadc82f49054835a9510049e652e
Pawel Leszczynski <leszczynski.pawel@gmail.com>
2023-12-15 09:25:06 +0100

    [FLINK] stick to dataset naming convention for kafka topics
    Signed-off-by: Pawel Leszczynski <leszczynski.pawel@gmail.com>

--- integration/flink/src/main/java/io/openlineage/flink/visitor/KafkaSinkVisitor.java
+++ integration/flink/src/main/java/io/openlineage/flink/visitor/KafkaSinkVisitor.java
@@ -6,12 +6,13 @@
 package io.openlineage.flink.visitor;
 
 import io.openlineage.client.OpenLineage;
+import io.openlineage.client.utils.DatasetIdentifier;
 import io.openlineage.flink.api.OpenLineageContext;
 import io.openlineage.flink.utils.AvroSchemaUtils;
+import io.openlineage.flink.utils.KafkaUtils;
 import io.openlineage.flink.visitor.wrapper.KafkaSinkWrapper;
 import java.util.Collections;
 import java.util.List;
-import java.util.Properties;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
@@ -32,9 +33,8 @@ public class KafkaSinkVisitor extends Visitor<OpenLineage.OutputDataset> {
   public List<OpenLineage.OutputDataset> apply(Object kafkaSink) {
     KafkaSinkWrapper wrapper = KafkaSinkWrapper.of((KafkaSink) kafkaSink);
     try {
-      Properties properties = wrapper.getKafkaProducerConfig();
-      String bootstrapServers = properties.getProperty("bootstrap.servers");
-      String topic = wrapper.getKafkaTopic();
+      DatasetIdentifier di =
+          KafkaUtils.datasetIdentifierOf(wrapper.getKafkaProducerConfig(), wrapper.getKafkaTopic());
 
       OpenLineage.DatasetFacetsBuilder datasetFacetsBuilder =
           outputDataset().getDatasetFacetsBuilder();
@@ -46,10 +46,9 @@ public class KafkaSinkVisitor extends Visitor<OpenLineage.OutputDataset> {
                   datasetFacetsBuilder.schema(
                       AvroSchemaUtils.convert(context.getOpenLineage(), schema)));
 
-      log.debug("Kafka output topic: {}", topic);
+      log.debug("Kafka output topic: {}", di.getName());
 
-      return Collections.singletonList(
-          outputDataset().getDataset(topic, bootstrapServers, datasetFacetsBuilder));
+      return Collections.singletonList(outputDataset().getDataset(di, datasetFacetsBuilder));
     } catch (IllegalAccessException e) {
       log.error("Can't access the field. ", e);
     }
