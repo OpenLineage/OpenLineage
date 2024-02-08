7f1e02bcf9baf4ce8ccc753e11576e848255bff0
Damien Hawes <d-m-h@users.noreply.github.com>
2024-02-08 13:19:00 +0100

    Refactor the CommonConfig Gradle plugin
    - Refactored CommonConfig plugin to align with Gradle best practices by reacting to plugin applications rather than applying plugins directly, enhancing modularity and reducing coupling. Adjusted build scripts to explicitly apply necessary plugins (Java library, PMD, Lombok, Spotless) ensuring compatibility and maintaining build integrity.
    
    Signed-off-by: Damien Hawes <d-m-h@users.noreply.github.com>

--- integration/spark/buildSrc/build.gradle.kts
+++ integration/spark/buildSrc/build.gradle.kts
@@ -10,10 +10,12 @@ repositories {
 
 val downloadTaskVersion: String = "5.5.0"
 val lombokPluginVersion: String = "8.4"
+val shadowPluginVersion: String = "8.1.1"
 val spotlessVersion: String = "6.13.0"
 
 dependencies {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:${spotlessVersion}")
+    implementation("com.github.johnrengelman:shadow:${shadowPluginVersion}")
     implementation("de.undercouch:gradle-download-task:${downloadTaskVersion}")
     implementation("io.freefair.gradle:lombok-plugin:${lombokPluginVersion}")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
