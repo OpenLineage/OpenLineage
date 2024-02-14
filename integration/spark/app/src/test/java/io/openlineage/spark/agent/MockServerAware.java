cc4df974e3cd8c791dc833aabf4b884673759a2d
Damien Hawes <d-m-h@users.noreply.github.com>
2024-02-14 21:43:03 +0100

    Add MockServer's junit jupiter dependency in order to gain access to the MockServerExtension
    Signed-off-by: Damien Hawes <d-m-h@users.noreply.github.com>

--- integration/spark/app/src/test/java/io/openlineage/spark/agent/MockServerAware.java
+++
@@ -1,7 +0,0 @@
-package io.openlineage.spark.agent;
-
-import org.mockserver.integration.ClientAndServer;
-
-interface MockServerAware {
-  void setClientAndServer(ClientAndServer clientAndServer);
-}
