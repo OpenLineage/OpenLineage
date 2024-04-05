d682fe4fec217c8552a2c9df4b0f82788a0591f5
Fabio Manganiello <fabio.manganiello@booking.com>
2024-04-05 13:30:16 +0200

    [Airflow] Fixed format returned by `airflow.macros.lineage_parent_id`. (#2578)
    * [#2488] Fixed format returned by `airflow.macros.lineage_parent_id`.
    
    - Returned format: `<namespace>/<name>/<run_id>`.
    
    - `name` should be `<dag_id>.<task_id>`, not a UUID.
    
    - `run_id` should be a UUID, not `<run_timestamp>.<try_number>`.
    
    - Both `lineage_run_id` and `lineage_parent_id` should expose the same
      interface - only a `TaskInstance` object is now required as argument.
    
    - Import `_DAG_NAMESPACE` instead of inferring it again.
    
    Airflow reference: https://github.com/apache/airflow/pull/37877
    
    Signed-off-by: Fabio Manganiello <fabio@manganiello.tech>
    
    * Fixed redefinition of `get_unknown_source_attribute_run_facet` introduced upon merge.
    
    Signed-off-by: Fabio Manganiello <fabio@manganiello.tech>
    
    * Addressed https://github.com/OpenLineage/OpenLineage/pull/2578#discussion_r1553171546
    
    Signed-off-by: Fabio Manganiello <fabio.manganiello@booking.com>
    
    * Fixed failing macro test
    
    Signed-off-by: Fabio Manganiello <fabio.manganiello@booking.com>
    
    ---------
    
    Signed-off-by: Fabio Manganiello <fabio@manganiello.tech>
    Signed-off-by: Fabio Manganiello <fabio.manganiello@booking.com>

--- integration/airflow/tests/test_macros.py
+++ integration/airflow/tests/test_macros.py
@@ -10,33 +10,39 @@ from openlineage.airflow.macros import lineage_parent_id, lineage_run_id
 def test_lineage_run_id():
     task_instance = mock.MagicMock(
         dag_id="dag_id",
+        task=mock.MagicMock(task_id="task_id"),
         execution_date="execution_date",
         try_number=1,
     )
-    task = mock.MagicMock(task_id="task_id")
-    actual = lineage_run_id(task=task, task_instance=task_instance)
+
+    actual = lineage_run_id(task_instance)
     expected = str(
         uuid.uuid3(
             uuid.NAMESPACE_URL,
             f"{_DAG_NAMESPACE}.dag_id.task_id.execution_date.1",
         )
     )
+
     assert actual == expected
 
 
 def test_lineage_parent_id():
     task_instance = mock.MagicMock(
         dag_id="dag_id",
+        task=mock.MagicMock(task_id="task_id"),
+        task_id="task_id",
         execution_date="execution_date",
         try_number=1,
     )
-    task = mock.MagicMock(task_id="task_id")
-    actual = lineage_parent_id(run_id="run_id", task_instance=task_instance, task=task)
-    job_name = str(
+
+    actual = lineage_parent_id(task_instance)
+    job_name = f"{task_instance.dag_id}.{task_instance.task.task_id}"
+    run_id = str(
         uuid.uuid3(
             uuid.NAMESPACE_URL,
             f"{_DAG_NAMESPACE}.dag_id.task_id.execution_date.1",
         )
     )
-    expected = f"{_DAG_NAMESPACE}/{job_name}/run_id"
+
+    expected = f"{_DAG_NAMESPACE}/{job_name}/{run_id}"
     assert actual == expected
