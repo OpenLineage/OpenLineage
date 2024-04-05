aa0cc282eb82cee979d11a5edba143523c548b05
Martynov Maxim <martinov_m_s_@mail.ru>
2024-04-05 15:46:57 +0300

    Airflow: Add lineage_job_namespace and lineage_job_name macros
    Signed-off-by: Martynov Maxim <martinov_m_s_@mail.ru>

--- integration/airflow/tests/test_macros.py
+++ integration/airflow/tests/test_macros.py
@@ -4,13 +4,32 @@ import uuid
 from unittest import mock
 
 from openlineage.airflow.adapter import _DAG_NAMESPACE
-from openlineage.airflow.macros import lineage_parent_id, lineage_run_id
+from openlineage.airflow.macros import (
+    lineage_job_name,
+    lineage_job_namespace,
+    lineage_parent_id,
+    lineage_run_id,
+)
+
+
+def test_lineage_job_namespace():
+    assert lineage_job_namespace() == _DAG_NAMESPACE
+
+
+def test_lineage_job_name():
+    task_instance = mock.MagicMock(
+        dag_id="dag_id",
+        task_id="task_id",
+        execution_date="execution_date",
+        try_number=1,
+    )
+    assert lineage_job_name(task_instance) == "dag_id.task_id"
 
 
 def test_lineage_run_id():
     task_instance = mock.MagicMock(
         dag_id="dag_id",
-        task=mock.MagicMock(task_id="task_id"),
+        task_id="task_id",
         execution_date="execution_date",
         try_number=1,
     )
@@ -29,14 +48,13 @@ def test_lineage_run_id():
 def test_lineage_parent_id():
     task_instance = mock.MagicMock(
         dag_id="dag_id",
-        task=mock.MagicMock(task_id="task_id"),
         task_id="task_id",
         execution_date="execution_date",
         try_number=1,
     )
 
     actual = lineage_parent_id(task_instance)
-    job_name = f"{task_instance.dag_id}.{task_instance.task.task_id}"
+    job_name = "dag_id.task_id"
     run_id = str(
         uuid.uuid3(
             uuid.NAMESPACE_URL,
