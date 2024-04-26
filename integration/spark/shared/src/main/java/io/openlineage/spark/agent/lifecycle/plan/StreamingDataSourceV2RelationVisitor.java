9032bd8cbd077955ed388a81d4da6cfbb5d63d2d
Damien Hawes <d-m-h@users.noreply.github.com>
2024-04-26 13:44:24 +0200

    Implement a first pass for extracting Kafka based inputs in a streaming configuration
    Signed-off-by: Damien Hawes <d-m-h@users.noreply.github.com>

---
+++ integration/spark/shared/src/main/java/io/openlineage/spark/agent/lifecycle/plan/StreamingDataSourceV2RelationVisitor.java
@@ -0,0 +1,63 @@
+/*
+/* Copyright 2018-2024 contributors to the OpenLineage project
+/* SPDX-License-Identifier: Apache-2.0
+*/
+
+package io.openlineage.spark.agent.lifecycle.plan;
+
+import io.openlineage.client.OpenLineage.InputDataset;
+import io.openlineage.spark.api.OpenLineageContext;
+import io.openlineage.spark.api.QueryPlanVisitor;
+import java.util.List;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan;
+import org.apache.spark.sql.execution.datasources.v2.StreamingDataSourceV2Relation;
+
+@Slf4j
+public class StreamingDataSourceV2RelationVisitor
+    extends QueryPlanVisitor<StreamingDataSourceV2Relation, InputDataset> {
+
+  public StreamingDataSourceV2RelationVisitor(@NonNull OpenLineageContext context) {
+    super(context);
+  }
+
+  @Override
+  public List<InputDataset> apply(LogicalPlan x) {
+    log.info(
+        "Applying {} to a logical plan with type {}",
+        this.getClass().getSimpleName(),
+        x.getClass().getCanonicalName());
+    final StreamingDataSourceV2Relation relation = (StreamingDataSourceV2Relation) x;
+    final StreamStrategy streamStrategy = selectStrategy(relation);
+    return streamStrategy.getInputDatasets();
+  }
+
+  @Override
+  public boolean isDefinedAt(LogicalPlan x) {
+    boolean result = x instanceof StreamingDataSourceV2Relation;
+    if (log.isDebugEnabled()) {
+      log.debug(
+          "The result of checking whether {} is an instance of {} is {}",
+          x.getClass().getCanonicalName(),
+          StreamingDataSourceV2Relation.class.getCanonicalName(),
+          result);
+    }
+    return result;
+  }
+
+  private StreamStrategy selectStrategy(StreamingDataSourceV2Relation relation) {
+    StreamStrategy streamStrategy;
+    Class<?> streamClass = relation.stream().getClass();
+    String streamClassName = streamClass.getCanonicalName();
+    if ("org.apache.spark.sql.kafka010.KafkaMicroBatchStream".equals(streamClassName)) {
+      streamStrategy = new KafkaMicroBatchStreamStrategy(inputDataset(), relation);
+    } else {
+      log.warn("The {} has been selected because no rules have matched", NoOpStreamStrategy.class);
+      streamStrategy = new NoOpStreamStrategy(inputDataset(), relation);
+    }
+
+    log.info("Selected this strategy: {}", streamStrategy.getClass().getSimpleName());
+    return streamStrategy;
+  }
+}
---
+++ integration/spark/shared/src/main/java/io/openlineage/spark/agent/lifecycle/plan/TopicPartitionProxy.java
@@ -0,0 +1,84 @@
+/*
+/* Copyright 2018-2024 contributors to the OpenLineage project
+/* SPDX-License-Identifier: Apache-2.0
+*/
+
+package io.openlineage.spark.agent.lifecycle.plan;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
+import javax.annotation.Nullable;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.reflect.MethodUtils;
+
+/**
+ * A proxy class for TopicPartition in Kafka. This class is used to interact with the TopicPartition
+ * object without directly depending on the Kafka library.
+ */
+@Slf4j
+final class TopicPartitionProxy {
+  private static final String EXPECTED_CLASS_NAME = "org.apache.kafka.common.TopicPartition";
+  private final Object topicPartition;
+
+  /**
+   * Constructs a new TopicPartitionProxy.
+   *
+   * @param topicPartition the TopicPartition object from Kafka library
+   * @throws IllegalArgumentException if the provided object is null or not an instance of
+   *     TopicPartition
+   */
+  public TopicPartitionProxy(Object topicPartition) {
+    if (topicPartition == null) {
+      throw new IllegalArgumentException("constructor argument cannot be null");
+    }
+
+    if (!EXPECTED_CLASS_NAME.equals(topicPartition.getClass().getCanonicalName())) {
+      throw new IllegalArgumentException(
+          "constructor argument must be of type" + EXPECTED_CLASS_NAME);
+    }
+
+    this.topicPartition = topicPartition;
+  }
+
+  /**
+   * Retrieves the topic name from the TopicPartition object.
+   *
+   * @return an Optional containing the topic name, or an empty Optional if the method invocation
+   *     fails
+   */
+  public Optional<String> topic() {
+    return Optional.ofNullable(tryInvokeMethod("topic"));
+  }
+
+  /**
+   * Retrieves the partition number from the TopicPartition object.
+   *
+   * @return an Optional containing the partition number, or an empty Optional if the method
+   *     invocation fails
+   */
+  public Optional<Integer> partition() {
+    return Optional.ofNullable(tryInvokeMethod("partition"));
+  }
+
+  /**
+   * Tries to invoke a method on the TopicPartition object.
+   *
+   * @param methodName the name of the method to invoke
+   * @return the result of the method invocation, or null if the invocation fails
+   */
+  @Nullable
+  private <T> T tryInvokeMethod(String methodName) {
+    T result;
+    try {
+      result = (T) MethodUtils.invokeMethod(topicPartition, methodName);
+    } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+      log.error(
+          "Method '{}' could not be invoked on object with type '{}'",
+          methodName,
+          topicPartition.getClass().getCanonicalName(),
+          e);
+      result = null;
+    }
+    return result;
+  }
+}
