f0c9e89606271571d9a1c8f55537affb5b038b1c
Jakub Dardzinski <kuba0221@gmail.com>
2024-05-21 23:42:26 +0200

    Suppress warning on importing v1 module in __init__.py.
    Signed-off-by: Jakub Dardzinski <kuba0221@gmail.com>

--- client/python/openlineage/client/__init__.py
+++ client/python/openlineage/client/__init__.py
@@ -2,7 +2,12 @@
 # SPDX-License-Identifier: Apache-2.0
 from __future__ import annotations
 
+import warnings
+
 from openlineage.client.client import OpenLineageClient, OpenLineageClientOptions
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", DeprecationWarning)
 from openlineage.client.facet import set_producer as set_producer_v1
 from openlineage.client.facet_v2 import set_producer as set_producer_v2
 
