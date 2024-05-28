6b5e168312472ec11978563fbc7a9eb71f480759
Martynov Maxim <martinov_m_s_@mail.ru>
2024-05-28 17:30:46 +0300

    Java: Remove deprecated localServerId option from Kafka config
    Signed-off-by: Martynov Maxim <martinov_m_s_@mail.ru>

--- proxy/backend/src/main/java/io/openlineage/proxy/api/models/KafkaConfig.java
+++ proxy/backend/src/main/java/io/openlineage/proxy/api/models/KafkaConfig.java
@@ -19,26 +19,4 @@ public final class KafkaConfig implements ProxyStreamConfig {
   @Getter @Setter private String messageKey;
   @Getter @Setter private String bootstrapServerUrl;
   @Getter @Setter private Properties properties;
-
-  /**
-   * @deprecated
-   *     <p>Since version 1.13.0.
-   *     <p>Will be removed in version 1.16.0.
-   *     <p>Please use {@link #getMessageKey()} instead
-   */
-  @Deprecated
-  String getLocalServerId() {
-    return messageKey;
-  }
-
-  /**
-   * @deprecated
-   *     <p>Since version 1.13.0.
-   *     <p>Will be removed in version 1.16.0.
-   *     <p>Please use {@link #setMessageKey()} instead
-   */
-  @Deprecated
-  void setLocalServerId(String localServerId) {
-    this.messageKey = localServerId;
-  }
 }
