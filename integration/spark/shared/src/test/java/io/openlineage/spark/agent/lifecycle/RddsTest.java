214f1c272911a4b1464fd5d1668990d8ec319fbe
Pawel Leszczynski <leszczynski.pawel@gmail.com>
2024-06-03 12:52:14 +0200

    [SPARK] fix NPE in column level lineage
    Signed-off-by: Pawel Leszczynski <leszczynski.pawel@gmail.com>

---
+++ integration/spark/shared/src/test/java/io/openlineage/spark/agent/lifecycle/RddsTest.java
@@ -0,0 +1,23 @@
+/*
+/* Copyright 2018-2024 contributors to the OpenLineage project
+/* SPDX-License-Identifier: Apache-2.0
+*/
+
+package io.openlineage.spark.agent.lifecycle;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.apache.spark.rdd.RDD;
+import org.junit.jupiter.api.Test;
+
+class RddsTest {
+  @Test
+  void testFindFileLikeRddsWhenDependeciesAreNull() {
+    RDD<?> rdd = mock(RDD.class);
+    when(rdd.getDependencies()).thenReturn(null);
+
+    assertThat(Rdds.findFileLikeRdds(rdd)).isEmpty();
+  }
+}
