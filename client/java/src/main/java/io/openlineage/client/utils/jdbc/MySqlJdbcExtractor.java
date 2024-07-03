d95bd7b648650a2f61f3e5464681b125c2c202cb
Martynov Maxim <martinov_m_s_@mail.ru>
2024-07-03 11:58:06 +0300

    Spark,Flink: Add MySqlJdbcExtractor
    Signed-off-by: Martynov Maxim <martinov_m_s_@mail.ru>

---
+++ client/java/src/main/java/io/openlineage/client/utils/jdbc/MySqlJdbcExtractor.java
@@ -0,0 +1,31 @@
+/*
+/* Copyright 2018-2024 contributors to the OpenLineage project
+/* SPDX-License-Identifier: Apache-2.0
+*/
+
+package io.openlineage.client.utils.jdbc;
+
+import java.net.URISyntaxException;
+import java.util.Properties;
+
+public class MySqlJdbcExtractor implements JdbcExtractor {
+  // https://dev.mysql.com/doc/connector-j/en/connector-j-reference-jdbc-url-format.html
+
+  private static final String PROTOCOL_PART = "^[\\w+:]+://";
+
+  private JdbcExtractor delegate() {
+    return new OverridingJdbcExtractor("mysql", "3306");
+  }
+
+  @Override
+  public boolean isDefinedAt(String jdbcUri) {
+    return delegate().isDefinedAt(jdbcUri);
+  }
+
+  @Override
+  public JdbcLocation extract(String rawUri, Properties properties) throws URISyntaxException {
+    // Schema could be 'mysql', 'mysql:part', 'mysql+srv:part'. Convert it to 'mysql'
+    String normalizedUri = rawUri.replaceFirst(PROTOCOL_PART, "mysql://");
+    return delegate().extract(normalizedUri, properties);
+  }
+}
