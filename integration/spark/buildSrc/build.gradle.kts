eef421d47e94cea65bc0a98e61d8ee569cab33e9
pawel.leszczynski <leszczynski.pawel@gmail.com>
2024-07-17 14:59:50 +0200

    verify major java version of compiled classes (#2859)
    Signed-off-by: Pawel Leszczynski <leszczynski.pawel@gmail.com>
    Signed-off-by: jonathan.moraes.gft <jonathan.moraes.gft@nubank.com.br>

--- integration/spark/buildSrc/build.gradle.kts
+++ integration/spark/buildSrc/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+    implementation("org.javassist:javassist:3.30.2-GA")
 }
 
 gradlePlugin {
