144334764e7080ec44063cea06ebe0169af81b36
pawelkocinski <pawel93kocinski@gmail.com>
2024-08-05 14:03:29 +0200

    Add events with raw spark sql query.
    Signed-off-by: pawelkocinski <pawel93kocinski@gmail.com>

---
+++ integration/spark/shared/src/main/java/io/openlineage/spark/agent/lifecycle/SQLQueryRecorder.java
@@ -0,0 +1,13 @@
+/*
+/* Copyright 2018-2024 contributors to the OpenLineage project
+/* SPDX-License-Identifier: Apache-2.0
+*/
+
+package io.openlineage.spark.agent.lifecycle;
+
+import java.util.Optional;
+import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan;
+
+public interface SQLQueryRecorder {
+  Optional<String> record(LogicalPlan plan);
+}
