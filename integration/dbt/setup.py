27e3f7069fefca4126406b5903e12b4e58497003
merobi-hub <merobi@gmail.com>
2024-08-21 14:54:08 -0400

    Prepare next development version 1.21.0-SNAPSHOT
    Signed-off-by: merobi-hub <merobi@gmail.com>

--- integration/dbt/setup.py
+++ integration/dbt/setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-__version__ = "1.20.2"
+__version__ = "1.21.0"
 
 requirements = [
     "tqdm>=4.62.0",
