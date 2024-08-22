583e68a315a9df8f4c72fba6f5d116e3459138c7
merobi-hub <merobi@gmail.com>
2024-08-22 10:28:34 -0400

    Prepare next development version 1.21.0-SNAPSHOT
    Signed-off-by: merobi-hub <merobi@gmail.com>

--- integration/dagster/setup.py
+++ integration/dagster/setup.py
@@ -10,7 +10,7 @@ from setuptools import find_namespace_packages, setup
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-__version__ = "1.20.4"
+__version__ = "1.21.0"
 
 DAGSTER_VERSION = "1.0.0"
 
