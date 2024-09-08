d4af91c9f874e0ec2b33d15157c273f4c123102a
Jonathan Moraes <jonathan.pinto@cs.unicid.edu.br>
2024-09-08 20:09:17 -0300

    fixing reference to helper class
    Signed-off-by: Jonathan Moraes <jonathan.pinto@cs.unicid.edu.br>

--- proxy/fluent-plugin-openlineage/test/plugin/test_parser_openlineage.rb
+++ proxy/fluent-plugin-openlineage/test/plugin/test_parser_openlineage.rb
@@ -1,5 +1,5 @@
 require_relative '../helper'
-require "lib/fluent/plugin/parser_openlineage.rb"
+require_relative '../../lib/fluent/plugin/parser_openlineage'
 
 class OpenlineageParserTest < Test::Unit::TestCase
   setup do
