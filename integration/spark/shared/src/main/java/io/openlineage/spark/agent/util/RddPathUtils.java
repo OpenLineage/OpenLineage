03af2c6176a28d30cdb529ea8da27e8f913fa950
Pahulpreet Singh <54016648+codelixir@users.noreply.github.com>
2024-10-11 18:36:29 +0530

    spark: Limit the Seq size in RddPathUtils::extract() to avoid OutOfMemoryError for large jobs (#3148)
    * Limit the Seq size in RddPathUtils::extract()
    
    Signed-off-by: Pahulpreet Singh <pahulpreets@google.com>
    
    * Add flag to limit the logs in RddPathUtils::extract()
    
    Signed-off-by: Pahulpreet Singh <pahulpreets@google.com>
    
    ---------
    
    Signed-off-by: Pahulpreet Singh <pahulpreets@google.com>

--- integration/spark/shared/src/main/java/io/openlineage/spark/agent/util/RddPathUtils.java
+++ integration/spark/shared/src/main/java/io/openlineage/spark/agent/util/RddPathUtils.java
@@ -7,6 +7,7 @@ package io.openlineage.spark.agent.util;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.reflect.FieldUtils;
@@ -115,11 +116,15 @@ public class RddPathUtils {
 
     @Override
     public Stream<Path> extract(ParallelCollectionRDD rdd) {
+      int SEQ_LIMIT = 1000;
+      AtomicBoolean loggingDone = new AtomicBoolean(false);
       try {
         Object data = FieldUtils.readField(rdd, "data", true);
         log.debug("ParallelCollectionRDD data: {}", data);
-        if (data instanceof Seq) {
-          return ScalaConversionUtils.fromSeq((Seq) data).stream()
+        if ((data instanceof Seq) && ((Seq) data).head() instanceof Tuple2) {
+          // exit if the first element is invalid
+          Seq data_slice = (Seq) ((Seq) data).slice(0, SEQ_LIMIT);
+          return ScalaConversionUtils.fromSeq(data_slice).stream()
               .map(
                   el -> {
                     Path path = null;
@@ -127,8 +132,9 @@ public class RddPathUtils {
                       // we're able to extract path
                       path = parentOf(((Tuple2) el)._1.toString());
                       log.debug("Found input {}", path);
-                    } else {
+                    } else if (!loggingDone.get()) {
                       log.warn("unable to extract Path from {}", el.getClass().getCanonicalName());
+                      loggingDone.set(true);
                     }
                     return path;
                   })
