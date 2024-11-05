56899b4715958a66a59b0792a326beff62f72b1c
Artur Owczarek <owczarek.artur@gmail.com>
2024-11-05 13:17:13 +0100

    docs: Upgrade docusaurus to 3.6 (#3219)
    Signed-off-by: Artur Owczarek <owczarek.artur@gmail.com>

--- website/plugins/home-blog-plugin.js
+++ website/plugins/home-blog-plugin.js
@@ -12,53 +12,21 @@ async function blogPluginExtended(...pluginArgs) {
      * Override the default `contentLoaded` hook to access blog posts data
      */
     contentLoaded: async function (data) {
-      const blogPosts = data.allContent['docusaurus-plugin-content-blog'].default.blogPosts;
-      // Get the 6 latest blog posts
-      const recentPosts = [...blogPosts].splice(0, 6);
-
+      // serve main page from / and /openlineage-site
       data.actions.addRoute({
         path: "/",
         exact: true,
 
         // The component to use for the "Home" page route
         component: "@site/src/pages/home.tsx",
-
-        // These are the props that will be passed to our "Home" page component
-        modules: {
-          recentPosts: recentPosts.map((post) => ({
-            content: {
-              __import: true,
-              // The markdown file for the blog post will be loaded by webpack
-              path: post.metadata.source,
-              query: {
-                truncated: true,
-              },
-            },
-          })),
-        },
       });
 
-      // serve main page from / and /openlineage-site
       data.actions.addRoute({
         path: "/openlineage-site",
         exact: true,
 
         // The component to use for the "Home" page route
         component: "@site/src/pages/home.tsx",
-
-        // These are the props that will be passed to our "Home" page component
-        modules: {
-          recentPosts: recentPosts.map((post) => ({
-            content: {
-              __import: true,
-              // The markdown file for the blog post will be loaded by webpack
-              path: post.metadata.source,
-              query: {
-                truncated: true,
-              },
-            },
-          })),
-        },
       });
 
       // Call the default overridden `contentLoaded` implementation
