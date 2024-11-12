195ecf49ea5f4404667d509176a730094f67a6d0
pawel.leszczynski <leszczynski.pawel@gmail.com>
2024-11-12 11:29:32 +0100

    Introduce InputStatisticsInputDatasetFacet (#3238)
    Signed-off-by: Pawel Leszczynski <leszczynski.pawel@gmail.com>

---
+++ client/python/openlineage/client/generated/input_statistics_input_dataset.py
@@ -0,0 +1,27 @@
+# Copyright 2018-2024 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from typing import ClassVar
+
+import attr
+from openlineage.client.generated.base import InputDatasetFacet
+
+
+@attr.define
+class InputStatisticsInputDatasetFacet(InputDatasetFacet):
+    rowCount: int | None = attr.field(default=None)  # noqa: N815
+    """The number of rows read"""
+
+    size: int | None = attr.field(default=None)
+    """The size in bytes read"""
+
+    fileCount: int | None = attr.field(default=None)  # noqa: N815
+    """The number of files read"""
+
+    _additional_skip_redact: ClassVar[list[str]] = ["rowCount", "size"]
+
+    @staticmethod
+    def _get_schema() -> str:
+        return "https://openlineage.io/spec/facets/1-0-0/InputStatisticsInputDatasetFacet.json#/$defs/InputStatisticsInputDatasetFacet"
