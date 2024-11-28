66a0b8bb45a6a5c7802dc44152e4a37b7772d5e2
pawel.leszczynski <leszczynski.pawel@gmail.com>
2024-11-28 14:43:52 +0100

    fix query recorder class loading (#3281)
    Signed-off-by: Pawel Leszczynski <leszczynski.pawel@gmail.com>

---
+++ integration/spark/app/src/test/java/io/openlineage/spark/agent/lifecycle/SparkSQLQueryParserTest.java
@@ -0,0 +1,31 @@
+/*
+/* Copyright 2018-2024 contributors to the OpenLineage project
+/* SPDX-License-Identifier: Apache-2.0
+*/
+
+package io.openlineage.spark.agent.lifecycle;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class SparkSQLQueryParserTest {
+
+  SparkSQLQueryParser parser = new SparkSQLQueryParser();
+
+  @ParameterizedTest
+  @CsvSource({
+    "2.4.8,io.openlineage.spark32.agent.lifecycle.plan.OpenLineageSQLQueryRecorder",
+    "3.0.1,io.openlineage.spark32.agent.lifecycle.plan.OpenLineageSQLQueryRecorder",
+    "3.1.0,io.openlineage.spark32.agent.lifecycle.plan.OpenLineageSQLQueryRecorder",
+    "3.2.0,io.openlineage.spark32.agent.lifecycle.plan.OpenLineageSQLQueryRecorder",
+    "3.3.0,io.openlineage.spark33.agent.lifecycle.plan.OpenLineageSQLQueryRecorder",
+    "3.4.0,io.openlineage.spark33.agent.lifecycle.plan.OpenLineageSQLQueryRecorder",
+    "3.5.0,io.openlineage.spark33.agent.lifecycle.plan.OpenLineageSQLQueryRecorder",
+    "4.0.0,io.openlineage.spark33.agent.lifecycle.plan.OpenLineageSQLQueryRecorder"
+  })
+  void testResolveParserClass(String sparkVersion, String expectedParserClass) {
+    assertThat(parser.resolveParserClass(sparkVersion)).isEqualTo(expectedParserClass);
+  }
+}
