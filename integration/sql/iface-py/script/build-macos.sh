468e31c98e98e81e716e4e19bd95e911c78986e2
Maciej Obuchowski <obuchowski.maciej@gmail.com>
2024-12-02 16:25:59 +0100

    sql: modernize build script on macos (#3279)
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>

--- integration/sql/iface-py/script/build-macos.sh
+++ integration/sql/iface-py/script/build-macos.sh
@@ -7,6 +7,37 @@
 # It's assumed that it will be run on MacOS
 set -e
 
+if [[ -d "./iface-py" ]]
+then
+  cd iface-py
+fi
+
+echo "Checking for Rust"
+if [[ ! $(command -v cargo) ]]
+then
+  echo "Installing Rust"
+  curl https://sh.rustup.rs -sSf | sh -s -- -y
+  source $HOME/.cargo/env
+fi
+
+echo "Checking for uv"
+if [[ ! $(command -v uv) ]]
+then
+  echo "Installing uv"
+  curl -LsSf https://astral.sh/uv/install.sh | sh
+  source $HOME/.local/bin/env
+fi
+
+rustup target add aarch64-apple-darwin
+rustup target add x86_64-apple-darwin
+
+echo "Installing Python 3.8"
+uv python install 3.8
+
+# Maturin is build tool that we're using. It can build python wheels based on standard Rust Cargo.toml.
+echo "Installing Maturin"
+uv sync --no-install-project
+
 # Disable incremental compilation, since it causes issues.
 export CARGO_INCREMENTAL=0
 
@@ -18,16 +49,12 @@ if [[ $RUN_TESTS = true ]]; then
 fi
 
 # Build release wheels
-if [[ -d "./iface-py" ]]
-then
-  cd iface-py
-fi
 uv tool run maturin build --target universal2-apple-darwin --out target/wheels --release --strip
 
 echo "Package build, trying to import"
 echo "Platform:"
-python -c "from distutils import util; print(util.get_platform())"
+echo "from distutils import util; print(util.get_platform())" | uv run -
 # Verify that it imports and works properly
-python -m pip install openlineage-sql --no-index --find-links target/wheels --force-reinstall
-python -c "from openlineage_sql import parse, ColumnLineage; import sys; sys.exit(len(parse([\"SELECT b.a from b\"]).column_lineage) != 1)"
+uv pip install openlineage-sql --no-index --find-links target/wheels --force-reinstall
+echo "from openlineage_sql import parse, ColumnLineage; import sys; sys.exit(len(parse([\"SELECT b.a from b\"]).column_lineage) != 1)" | uv run -
 echo "all good"
