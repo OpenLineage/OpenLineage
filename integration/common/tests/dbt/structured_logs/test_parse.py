fb1baab322e76c6bf9e0e0adba7ff2f3125f46a6
Massy Bourennani <massy.bourennani@datadoghq.com>
2024-12-10 18:56:56 +0100

    add inputs/outputs to ol events
    Signed-off-by: Massy Bourennani <massy.bourennani@datadoghq.com>

--- integration/common/tests/dbt/structured_logs/test_parse.py
+++ integration/common/tests/dbt/structured_logs/test_parse.py
@@ -8,38 +8,42 @@ from openlineage.common.test import match
 
 
 @pytest.mark.parametrize(
-    "path, logs_path, expected_ol_events_path",
+    "target, logs_path, expected_ol_events_path, manifest_path",
     [
         # successful pg run
         (
-            "tests/dbt/structured_logs/run",
-            "./tests/dbt/structured_logs/run/logs/pg_successful_run.log",
-            "./tests/dbt/structured_logs/run/logs/pg_successful_run_ol_events.json"
+            "postgres",
+            "./tests/dbt/structured_logs/postgres/run/logs/successful_run.log",
+            "./tests/dbt/structured_logs/postgres/run/results/successful_run_ol_events.json",
+            "./tests/dbt/structured_logs/postgres/run/target/manifest.json"
         ),
 
         # failed pg run. Model has SQL error in it
         (
-            "tests/dbt/structured_logs/run",
-            "./tests/dbt/structured_logs/run/logs/pg_failed_run.log",
-            "./tests/dbt/structured_logs/run/logs/pg_failed_run_ol_events.json"
+            "postgres",
+            "./tests/dbt/structured_logs/postgres/run/logs/failed_run.log",
+            "./tests/dbt/structured_logs/postgres/run/results/failed_run_ol_events.json",
+            "./tests/dbt/structured_logs/postgres/run/target/manifest.json"
         ),
 
         # successful Snowflake run
         (
-            "tests/dbt/structured_logs/run",
-            "./tests/dbt/structured_logs/run/logs/snowflake_successful_run.log",
-            "./tests/dbt/structured_logs/run/logs/snowflake_successful_run_ol_events.json"
+            "snowflake",
+            "./tests/dbt/structured_logs/snowflake/run/logs/successful_run.log",
+            "./tests/dbt/structured_logs/snowflake/run/results/successful_run_ol_events.json",
+            "./tests/dbt/structured_logs/snowflake/run/target/manifest.json"
         ),
         # failed snowflake run
         (
-                "tests/dbt/structured_logs/run",
-                "./tests/dbt/structured_logs/run/logs/snowflake_failed_run.log",
-                "./tests/dbt/structured_logs/run/logs/snowflake_failed_run_ol_events.json"
+            "snowflake",
+            "./tests/dbt/structured_logs/snowflake/run/logs/failed_run.log",
+            "./tests/dbt/structured_logs/snowflake/run/results/failed_run_ol_events.json",
+            "./tests/dbt/structured_logs/snowflake/run/target/manifest.json"
         ),
     ],
-    ids=["pg_successful_dbt_run", "pg_failed_dbt_run", "snowflake_successful_dbt_run", "snowflake_failed_dbt_run"]
+    ids=["postgres_successful_dbt_run", "postgres_failed_dbt_run", "snowflake_successful_dbt_run", "snowflake_failed_dbt_run"]
 )
-def test_parse(path, logs_path, expected_ol_events_path, monkeypatch):
+def test_parse(target, logs_path, expected_ol_events_path, manifest_path, monkeypatch):
     def dummy_run_dbt_command(self):
         return open(logs_path).readlines()
 
@@ -47,12 +51,17 @@ def test_parse(path, logs_path, expected_ol_events_path, monkeypatch):
         "openlineage.common.provider.dbt.local_structured_logging.DbtStructuredLoggingProcessor._run_dbt_command",
         dummy_run_dbt_command
     )
+    monkeypatch.setattr(
+        "openlineage.common.provider.dbt.local_structured_logging.DbtStructuredLoggingProcessor.compile_manifest_path",
+        manifest_path
+    )
 
     processor = DbtStructuredLoggingProcessor(
         producer="https://github.com/OpenLineage/OpenLineage/tree/0.0.1/integration/dbt",
         job_namespace="dbt-test-namespace",
-        project_dir=path,
-        dbt_command=f"dbt --log-format json run --profiles-dir {get_profiles_dir()}".split(" ")
+        project_dir="tests/dbt/structured_logs",
+        target=target,
+        dbt_command=f"dbt --log-format json run --profiles-dir {get_profiles_dir()} --target snowflake".split(" ")
     )
 
 
@@ -68,17 +77,19 @@ def test_parse(path, logs_path, expected_ol_events_path, monkeypatch):
 #todo test when there are errors that are triggered as well
 ############
 # fixtures
-###########
+############
 
 
 @pytest.fixture(autouse=True)
 def patch_functions(monkeypatch):
+    def do_nothing(*args, **kwargs):
+        pass
 
-    compile_manifest_path = "./tests/dbt/structured_logs/ol-compile-target/manifest.json"
     monkeypatch.setattr(
-        "openlineage.common.provider.dbt.local_structured_logging.DbtStructuredLoggingProcessor.compile_manifest_path",
-        compile_manifest_path
+        "openlineage.common.provider.dbt.local_structured_logging.DbtStructuredLoggingProcessor._compile_manifest",
+        do_nothing
     )
+
 
 # todo test the parent/child relationships for the events
 
