f2bf4f73073265351a1d9e293bd63b06e302c5e8
Massy Bourennani <massy.bourennani@datadoghq.com>
2024-12-12 13:32:12 +0100

    add utests
    Signed-off-by: Massy Bourennani <massy.bourennani@datadoghq.com>

--- integration/common/tests/dbt/structured_logs/test_structured_logs.py
+++ integration/common/tests/dbt/structured_logs/test_structured_logs.py
@@ -2,10 +2,34 @@ import pytest
 import attr
 from enum import Enum
 import json
+import yaml
+from typing import Dict
 
 from openlineage.common.provider.dbt.structured_logs import DbtStructuredLogsProcessor
 from openlineage.common.test import match
 
+from openlineage.common.provider.dbt.processor import Adapter
+
+
+###########
+# helpers
+###########
+
+def ol_event_to_dict(event) -> Dict:
+    return attr.asdict(event, value_serializer=serialize)
+
+def serialize(inst, field, value):
+    if isinstance(value, Enum):
+        return value.value
+    return value
+
+def get_profiles_dir() -> str:
+    return "./tests/dbt/structured_logs"
+
+
+##################
+# test functions
+##################
 
 @pytest.mark.parametrize(
     "target, logs_path, expected_ol_events_path, manifest_path",
@@ -58,7 +82,7 @@ def test_parse(target, logs_path, expected_ol_events_path, manifest_path, monkey
         job_namespace="dbt-test-namespace",
         project_dir="tests/dbt/structured_logs",
         target=target,
-        dbt_command=f"dbt --log-format json run --profiles-dir {get_profiles_dir()} --target snowflake".split(" ")
+        dbt_command_line=f"dbt --log-format json run --profiles-dir {get_profiles_dir()} --target snowflake".split(" ")
     )
 
     processor.manifest_path = manifest_path
@@ -73,17 +97,63 @@ def test_parse(target, logs_path, expected_ol_events_path, manifest_path, monkey
     assert match(expected=expected_ol_events, result=actual_ol_events)
 
 
-############
-# helpers
-###########
+@pytest.mark.parametrize(
+    "target, expected_adapter_type",
+    [
+        ("postgres", Adapter.POSTGRES),
+        ("snowflake", Adapter.SNOWFLAKE),
 
-def ol_event_to_dict(event):
-    return attr.asdict(event, value_serializer=serialize)
+    ],
+    ids=["postgres", "snowflake"]
+)
+def test_adapter_type(target, expected_adapter_type, monkeypatch):
+    monkeypatch.setattr(
+        "openlineage.common.provider.dbt.structured_logs.DbtStructuredLogsProcessor._run_dbt_command",
+        lambda *args, **kwargs: []
+    )
 
-def serialize(inst, field, value):
-    if isinstance(value, Enum):
-        return value.value
-    return value
+    processor = DbtStructuredLogsProcessor(
+        producer="https://github.com/OpenLineage/OpenLineage/tree/0.0.1/integration/dbt",
+        job_namespace="dbt-test-namespace",
+        project_dir="tests/dbt/structured_logs",
+        target=target,
+        dbt_command_line=f"dbt --log-format json run --profiles-dir {get_profiles_dir()} --target snowflake".split(" ")
+    )
 
-def get_profiles_dir() -> str:
-    return "./tests/dbt/structured_logs"
+    try:
+        next(processor.parse())
+    except StopIteration:
+        pass
+
+    assert processor.adapter_type == expected_adapter_type
+
+
+@pytest.mark.parametrize(
+    "target, expected_dataset_namespace",
+    [
+        ("postgres", "postgres://postgres:5432"),
+        ("snowflake", "snowflake://mysnowflakeaccount.us-east-1.aws"),
+
+    ],
+    ids=["postgres", "snowflake"]
+)
+def test_dataset_namespace(target, expected_dataset_namespace, monkeypatch):
+    monkeypatch.setattr(
+        "openlineage.common.provider.dbt.structured_logs.DbtStructuredLogsProcessor._run_dbt_command",
+        lambda *args, **kwargs: []
+    )
+
+    processor = DbtStructuredLogsProcessor(
+        producer="https://github.com/OpenLineage/OpenLineage/tree/0.0.1/integration/dbt",
+        job_namespace="dbt-test-namespace",
+        project_dir="tests/dbt/structured_logs",
+        target=target,
+        dbt_command_line=f"dbt --log-format json run --profiles-dir {get_profiles_dir()} --target snowflake".split(" ")
+    )
+
+    try:
+        next(processor.parse())
+    except StopIteration:
+        pass
+
+    assert processor.dataset_namespace == expected_dataset_namespace
