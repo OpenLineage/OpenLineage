bc7ee05137489c77e04630b2f065a4f6582d1714
merobi-hub <merobi@gmail.com>
2025-01-16 08:01:14 -0500

    Rm unused breakpoint values.
    Signed-off-by: merobi-hub <merobi@gmail.com>

--- website/src/pages/community.tsx
+++ website/src/pages/community.tsx
@@ -18,11 +18,7 @@ import { Meetups, Meetup } from "@site/static/meetups/meetupStrings";
 const theme = createTheme({
   breakpoints: {
     values: {
-      xs: 0,
       sm: 600,
-      md: 900,
-      lg: 1200,
-      xl: 1536,
     },
   },
 });
