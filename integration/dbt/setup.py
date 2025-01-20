a928415955646dc19de1138e46e60418b65c8864
Maciej Obuchowski <obuchowski.maciej@gmail.com>
2025-01-20 18:37:47 +0100

    Prepare next development version 1.28.0-SNAPSHOT
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>
    Signed-off-by: David Kershaw <dk107dk@hotmail.com>

--- integration/dbt/setup.py
+++ integration/dbt/setup.py
@@ -8,7 +8,7 @@ from setuptools import find_namespace_packages, setup
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-__version__ = "1.27.0"
+__version__ = "1.28.0"
 
 requirements = [
     "tqdm>=4.62.0",
