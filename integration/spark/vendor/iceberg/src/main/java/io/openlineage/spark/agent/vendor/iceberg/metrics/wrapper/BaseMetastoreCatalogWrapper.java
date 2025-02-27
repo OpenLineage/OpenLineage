b3d4d9a17c3ab03e8361c6a1b7a2f8b33b3f639d
pawel.leszczynski <leszczynski.pawel@gmail.com>
2025-02-27 08:33:40 +0100

    Inject metrics reporter to RESTCatalog (#3515)
    * inject metrics reporter to RESTCatalog
    
    Signed-off-by: Pawel Leszczynski <leszczynski.pawel@gmail.com>
    
    * put more comments
    
    Signed-off-by: Pawel Leszczynski <leszczynski.pawel@gmail.com>
    
    ---------
    
    Signed-off-by: Pawel Leszczynski <leszczynski.pawel@gmail.com>

---
+++ integration/spark/vendor/iceberg/src/main/java/io/openlineage/spark/agent/vendor/iceberg/metrics/wrapper/BaseMetastoreCatalogWrapper.java
@@ -0,0 +1,61 @@
+/*
+/* Copyright 2018-2025 contributors to the OpenLineage project
+/* SPDX-License-Identifier: Apache-2.0
+*/
+
+package io.openlineage.spark.agent.vendor.iceberg.metrics.wrapper;
+
+import io.openlineage.spark.agent.vendor.iceberg.metrics.OpenLineageMetricsReporter;
+import java.lang.reflect.Field;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.iceberg.BaseMetastoreCatalog;
+import org.apache.iceberg.metrics.MetricsReporter;
+
+/** Wrapper to inject MetricsReporter into RESTCatalog. Uses reflection to access private fields. */
+@Slf4j
+public class BaseMetastoreCatalogWrapper implements CatalogWrapper {
+
+  public static final String METRICS_REPORTER = "metricsReporter";
+  BaseMetastoreCatalog catalog;
+
+  public BaseMetastoreCatalogWrapper(BaseMetastoreCatalog catalog) {
+    this.catalog = catalog;
+  }
+
+  /**
+   * Get the metricsReporter field from {@link BaseMetastoreCatalog}. Returns null if the field is
+   * not found.
+   *
+   * @return MetricsReporter
+   */
+  @Override
+  public MetricsReporter getExistingReporter() {
+    Field metricsReporterField =
+        FieldUtils.getField(BaseMetastoreCatalog.class, METRICS_REPORTER, true);
+
+    if (metricsReporterField == null) {
+      log.warn("Could obtain metrics reporter: no such field");
+      return null;
+    }
+
+    try {
+      return (MetricsReporter) metricsReporterField.get(catalog);
+    } catch (IllegalAccessException e) {
+      log.warn("Could obtain metrics reporter: {}", e.getMessage());
+      return null;
+    }
+  }
+
+  /**
+   * Update the metricsReporter field. Throws IllegalAccessException if the field is not found.
+   *
+   * @param reporter OpenLineageMetricsReporter
+   */
+  @Override
+  public void updateMetricsReporter(OpenLineageMetricsReporter reporter)
+      throws IllegalAccessException {
+    Field reporterField = FieldUtils.getField(BaseMetastoreCatalog.class, METRICS_REPORTER, true);
+    reporterField.set(catalog, reporter);
+  }
+}
