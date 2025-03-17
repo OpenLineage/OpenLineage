02e1c06274f47f275c10c257c65bbc4eaa56f57d
Maciej Obuchowski <maciej.obuchowski@datadoghq.com>
2025-03-17 11:16:16 +0100

    Prepare next development version 1.31.0-SNAPSHOT
    Signed-off-by: Maciej Obuchowski <maciej.obuchowski@datadoghq.com>

--- integration/common/setup.py
+++ integration/common/setup.py
@@ -10,7 +10,7 @@ from setuptools import find_namespace_packages, setup
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-__version__ = "1.30.0"
+__version__ = "1.31.0"
 
 
 project_urls = {
