1d1d5aecb2720ebdf865cc1a54b93a4b73a06147
Dominik Dębowczyk <dominik.debowczyk@getindata.com>
2025-03-25 17:20:25 +0100

    [spark] Extract column lineage out of query string
    Signed-off-by: Dominik Dębowczyk <dominik.debowczyk@getindata.com>

--- integration/spark/spark3/src/main/java/io/openlineage/spark3/agent/utils/DataSourceV2RelationDatasetExtractor.java
+++ integration/spark/spark3/src/main/java/io/openlineage/spark3/agent/utils/DataSourceV2RelationDatasetExtractor.java
@@ -5,6 +5,8 @@
 
 package io.openlineage.spark3.agent.utils;
 
+import static io.openlineage.spark.agent.util.JdbcSparkUtils.generateSchemaFromSqlMeta;
+
 import io.openlineage.client.OpenLineage;
 import io.openlineage.client.dataset.DatasetCompositeFacetsBuilder;
 import io.openlineage.client.utils.DatasetIdentifier;
@@ -19,10 +21,12 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.sql.connector.catalog.Identifier;
 import org.apache.spark.sql.connector.catalog.TableCatalog;
 import org.apache.spark.sql.execution.datasources.v2.DataSourceV2Relation;
+import org.jetbrains.annotations.NotNull;
 
 @Slf4j
 public class DataSourceV2RelationDatasetExtractor {
@@ -38,11 +42,14 @@ public class DataSourceV2RelationDatasetExtractor {
       DataSourceV2Relation relation,
       DatasetCompositeFacetsBuilder datasetFacetsBuilder) {
     OpenLineage openLineage = context.getOpenLineage();
-    Optional<DatasetIdentifier> di = getDatasetIdentifierExtended(context, relation);
-    return di.map(
+    if (ExtensionDataSourceV2Utils.hasQueryExtensionLineage(relation)) {
+      return getQueryDatasets(datasetFactory, relation);
+    }
+    List<DatasetIdentifier> di = getDatasetIdentifierExtended(context, relation);
+    return di.stream()
+        .map(
             identifier -> {
-              if (ExtensionDataSourceV2Utils.hasExtensionLineage(relation)
-                  || ExtensionDataSourceV2Utils.hasQueryExtensionLineage(relation)) {
+              if (ExtensionDataSourceV2Utils.hasExtensionLineage(relation)) {
                 ExtensionDataSourceV2Utils.loadBuilder(openLineage, datasetFacetsBuilder, relation);
               } else {
                 TableCatalog tableCatalog = (TableCatalog) relation.catalog().get();
@@ -58,8 +65,48 @@ public class DataSourceV2RelationDatasetExtractor {
                   .schema(PlanUtils.schemaFacet(openLineage, relation.schema()))
                   .dataSource(PlanUtils.datasourceFacet(openLineage, identifier.getNamespace()));
 
-              return Collections.singletonList(
-                  datasetFactory.getDataset(identifier, datasetFacetsBuilder));
+              return datasetFactory.getDataset(identifier, datasetFacetsBuilder);
+            })
+        .collect(Collectors.toList());
+  }
+
+  private static <D extends OpenLineage.Dataset> @NotNull List<D> getQueryDatasets(
+      DatasetFactory<D> datasetFactory, DataSourceV2Relation relation) {
+    String namespace = relation.table().properties().get("openlineage.dataset.namespace");
+    String query = relation.table().properties().get("openlineage.dataset.query");
+    Optional<SqlMeta> optionalSqlMeta =
+        OpenLineageSql.parse(Collections.singletonList(query), namespace);
+    return optionalSqlMeta
+        .map(
+            sqm -> {
+              if (sqm.columnLineage().isEmpty()) {
+                int numberOfTables = sqm.inTables().size();
+                return sqm.inTables().stream()
+                    .map(
+                        dbtm -> {
+                          DatasetIdentifier di =
+                              new DatasetIdentifier(dbtm.qualifiedName(), namespace);
+
+                          if (numberOfTables > 1) {
+                            return datasetFactory.getDataset(di.getName(), di.getNamespace());
+                          }
+
+                          return datasetFactory.getDataset(
+                              di.getName(), di.getNamespace(), relation.schema());
+                        })
+                    .collect(Collectors.toList());
+              }
+              return sqm.inTables().stream()
+                  .map(
+                      dbtm -> {
+                        DatasetIdentifier di =
+                            new DatasetIdentifier(dbtm.qualifiedName(), namespace);
+                        return datasetFactory.getDataset(
+                            di.getName(),
+                            di.getNamespace(),
+                            generateSchemaFromSqlMeta(dbtm, relation.schema(), sqm));
+                      })
+                  .collect(Collectors.toList());
             })
         .orElse(Collections.emptyList());
   }
@@ -87,11 +134,11 @@ public class DataSourceV2RelationDatasetExtractor {
                     r.table().properties()));
   }
 
-  public static Optional<DatasetIdentifier> getDatasetIdentifierExtended(
+  public static List<DatasetIdentifier> getDatasetIdentifierExtended(
       OpenLineageContext context, DataSourceV2Relation relation) {
     // Check if the dataset has extension lineage
     if (ExtensionDataSourceV2Utils.hasExtensionLineage(relation)) {
-      return Optional.of(ExtensionDataSourceV2Utils.getDatasetIdentifier(relation));
+      return Collections.singletonList(ExtensionDataSourceV2Utils.getDatasetIdentifier(relation));
     }
 
     if (ExtensionDataSourceV2Utils.hasQueryExtensionLineage(relation)) {
@@ -101,23 +148,27 @@ public class DataSourceV2RelationDatasetExtractor {
               Collections.singletonList(
                   relation.table().properties().get("openlineage.dataset.query")),
               namespace);
-      return sqlMeta.flatMap(
-          meta ->
-              meta.inTables().stream()
-                  .findFirst()
-                  .map(dbtm -> new DatasetIdentifier(dbtm.qualifiedName(), namespace)));
+      return sqlMeta
+          .map(
+              meta ->
+                  meta.inTables().stream()
+                      .map(dbtm -> new DatasetIdentifier(dbtm.qualifiedName(), namespace))
+                      .collect(Collectors.toList()))
+          .orElse(Collections.emptyList());
     }
 
     // Check if the relation identifier is empty
     if (relation.identifier().isEmpty()) {
       log.warn("Couldn't find identifier for dataset in plan {}", relation);
-      return getDatasetIdentifier(context, relation);
+      return getDatasetIdentifier(context, relation)
+          .map(Collections::singletonList)
+          .orElse(Collections.emptyList());
     }
 
     // Check if the catalog is present and is an instance of TableCatalog
     if (relation.catalog().isEmpty() || !(relation.catalog().get() instanceof TableCatalog)) {
       log.warn("Couldn't find catalog for dataset in plan {}", relation);
-      return Optional.empty();
+      return Collections.emptyList();
     }
 
     Identifier identifier = relation.identifier().get();
@@ -125,12 +176,13 @@ public class DataSourceV2RelationDatasetExtractor {
     Map<String, String> tableProperties = relation.table().properties();
 
     // Get the dataset identifier
-    return PlanUtils3.getDatasetIdentifier(context, tableCatalog, identifier, tableProperties);
+    return PlanUtils3.getDatasetIdentifier(context, tableCatalog, identifier, tableProperties)
+        .map(Collections::singletonList)
+        .orElse(Collections.emptyList());
   }
 
   private static Optional<DatasetIdentifier> getDatasetIdentifierFromRelation(
       DataSourceV2Relation relation) {
-
     try {
       return (Optional.of(CatalogUtils3.getDatasetIdentifierFromRelation(relation)));
     } catch (UnsupportedCatalogException ex) {
