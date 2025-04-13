5c64162dc35c4635ebd9d8711f7d690c9d89d831
Sheeri <me@sheeri.com>
2025-04-13 10:58:07 -0400

    format change

--- website/static/talks/talkStrings.tsx
+++ website/static/talks/talkStrings.tsx
@@ -19,8 +19,7 @@ export const Talks: Talk[] = [
     description:
       "Whether you're troubleshooting data issues, planning pipeline changes, or ensuring compliance, this session will equip you with the knowledge to leverage OpenLineage for better data visibility and control. Demystify data lineage and its critical role in modern data ecosystems, explore real-world use cases that demonstrate the power of lineage tracking, and learn how following your data's breadcrumbs (Data Lineage) can transform your team's efficiency and data reliability!",
     video_url: "https://www.youtube.com/watch?v=fSL110nYCYo&list=PLgyvStszwUHj-HwMVPGQAWRwhnZSkePRA",
-    conf_url:
-      "https://movedata.airbyte.com/",
+    conf_url: "https://movedata.airbyte.com/",
   },
   {
     conf: "Airflow Summit 2024",
