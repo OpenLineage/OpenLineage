5e89df5233f43560f7bda9dd23582ff30e17154b
tnazarew <tomasz.nazarewicz@getindata.com>
2025-04-17 10:19:50 +0200

    define equals for TransformedInput to avoid duplicates (#3644)
    * define equals for TransformedInput to avoid duplicates
    
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>
    
    * update changelog
    
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>
    
    * added braces for readability
    
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>
    
    ---------
    
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>

--- integration/spark/shared/src/main/java/io/openlineage/spark/agent/lifecycle/plan/column/TransformedInput.java
+++ integration/spark/shared/src/main/java/io/openlineage/spark/agent/lifecycle/plan/column/TransformedInput.java
@@ -6,6 +6,7 @@
 package io.openlineage.spark.agent.lifecycle.plan.column;
 
 import io.openlineage.client.utils.DatasetIdentifier;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,5 +22,23 @@ public class TransformedInput {
 
   public String getName() {
     return input.getFieldName();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    TransformedInput that = (TransformedInput) o;
+    return Objects.equals(input, that.input)
+        && Objects.equals(transformationInfo, that.transformationInfo);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(input, transformationInfo);
   }
 }
