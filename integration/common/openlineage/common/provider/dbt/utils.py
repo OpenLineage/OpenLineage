bf693fd0b158a5901c7fdc45f9150926aa0b29a4
Martynov Maxim <martinov_m_s_@mail.ru>
2025-05-20 15:49:16 +0300

    DBT: Move facets from processor module
    Signed-off-by: Martynov Maxim <martinov_m_s_@mail.ru>

--- integration/common/openlineage/common/provider/dbt/utils.py
+++ integration/common/openlineage/common/provider/dbt/utils.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from typing import Dict, List, Optional
 
 from openlineage.client.event_v2 import InputDataset, Job, OutputDataset, Run, RunEvent, RunState
-from openlineage.common.provider.dbt.processor import ParentRunMetadata
+from openlineage.common.provider.dbt.facets import ParentRunMetadata
 from openlineage.common.utils import get_from_nullable_chain, parse_single_arg
 
 __version__ = "1.34.0"
