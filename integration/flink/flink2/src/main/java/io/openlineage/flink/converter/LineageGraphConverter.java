13cc7988aa594dad3100e6f012772231c11fc99c
Maxim Martynov <martinov_m_s_@mail.ru>
2025-05-30 16:11:12 +0300

    [Flink] Add processing_engine facet (#3726)
    Signed-off-by: Martynov Maxim <martinov_m_s_@mail.ru>

--- integration/flink/flink2/src/main/java/io/openlineage/flink/converter/LineageGraphConverter.java
+++ integration/flink/flink2/src/main/java/io/openlineage/flink/converter/LineageGraphConverter.java
@@ -9,8 +9,10 @@ import io.openlineage.client.OpenLineage;
 import io.openlineage.client.OpenLineage.RunEvent;
 import io.openlineage.client.OpenLineage.RunEvent.EventType;
 import io.openlineage.flink.api.OpenLineageContext;
+import io.openlineage.flink.client.Versions;
 import io.openlineage.flink.visitor.Flink2VisitorFactory;
 import java.time.ZonedDateTime;
+import org.apache.flink.runtime.util.EnvironmentInformation;
 import org.apache.flink.streaming.api.lineage.LineageGraph;
 
 public class LineageGraphConverter {
@@ -27,16 +29,28 @@ public class LineageGraphConverter {
   }
 
   public RunEvent convert(LineageGraph graph, EventType eventType) {
-    return context
-        .getOpenLineage()
+    OpenLineage openLineage = context.getOpenLineage();
+    return openLineage
         .newRunEventBuilder()
         .eventTime(ZonedDateTime.now())
         .eventType(eventType)
         .job(jobExtractor.extract(graph))
         .run(
-            context
-                .getOpenLineage()
-                .newRun(context.getRunUuid(), new OpenLineage.RunFacetsBuilder().build()))
+            openLineage
+                .newRunBuilder()
+                .runId(context.getRunUuid())
+                .facets(
+                    openLineage
+                        .newRunFacetsBuilder()
+                        .processing_engine(
+                            openLineage
+                                .newProcessingEngineRunFacetBuilder()
+                                .name("flink")
+                                .version(EnvironmentInformation.getVersion())
+                                .openlineageAdapterVersion(Versions.getVersion())
+                                .build())
+                        .build())
+                .build())
         .inputs(datasetExtractor.extractInputs(graph))
         .outputs(datasetExtractor.extractOutputs(graph))
         .build();
