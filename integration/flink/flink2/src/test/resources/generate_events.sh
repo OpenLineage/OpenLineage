df122501f97cd3c89303b59f9cd92f89e8ed2346
Martynov Maxim <martinov_m_s_@mail.ru>
2025-06-03 14:50:47 +0300

    Run prettier on .json files
    Signed-off-by: Martynov Maxim <martinov_m_s_@mail.ru>

--- integration/flink/flink2/src/test/resources/generate_events.sh
+++ integration/flink/flink2/src/test/resources/generate_events.sh
@@ -9,14 +9,14 @@ kafka-avro-console-producer \
   --topic io.openlineage.flink.kafka.input1 \
   --broker-list kafka-host:9092 \
   --property schema.registry.url=http://schema-registry:8081 \
-  --property value.schema="$(< /tmp/InputEvent.avsc)" < /tmp/events.json &&
+  --property value.schema="$(< /tmp/InputEvent.avsc)" < /tmp/events.jsonl &&
   
 # Produce Avro messages to input2
 kafka-avro-console-producer \
   --topic io.openlineage.flink.kafka.input2 \
   --broker-list kafka-host:9092 \
   --property schema.registry.url=http://schema-registry:8081 \
-  --property value.schema="$(< /tmp/InputEvent.avsc)" < /tmp/events.json &&
+  --property value.schema="$(< /tmp/InputEvent.avsc)" < /tmp/events.jsonl &&
 echo 'input1 and input2 produced'
 
 ## Produce Protobuf messages
@@ -24,5 +24,5 @@ echo 'input1 and input2 produced'
 #  --topic io.openlineage.flink.kafka.protobuf_input \
 #  --broker-list kafka-host:9092 \
 #  --property schema.registry.url=http://schema-registry:8081 \
-#  --property value.schema="$(< /tmp/InputEvent.proto)" < /tmp/events_proto.json &&
+#  --property value.schema="$(< /tmp/InputEvent.proto)" < /tmp/events_proto.jsonl &&
 #echo 'input_protobuf produced'
