92b5588ea2598d5a25159bee9425b12405fe4675
Dominik Dębowczyk <dominik.debowczyk@getindata.com>
2025-07-03 15:42:31 +0200

    [SPARK] Support additional facets
    Signed-off-by: Dominik Dębowczyk <dominik.debowczyk@getindata.com>

--- integration/spark/spark3/src/main/java/io/openlineage/spark3/agent/lifecycle/plan/catalog/CatalogHandler.java
+++ integration/spark/spark3/src/main/java/io/openlineage/spark3/agent/lifecycle/plan/catalog/CatalogHandler.java
@@ -7,13 +7,36 @@ package io.openlineage.spark3.agent.lifecycle.plan.catalog;
 
 import io.openlineage.client.OpenLineage;
 import io.openlineage.client.utils.DatasetIdentifier;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import lombok.Getter;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.connector.catalog.Identifier;
 import org.apache.spark.sql.connector.catalog.TableCatalog;
 
 public interface CatalogHandler {
+
+  @Getter
+  class CatalogWithAdditionalFacets {
+
+    private final OpenLineage.CatalogDatasetFacet catalogDatasetFacet;
+    private final Map<String, OpenLineage.DatasetFacet> additionalFacets = new HashMap<>();
+
+    private CatalogWithAdditionalFacets(OpenLineage.CatalogDatasetFacet catalogDatasetFacet) {
+      this.catalogDatasetFacet = catalogDatasetFacet;
+    }
+
+    public void addFacet(String key, OpenLineage.DatasetFacet facet) {
+      additionalFacets.put(key, facet);
+    }
+
+    public static CatalogWithAdditionalFacets of(
+        OpenLineage.CatalogDatasetFacet catalogDatasetFacet) {
+      return new CatalogWithAdditionalFacets(catalogDatasetFacet);
+    }
+  }
+
   boolean hasClasses();
 
   boolean isClass(TableCatalog tableCatalog);
@@ -29,7 +52,7 @@ public interface CatalogHandler {
     return Optional.empty();
   }
 
-  default Optional<OpenLineage.CatalogDatasetFacet> getCatalogDatasetFacet(
+  default Optional<CatalogWithAdditionalFacets> getCatalogDatasetFacet(
       TableCatalog tableCatalog, Map<String, String> properties) {
     return Optional.empty();
   }
