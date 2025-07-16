8de249eafe3081f0a1ca6a3efdda7422c0d4b917
orthoxerox <orthoxerox@gmail.com>
2025-07-16 22:16:01 +0300

    spark: added Iceberg WriteDelta and WriteIcebergDelta support for merge-on-read tables
    Signed-off-by: orthoxerox <orthoxerox@gmail.com>

--- integration/spark/spark32/src/main/java/io/openlineage/spark32/agent/lifecycle/plan/column/MergeIntoIceberg013ColumnLineageVisitor.java
+++ integration/spark/spark32/src/main/java/io/openlineage/spark32/agent/lifecycle/plan/column/MergeIntoIceberg013ColumnLineageVisitor.java
@@ -13,12 +13,22 @@ import io.openlineage.spark.api.OpenLineageContext;
 import io.openlineage.spark3.agent.lifecycle.plan.column.InputFieldsCollector;
 import io.openlineage.spark3.agent.lifecycle.plan.column.OutputFieldsCollector;
 import java.util.List;
+import java.util.Optional;
+
 import lombok.extern.slf4j.Slf4j;
+import scala.Option;
+
+import org.apache.spark.sql.catalyst.ProjectingInternalRow;
 import org.apache.spark.sql.catalyst.analysis.NamedRelation;
+import org.apache.spark.sql.catalyst.expressions.Attribute;
 import org.apache.spark.sql.catalyst.expressions.NamedExpression;
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan;
+import org.apache.spark.sql.catalyst.plans.logical.MergeRows;
 import org.apache.spark.sql.catalyst.plans.logical.Project;
+import org.apache.spark.sql.catalyst.plans.logical.RebalancePartitions;
 import org.apache.spark.sql.catalyst.plans.logical.ReplaceData;
+import org.apache.spark.sql.catalyst.plans.logical.WriteDelta;
+import org.apache.spark.sql.catalyst.util.WriteDeltaProjections;
 
 @Slf4j
 public class MergeIntoIceberg013ColumnLineageVisitor implements ColumnLevelLineageVisitor {
@@ -29,7 +39,8 @@ public class MergeIntoIceberg013ColumnLineageVisitor implements ColumnLevelLinea
   }
 
   public static boolean hasClasses() {
-    return ReflectionUtils.hasClass("org.apache.spark.sql.catalyst.plans.logical.ReplaceData");
+    return ReflectionUtils.hasClass("org.apache.spark.sql.catalyst.plans.logical.ReplaceData")
+        && ReflectionUtils.hasClass("org.apache.spark.sql.catalyst.plans.logical.WriteDelta");
   }
 
   @Override
@@ -38,12 +49,19 @@ public class MergeIntoIceberg013ColumnLineageVisitor implements ColumnLevelLinea
       InputFieldsCollector.collect(context, ((ReplaceData) node).child());
       InputFieldsCollector.collect(context, (LogicalPlan) ((ReplaceData) node).table());
     }
+    if (node instanceof WriteDelta) {
+      InputFieldsCollector.collect(context, ((WriteDelta) node).child());
+      InputFieldsCollector.collect(context, (LogicalPlan) ((WriteDelta) node).table());
+    }
   }
 
   @Override
   public void collectOutputs(ColumnLevelLineageContext context, LogicalPlan node) {
     if (node instanceof ReplaceData) {
       OutputFieldsCollector.collect(context, (LogicalPlan) ((ReplaceData) node).table());
+    }
+    if (node instanceof WriteDelta) {
+      OutputFieldsCollector.collect(context, (LogicalPlan) ((WriteDelta) node).table());
     }
   }
 
@@ -52,18 +70,17 @@ public class MergeIntoIceberg013ColumnLineageVisitor implements ColumnLevelLinea
     if (node instanceof ReplaceData) {
       ReplaceData replaceIcebergData = (ReplaceData) node;
       NamedRelation namedRelation = replaceIcebergData.table();
-
       LogicalPlan query = replaceIcebergData.query();
-
+      
       if (query instanceof Project) {
         List<NamedExpression> queryOutputs =
             ScalaConversionUtils.<NamedExpression>fromSeq(((Project) query).projectList());
-
+    
         if (queryOutputs.size() != ((LogicalPlan) namedRelation).output().size()) {
           log.warn("Project and table output sizes do not match");
           return;
         }
-
+  
         for (int i = 0; i < queryOutputs.size(); i++) {
           context
               .getBuilder()
@@ -73,5 +90,39 @@ public class MergeIntoIceberg013ColumnLineageVisitor implements ColumnLevelLinea
         }
       }
     }
+
+    if (node instanceof WriteDelta) {
+      WriteDelta writeIcebergDelta = (WriteDelta) node;
+      NamedRelation namedRelation = writeIcebergDelta.table();
+      LogicalPlan query = writeIcebergDelta.query();
+
+      log.debug("Query is a {}", query);
+
+      if (query instanceof MergeRows || 
+          query instanceof RebalancePartitions && ((RebalancePartitions)query).child() instanceof MergeRows) {
+        List<Attribute> inputs =
+            ScalaConversionUtils.fromSeq(query.output());
+        List<Attribute> outputs =
+            ScalaConversionUtils.fromSeq(((LogicalPlan) namedRelation).output());
+
+        for (int i = 0; i < outputs.size(); i++) {
+          log.debug("Adding dependency: {}, {}",outputs.get(i).exprId(), inputs.get(i+1).exprId());
+          context
+              .getBuilder()
+              .addDependency(
+                outputs.get(i).exprId(),
+                inputs.get(i+1).exprId());
+        }
+      }
+
+      // RebalancePartitions rp = (RebalancePartitions) writeIcebergDelta.query() ;
+      // log.debug("RP output {}", rp.output());
+
+      // MergeRows mr = (MergeRows) rp.child();
+      // log.debug("MR output {}", mr.output());
+
+      // log.debug("Projections: {}", writeIcebergDelta.projections());
+    }
+
   }
 }
