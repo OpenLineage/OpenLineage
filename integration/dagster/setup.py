b96613d2d18ec72d734601fc1e1704b292b62430
Maciej Obuchowski <maciej.obuchowski@datadoghq.com>
2025-07-22 19:05:08 +0200

    Prepare next development version 1.37.0-SNAPSHOT
    Signed-off-by: Maciej Obuchowski <maciej.obuchowski@datadoghq.com>

--- integration/dagster/setup.py
+++ integration/dagster/setup.py
@@ -10,7 +10,7 @@ from setuptools import find_namespace_packages, setup
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-__version__ = "1.36.0"
+__version__ = "1.37.0"
 
 DAGSTER_VERSION = "1.0.0"
 
