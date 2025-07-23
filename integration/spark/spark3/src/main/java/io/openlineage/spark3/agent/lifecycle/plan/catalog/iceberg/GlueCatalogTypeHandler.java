7492dc5e70df974dc92be40fe475538ef76cbed1
Dominik Dębowczyk <dominik.debowczyk@getindata.com>
2025-07-23 14:19:07 +0200

    Use AwsGlue naming schema
    Signed-off-by: Dominik Dębowczyk <dominik.debowczyk@getindata.com>

--- integration/spark/spark3/src/main/java/io/openlineage/spark3/agent/lifecycle/plan/catalog/iceberg/GlueCatalogTypeHandler.java
+++ integration/spark/spark3/src/main/java/io/openlineage/spark3/agent/lifecycle/plan/catalog/iceberg/GlueCatalogTypeHandler.java
@@ -5,13 +5,14 @@
 
 package io.openlineage.spark3.agent.lifecycle.plan.catalog.iceberg;
 
-import static io.openlineage.spark.agent.util.PathUtils.GLUE_TABLE_PREFIX;
 import static io.openlineage.spark3.agent.lifecycle.plan.catalog.iceberg.IcebergHandler.CATALOG_IMPL;
 
+import io.openlineage.client.dataset.Naming;
 import io.openlineage.client.utils.DatasetIdentifier;
 import io.openlineage.spark.agent.util.AwsUtils;
 import java.util.Map;
 import java.util.Optional;
+import java.util.regex.Pattern;
 import org.apache.spark.SparkContext;
 import org.apache.spark.sql.SparkSession;
 
@@ -32,9 +33,17 @@ class GlueCatalogTypeHandler extends BaseCatalogTypeHandler {
   DatasetIdentifier getIdentifier(
       SparkSession session, Map<String, String> catalogConf, String table) {
     SparkContext sparkContext = session.sparkContext();
-    Optional<String> arn =
-        AwsUtils.getGlueArn(sparkContext.getConf(), sparkContext.hadoopConfiguration());
-    return arn.map(s -> new DatasetIdentifier(GLUE_TABLE_PREFIX + table.replace(".", "/"), s))
-        .orElse(null);
+    String[] splitTable = table.split(Pattern.quote("."), 2);
+    if (splitTable.length != 2) {
+      throw new IllegalArgumentException(
+          "Invalid table format. Expected 'database.table', got: " + table);
+    }
+    Optional<Naming.AWSGlue> arn =
+        AwsUtils.getGlueName(
+            sparkContext.getConf(),
+            sparkContext.hadoopConfiguration(),
+            splitTable[0],
+            splitTable[1]);
+    return arn.map(DatasetIdentifier::new).orElse(null);
   }
 }
