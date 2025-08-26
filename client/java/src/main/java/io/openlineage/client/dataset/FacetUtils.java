f1d1bbf8880b1d5d44e2e65f1e681401dde6a64f
Pawel Leszczynski <leszczynski.pawel@gmail.com>
2025-08-26 09:06:53 +0200

    Detect subset locations facet for S3 objects in Spark (aka normalize S3 locations)
    Signed-off-by: Pawel Leszczynski <leszczynski.pawel@gmail.com>

---
+++ client/java/src/main/java/io/openlineage/client/dataset/FacetUtils.java
@@ -0,0 +1,39 @@
+/*
+/* Copyright 2018-2025 contributors to the OpenLineage project
+/* SPDX-License-Identifier: Apache-2.0
+*/
+
+package io.openlineage.client.dataset;
+
+import io.openlineage.client.OpenLineage;
+import io.openlineage.client.OpenLineage.InputDatasetInputFacetsBuilder;
+import io.openlineage.client.OpenLineage.OutputDatasetOutputFacetsBuilder;
+
+public class FacetUtils {
+
+  public static InputDatasetInputFacetsBuilder toBuilder(
+      OpenLineage openLineage, OpenLineage.InputDatasetInputFacets facets) {
+    InputDatasetInputFacetsBuilder builder = openLineage.newInputDatasetInputFacetsBuilder();
+    builder
+        .dataQualityAssertions(facets.getDataQualityAssertions())
+        .dataQualityMetrics(facets.getDataQualityMetrics())
+        .inputStatistics(facets.getInputStatistics())
+        .iceberg_scan_report(facets.getIceberg_scan_report());
+
+    facets.getAdditionalProperties().forEach(builder::put);
+
+    return builder;
+  }
+
+  public static OutputDatasetOutputFacetsBuilder toBuilder(
+      OpenLineage openLineage, OpenLineage.OutputDatasetOutputFacets facets) {
+    OutputDatasetOutputFacetsBuilder builder = openLineage.newOutputDatasetOutputFacetsBuilder();
+    builder
+        .outputStatistics(facets.getOutputStatistics())
+        .iceberg_scan_report(facets.getIceberg_scan_report());
+
+    facets.getAdditionalProperties().forEach(builder::put);
+
+    return builder;
+  }
+}
