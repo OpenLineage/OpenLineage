8b0ccaac5e130cdfd6ef5822660db510b75a38d2
kchledowski <github@chledowski.com>
2025-08-27 09:50:00 +0200

    Introduce SortNodeVisitor
    Signed-off-by: kchledowski <github@chledowski.com>

---
+++ integration/spark/spark3/src/test/java/io/openlineage/spark3/agent/lifecycle/plan/column/visitors/SortNodeVisitorTest.java
@@ -0,0 +1,79 @@
+/*
+/* Copyright 2018-2025 contributors to the OpenLineage project
+/* SPDX-License-Identifier: Apache-2.0
+*/
+
+package io.openlineage.spark3.agent.lifecycle.plan.column.visitors;
+
+import static io.openlineage.spark3.agent.lifecycle.plan.column.ColumnLevelFixtures.EXPR_ID_1;
+import static io.openlineage.spark3.agent.lifecycle.plan.column.ColumnLevelFixtures.NAME_1;
+import static io.openlineage.spark3.agent.lifecycle.plan.column.ColumnLevelFixtures.field;
+import static io.openlineage.spark3.agent.lifecycle.plan.column.ColumnLevelFixtures.mockNewExprId;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import io.openlineage.client.utils.TransformationInfo;
+import io.openlineage.spark.agent.lifecycle.plan.column.ColumnLevelLineageBuilder;
+import io.openlineage.spark.agent.util.ScalaConversionUtils;
+import java.util.Collections;
+import java.util.concurrent.atomic.LongAccumulator;
+import org.apache.spark.sql.catalyst.expressions.ExprId;
+import org.apache.spark.sql.catalyst.expressions.NamedExpression;
+import org.apache.spark.sql.catalyst.expressions.NullOrdering;
+import org.apache.spark.sql.catalyst.expressions.SortDirection;
+import org.apache.spark.sql.catalyst.expressions.SortOrder;
+import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan;
+import org.apache.spark.sql.catalyst.plans.logical.Sort;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+class SortNodeVisitorTest {
+
+  SortNodeVisitor visitor = new SortNodeVisitor();
+  ColumnLevelLineageBuilder builder = mock(ColumnLevelLineageBuilder.class);
+  LongAccumulator exprIdAccumulator = new LongAccumulator(Long::sum, 0L);
+
+  @BeforeEach
+  void setup() {
+    exprIdAccumulator.reset();
+  }
+
+  @Test
+  void testIsDefinedAt() {
+    assertTrue(visitor.isDefinedAt(mock(Sort.class)));
+    assertFalse(visitor.isDefinedAt(mock(LogicalPlan.class)));
+  }
+
+  @Test
+  void testApply() {
+    try (MockedStatic<NamedExpression> utilities = mockStatic(NamedExpression.class)) {
+      mockNewExprId(exprIdAccumulator, utilities);
+      ExprId datasetDependencyExpression = ExprId.apply(0);
+      Sort sort =
+          new Sort(
+              ScalaConversionUtils.fromList(
+                  Collections.singletonList(
+                      new SortOrder(
+                          field(NAME_1, EXPR_ID_1),
+                          mock(SortDirection.class),
+                          mock(NullOrdering.class),
+                          ScalaConversionUtils.asScalaSeqEmpty()))),
+              true,
+              mock(LogicalPlan.class));
+
+      visitor.apply(sort, builder);
+
+      verify(builder, times(1)).addDatasetDependency(datasetDependencyExpression);
+      verify(builder, times(1))
+          .addDependency(
+              datasetDependencyExpression,
+              EXPR_ID_1,
+              TransformationInfo.indirect(TransformationInfo.Subtypes.SORT));
+      utilities.verify(NamedExpression::newExprId, times(1));
+    }
+  }
+}
