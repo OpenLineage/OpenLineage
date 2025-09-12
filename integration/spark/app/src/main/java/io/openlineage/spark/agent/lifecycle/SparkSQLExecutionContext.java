760cd955e106384e204adc61d4dba2c91083a584
Eugen <evgenij.ushin@gmail.com>
2025-09-12 12:06:22 +0300

    spark: serialize event to json for logging (#4029)
    * serialize event to json for logging
    
    Signed-off-by: EugenYushin <evgenij.ushin@gmail.com>
    
    * run precommit checks
    
    Signed-off-by: EugenYushin <evgenij.ushin@gmail.com>
    
    * wrap debug log calls with isDebugEnabled checks
    
    Signed-off-by: EugenYushin <evgenij.ushin@gmail.com>
    
    ---------
    
    Signed-off-by: EugenYushin <evgenij.ushin@gmail.com>

--- integration/spark/app/src/main/java/io/openlineage/spark/agent/lifecycle/SparkSQLExecutionContext.java
+++ integration/spark/app/src/main/java/io/openlineage/spark/agent/lifecycle/SparkSQLExecutionContext.java
@@ -109,7 +109,11 @@ class SparkSQLExecutionContext implements ExecutionContext {
                 .jobFacetsBuilder(getJobFacetsBuilder(olContext.getQueryExecution().get()))
                 .build());
 
-    log.debug("Posting event for start {}: {}", executionId, event);
+    if (log.isDebugEnabled()) {
+      log.debug(
+          "Posting event for start {}: {}", executionId, OpenLineageClientUtils.toJson(event));
+    }
+
     eventEmitter.emit(event);
   }
 
@@ -193,7 +197,12 @@ class SparkSQLExecutionContext implements ExecutionContext {
                 .jobFacetsBuilder(getJobFacetsBuilder(olContext.getQueryExecution().get()))
                 .build());
 
-    log.debug("Posting event for stage submitted {}: {}", executionId, event);
+    if (log.isDebugEnabled()) {
+      log.debug(
+          "Posting event for stage submitted {}: {}",
+          executionId,
+          OpenLineageClientUtils.toJson(event));
+    }
     eventEmitter.emit(event);
   }
 
@@ -223,7 +232,12 @@ class SparkSQLExecutionContext implements ExecutionContext {
                 .jobFacetsBuilder(getJobFacetsBuilder(olContext.getQueryExecution().get()))
                 .build());
 
-    log.debug("Posting event for stage completed {}: {}", executionId, event);
+    if (log.isDebugEnabled()) {
+      log.debug(
+          "Posting event for stage completed {}: {}",
+          executionId,
+          OpenLineageClientUtils.toJson(event));
+    }
     eventEmitter.emit(event);
   }
 
@@ -278,7 +292,11 @@ class SparkSQLExecutionContext implements ExecutionContext {
                 .jobFacetsBuilder(getJobFacetsBuilder(olContext.getQueryExecution().get()))
                 .build());
 
-    log.debug("Posting event for start {}: {}", executionId, event);
+    if (log.isDebugEnabled()) {
+      log.debug(
+          "Posting event for start {}: {}", executionId, OpenLineageClientUtils.toJson(event));
+    }
+
     eventEmitter.emit(event);
   }
 
@@ -331,7 +349,11 @@ class SparkSQLExecutionContext implements ExecutionContext {
       // clean up metrics on complete only
       JobMetricsHolder.getInstance().cleanUp(jobEnd.jobId());
     }
-    log.debug("Posting event for end {}: {}", executionId, event);
+
+    if (log.isDebugEnabled()) {
+      log.debug("Posting event for end {}: {}", executionId, OpenLineageClientUtils.toJson(event));
+    }
+
     eventEmitter.emit(event);
   }
 
