1f7f17e4c392958770379e3510bd1a6232d67d53
Pawel Leszczynski <leszczynski.pawel@gmail.com>
2025-10-10 09:35:00 +0200

    fix spark connector on databricks setup
    Signed-off-by: Pawel Leszczynski <leszczynski.pawel@gmail.com>

---
+++ client/java/src/test/java/io/openlineage/client/transports/TransportResolverTest.java
@@ -0,0 +1,27 @@
+/*
+/* Copyright 2018-2025 contributors to the OpenLineage project
+/* SPDX-License-Identifier: Apache-2.0
+*/
+
+package io.openlineage.client.transports;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+import org.junit.jupiter.api.Test;
+
+class TransportResolverTest {
+
+  @Test
+  void testResolveTransportConfigByTypeForCommonType() {
+    assertThat(TransportResolver.resolveTransportConfigByType("http")).isSameAs(HttpConfig.class);
+  }
+
+  @Test
+  void testResolveTransportByConfigForCommonType() {
+    HttpConfig config = new HttpConfig();
+    config.setUrl(URI.create("https://localhost:1500/api/v1/lineage"));
+    assertThat(TransportResolver.resolveTransportByConfig(config))
+        .isInstanceOf(HttpTransport.class);
+  }
+}
