96072bd32109b8e12d740ff83e236f8ebea46eda
Maciej Obuchowski <maciej.obuchowski@datadoghq.com>
2025-10-24 17:18:49 +0200

    fix snowflake ident parsing
    Signed-off-by: Maciej Obuchowski <maciej.obuchowski@datadoghq.com>

---
+++ client/java/src/test/java/io/openlineage/client/utils/SnowflakeUtilsTest.java
@@ -0,0 +1,38 @@
+/*
+/* Copyright 2018-2025 contributors to the OpenLineage project
+/* SPDX-License-Identifier: Apache-2.0
+*/
+
+package io.openlineage.client.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class SnowflakeUtilsTest {
+
+  @ParameterizedTest
+  @CsvSource({
+    // Organization-account format (preferred) - never includes region/cloud in URL
+    "https://myorg-myaccount.snowflakecomputing.com, myorg-myaccount",
+    "http://myorg-myaccount.snowflakecomputing.com, myorg-myaccount",
+    "myorg-myaccount, myorg-myaccount",
+    "https://my-org-name-my-account.snowflakecomputing.com, my-org-name-my-account",
+    "https://org123-account456.snowflakecomputing.com, org123-account456",
+    // Legacy account locator format - return with region.cloud
+    "https://xy12345.snowflakecomputing.com, xy12345.us-west-1.aws",
+    "https://xy12345.us-west-2.snowflakecomputing.com, xy12345.us-west-2.aws",
+    "https://xy12345.us-west-2.aws.snowflakecomputing.com, xy12345.us-west-2.aws",
+    "https://xy12345.east-us-2.azure.snowflakecomputing.com, xy12345.east-us-2.azure",
+    "https://abc123.us-central1.gcp.snowflakecomputing.com, abc123.us-central1.gcp",
+    "accountlocator, accountlocator.us-west-1.aws",
+    "xy12345.us-east-1, xy12345.us-east-1.aws",
+    "xy12345.us-east-1.gcp, xy12345.us-east-1.gcp"
+  })
+  void testParseAccountIdentifier(String input, String expected) {
+    String result = SnowflakeUtils.parseAccountIdentifier(input);
+    assertEquals(
+        expected, result, String.format("Failed to parse account identifier from URL: %s", input));
+  }
+}
--- integration/common/tests/test_snowflake.py
+++ integration/common/tests/test_snowflake.py
@@ -8,30 +8,37 @@ from openlineage.common.provider.snowflake import fix_account_name, fix_snowflak
 @pytest.mark.parametrize(
     "source,target",
     [
+        # Account locator format with full region.cloud
         (
             "snowflake://user:pass@xy123456.us-east-1.aws/database/schema",
             "snowflake://xy123456.us-east-1.aws/database/schema",
         ),
+        # Account locator without region/cloud - defaults added
         (
             "snowflake://xy123456/database/schema",
             "snowflake://xy123456.us-west-1.aws/database/schema",
         ),
+        # Account locator with region only - cloud defaults to aws
         (
             "snowflake://xy12345.ap-southeast-1/database/schema",
             "snowflake://xy12345.ap-southeast-1.aws/database/schema",
         ),
+        # Account locator with Azure
         (
             "snowflake://user:pass@xy12345.south-central-us.azure/database/schema",
             "snowflake://xy12345.south-central-us.azure/database/schema",
         ),
+        # Account locator with GCP
         (
             "snowflake://user:pass@xy12345.us-east4.gcp/database/schema",
             "snowflake://xy12345.us-east4.gcp/database/schema",
         ),
+        # Organization-account format - returned as-is
         (
             "snowflake://user:pass@organization-account/database/schema",
             "snowflake://organization-account/database/schema",
         ),
+        # Organization-account with special chars in password
         (
             "snowflake://user:p[ass@organization-account/database/schema",
             "snowflake://organization-account/database/schema",
@@ -50,16 +57,17 @@ def test_snowflake_sqlite_account_urls(source, target):
 @pytest.mark.parametrize(
     "name, expected",
     [
-        ("xy12345", "xy12345.us-west-1.aws"),  # No '-' or '_' in name
+        # Account locator format (no hyphen) - add defaults
+        ("xy12345", "xy12345.us-west-1.aws"),  # No region/cloud
+        ("xy12345.us-west-1", "xy12345.us-west-1.aws"),  # Region only, cloud defaults to aws
         ("xy12345.us-west-1.aws", "xy12345.us-west-1.aws"),  # Already complete locator
         ("xy12345.us-west-2.gcp", "xy12345.us-west-2.gcp"),  # Already complete locator for GCP
-        ("xy12345aws", "xy12345aws.us-west-1.aws"),  # AWS without '-' or '_'
-        ("xy12345-aws", "xy12345-aws"),  # AWS with '-'
-        ("xy12345_gcp-europe-west1", "xy12345.europe-west1.gcp"),  # GCP with '_'
-        ("myaccount_gcp-asia-east1", "myaccount.asia-east1.gcp"),  # GCP with region and '_'
-        ("myaccount_azure-eastus", "myaccount.eastus.azure"),  # Azure with region
-        ("myorganization-1234", "myorganization-1234"),  # No change needed
-        ("my.organization", "my.organization.us-west-1.aws"),  # Dot in name
+        ("abc123.east-us-2.azure", "abc123.east-us-2.azure"),  # Azure complete
+        ("xy12345aws", "xy12345aws.us-west-1.aws"),  # Account locator without hyphen
+        # Organization-account format (contains hyphen) - return as-is
+        ("myorg-account", "myorg-account"),  # Organization-account format
+        ("my-organization-1234", "my-organization-1234"),  # Organization-account with multiple hyphens
+        ("org123-account456", "org123-account456"),  # Organization-account format
     ],
 )
 def test_fix_account_name(name, expected):
