21f61acb09563383b6c943f6cd0aa86946b2637f
Pawel Leszczynski <leszczynski.pawel@gmail.com>
2025-11-13 11:21:02 +0100

    fetch input statistics for single input RDD
    Signed-off-by: Pawel Leszczynski <leszczynski.pawel@gmail.com>

--- integration/spark/shared/src/test/java/io/openlineage/spark/agent/JobMetricsHolderTest.java
+++ integration/spark/shared/src/test/java/io/openlineage/spark/agent/JobMetricsHolderTest.java
@@ -29,16 +29,18 @@ class JobMetricsHolderTest {
     // on job start event
     underTest.addJobStages(0, new HashSet<>(Arrays.asList(1, 2, 3)));
     // on task end event
-    underTest.addMetrics(1, outputTaskMetrics(0, 0));
-    underTest.addMetrics(2, outputTaskMetrics(10, 1));
-    underTest.addMetrics(3, outputTaskMetrics(100, 1));
+    underTest.addMetrics(1, taskMetrics(0, 0, 0, 0));
+    underTest.addMetrics(2, taskMetrics(10, 1, 10, 1));
+    underTest.addMetrics(3, taskMetrics(100, 1, 100, 1));
 
     // on job end event
     Map<JobMetricsHolder.Metric, Number> result = underTest.pollMetrics(0);
 
     assertThat(result)
         .containsEntry(JobMetricsHolder.Metric.WRITE_RECORDS, 2L)
-        .containsEntry(JobMetricsHolder.Metric.WRITE_BYTES, 110L);
+        .containsEntry(JobMetricsHolder.Metric.WRITE_BYTES, 110L)
+        .containsEntry(Metric.READ_RECORDS, 2L)
+        .containsEntry(JobMetricsHolder.Metric.READ_BYTES, 110L);
 
     // second poll event should clear the maps
     underTest.cleanUp(0);
@@ -52,8 +54,8 @@ class JobMetricsHolderTest {
     underTest.addJobStages(0, new HashSet<>(Arrays.asList(1)));
     underTest.addJobStages(1, new HashSet<>(Arrays.asList(2)));
     // on task end event
-    underTest.addMetrics(1, outputTaskMetrics(100, 10));
-    underTest.addMetrics(2, outputTaskMetrics(10, 1));
+    underTest.addMetrics(1, taskMetrics(100, 10, 100, 10));
+    underTest.addMetrics(2, taskMetrics(10, 1, 10, 1));
 
     // on job end event
     Map<JobMetricsHolder.Metric, Number> job0 = underTest.pollMetrics(0);
@@ -61,16 +63,20 @@ class JobMetricsHolderTest {
 
     assertThat(job0)
         .containsEntry(JobMetricsHolder.Metric.WRITE_RECORDS, 10L)
-        .containsEntry(JobMetricsHolder.Metric.WRITE_BYTES, 100L);
+        .containsEntry(JobMetricsHolder.Metric.WRITE_BYTES, 100L)
+        .containsEntry(JobMetricsHolder.Metric.READ_RECORDS, 10L)
+        .containsEntry(JobMetricsHolder.Metric.READ_BYTES, 100L);
     assertThat(job1)
         .containsEntry(JobMetricsHolder.Metric.WRITE_RECORDS, 1L)
-        .containsEntry(JobMetricsHolder.Metric.WRITE_BYTES, 10L);
+        .containsEntry(JobMetricsHolder.Metric.WRITE_BYTES, 10L)
+        .containsEntry(JobMetricsHolder.Metric.READ_RECORDS, 1L)
+        .containsEntry(JobMetricsHolder.Metric.READ_BYTES, 10L);
   }
 
   @Test
   void testCleanUpClearsBothMaps() {
     underTest.addJobStages(0, Collections.singleton(1));
-    underTest.addMetrics(1, outputTaskMetrics(10, 1));
+    underTest.addMetrics(1, taskMetrics(10, 1, 10, 1));
 
     underTest.cleanUp(0);
 
@@ -89,7 +95,7 @@ class JobMetricsHolderTest {
     // on job start event
     underTest.addJobStages(0, new HashSet<>(Arrays.asList(1)));
     // on task end event
-    underTest.addMetrics(1, outputTaskMetrics(100, 10));
+    underTest.addMetrics(1, taskMetrics(100, 10, 100, 10));
 
     underTest.cleanUp(0);
 
@@ -117,7 +123,7 @@ class JobMetricsHolderTest {
   void testMetricsCanBePolledAfterCleanup() {
     // add some stage and metric
     underTest.addJobStages(0, new HashSet<>(Arrays.asList(1)));
-    underTest.addMetrics(1, outputTaskMetrics(100, 10));
+    underTest.addMetrics(1, taskMetrics(100, 10, 100, 10));
 
     underTest.cleanUp(0);
     Map<JobMetricsHolder.Metric, Number> jobMetrics = underTest.pollMetrics(0);
@@ -129,7 +135,7 @@ class JobMetricsHolderTest {
   void testCleanUpClearsMaps() {
     // add some stage and metric
     underTest.addJobStages(0, new HashSet<>(Arrays.asList(1)));
-    underTest.addMetrics(1, outputTaskMetrics(100, 10));
+    underTest.addMetrics(1, taskMetrics(100, 10, 100, 10));
 
     assertThat(underTest.pollMetrics(0).get(Metric.WRITE_RECORDS)).isEqualTo(10L);
     underTest.cleanUp(0);
@@ -139,7 +145,7 @@ class JobMetricsHolderTest {
   @Test
   void testEmptyMetrics() {
     underTest.addJobStages(0, new HashSet<>(Arrays.asList(1)));
-    underTest.addMetrics(1, outputTaskMetrics(0, 0));
+    underTest.addMetrics(1, taskMetrics(0, 0, 0, 0));
 
     assertThat(underTest.pollMetrics(0)).isEmpty();
   }
@@ -148,19 +154,45 @@ class JobMetricsHolderTest {
   void testMultipleTasksPerStage() {
     // add some stage and metric
     underTest.addJobStages(0, new HashSet<>(Arrays.asList(1)));
-    underTest.addMetrics(1, outputTaskMetrics(100, 10));
-    underTest.addMetrics(1, outputTaskMetrics(100, 10));
-    underTest.addMetrics(1, outputTaskMetrics(100, 10));
+    underTest.addMetrics(1, taskMetrics(100, 10, 100, 10));
+    underTest.addMetrics(1, taskMetrics(100, 10, 100, 10));
+    underTest.addMetrics(1, taskMetrics(100, 10, 100, 10));
 
     Map<Metric, Number> metrics = underTest.pollMetrics(0);
     assertThat(metrics.get(Metric.WRITE_RECORDS)).isEqualTo(30L);
     assertThat(metrics.get(Metric.WRITE_BYTES)).isEqualTo(300L);
+    assertThat(metrics.get(Metric.READ_RECORDS)).isEqualTo(30L);
+    assertThat(metrics.get(Metric.READ_BYTES)).isEqualTo(300L);
   }
 
-  private TaskMetrics outputTaskMetrics(int bytes, int records) {
+  @Test
+  void testContainsMetrics() {
+    underTest.addJobStages(0, new HashSet<>(Arrays.asList(1)));
+    underTest.addMetrics(1, taskMetrics(0, 0, 0, 0));
+
+    assertThat(underTest.containsReadMetrics(0)).isFalse();
+    assertThat(underTest.containsWriteMetrics(0)).isFalse();
+
+    underTest = new JobMetricsHolder();
+    underTest.addJobStages(0, new HashSet<>(Arrays.asList(1)));
+    underTest.addMetrics(1, taskMetrics(1, 0, 0, 0));
+    assertThat(underTest.containsReadMetrics(0)).isTrue();
+    assertThat(underTest.containsWriteMetrics(0)).isFalse();
+
+    underTest = new JobMetricsHolder();
+    underTest.addJobStages(0, new HashSet<>(Arrays.asList(1)));
+    underTest.addMetrics(1, taskMetrics(0, 0, 1, 0));
+    assertThat(underTest.containsReadMetrics(0)).isFalse();
+    assertThat(underTest.containsWriteMetrics(0)).isTrue();
+  }
+
+  private TaskMetrics taskMetrics(
+      int bytesRead, int recordsRead, int bytesWritten, int recordsWritten) {
     TaskMetrics taskMetrics = new TaskMetrics();
-    taskMetrics.outputMetrics()._bytesWritten().add(bytes);
-    taskMetrics.outputMetrics()._recordsWritten().add(records);
+    taskMetrics.outputMetrics()._bytesWritten().add(bytesWritten);
+    taskMetrics.outputMetrics()._recordsWritten().add(recordsWritten);
+    taskMetrics.inputMetrics()._bytesRead().add(bytesRead);
+    taskMetrics.inputMetrics()._recordsRead().add(recordsRead);
     return taskMetrics;
   }
 }
