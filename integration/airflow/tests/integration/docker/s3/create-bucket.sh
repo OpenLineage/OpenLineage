e09ab4fde388a5ee3a0cf20f984891cfe99ff873
Maciej Obuchowski <maciej.obuchowski@datadoghq.com>
2025-11-18 12:51:45 +0100

    remove airflow integration integration tests
    Signed-off-by: Maciej Obuchowski <maciej.obuchowski@datadoghq.com>

--- integration/airflow/tests/integration/docker/s3/create-bucket.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Copyright 2018-2025 contributors to the OpenLineage project
-# SPDX-License-Identifier: Apache-2.0
-
-awslocal s3 mb s3://testbucket
-echo "OpenLineage rocks!" > testfile
-awslocal s3 cp testfile s3://testbucket/
-rm testfile
