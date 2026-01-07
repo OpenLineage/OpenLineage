a42f4bbd936f7d46f83a34927bbbdc62186af6e9
Maciej Obuchowski <maciej.obuchowski@datadoghq.com>
2026-01-07 23:47:59 +0100

    manylinux comp
    Signed-off-by: Maciej Obuchowski <maciej.obuchowski@datadoghq.com>

--- integration/sql/iface-py/script/build.sh
+++ integration/sql/iface-py/script/build.sh
@@ -35,7 +35,7 @@ fi
 
 # Build release wheels
 cd iface-py
-maturin build --sdist --out target/wheels --release --strip --compatibility manylinux_2_24
+maturin build --sdist --out target/wheels --release --strip --compatibility manylinux_2_28
 
 ls -halt target/wheels
 
