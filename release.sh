5acfedb429aab1309f6b66612fcd2b4c34692626
tnazarew <tomasz.nazarewicz@getindata.com>
2026-03-06 11:43:10 +0100

    add go-style tag during release
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>

--- release.sh
+++ release.sh
@@ -300,6 +300,9 @@ git commit --no-verify -sam "Prepare for release ${RELEASE_VERSION}" --signoff
 # (5) Pull latest tags, then prepare release tag
 git fetch --all --tags
 git tag -a "${RELEASE_VERSION}" -m "openlineage ${RELEASE_VERSION}"
+# Required for go client so it can be imported with from github.com/OpenLineage/OpenLineage@v${RELEASE_VERSION}
+git tag -a "client/go/v${RELEASE_VERSION}" -m "openlineage go-style version client/go/v${RELEASE_VERSION}"
+
 
 # (6) Prepare next development version
 # Determine bump type based on release version and next version
@@ -348,7 +351,7 @@ fi
 
 # (9) Push commits and tag
 if [[ $COMMITS = "true" ]] && [[ ! ${PUSH} = "false" ]]; then
-  git push origin main && git push origin "${RELEASE_VERSION}"
+  git push origin main && git push origin "${RELEASE_VERSION}" && git push origin "client/go/v${RELEASE_VERSION}"
 else
   echo "...skipping push; to push manually, use 'git push origin main && git push origin \"${RELEASE_VERSION}\"'"
 fi
