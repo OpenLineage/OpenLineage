850c377ab92f4fa7804907dc1718ed93cc0c71b4
kchledowski <github@chledowski.com>
2026-04-13 16:16:58 +0200

    python: Add support for dataset normalization in Python Client
    Signed-off-by: kchledowski <github@chledowski.com>

---
+++ client/python/src/openlineage/client/dataset/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2018-2026 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+
+from openlineage.client.dataset.dataset import DatasetConfig, DatasetNormalizer
+
+__all__ = [
+    "DatasetConfig",
+    "DatasetNormalizer",
+]
