eb80e50d7420a5e05775d70f02054072605697a9
Artur Owczarek <owczarek.artur@gmail.com>
2026-06-10 22:07:52 +0200

    Replace Makefile and custom build scripts with Taskfile
    Signed-off-by: Artur Owczarek <owczarek.artur@gmail.com>

--- integration/spark/buildDependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright 2018-2026 contributors to the OpenLineage project
-# SPDX-License-Identifier: Apache-2.0
-
-set -x -e
-(cd ../../client/java && $SHELL -l -i -c './gradlew generateCode publishToMavenLocal -x test --rerun-tasks')
-(cd ../spark-extension-interfaces && ./gradlew clean publishToMavenLocal -x test)
-(cd ../sql/iface-java && ./script/compile.sh && ./script/build.sh)
