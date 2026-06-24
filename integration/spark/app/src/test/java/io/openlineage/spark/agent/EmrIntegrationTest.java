717c549e29d1c5a95cc43d364ef7dd387b1d6a18
Artur Owczarek <owczarek.artur@gmail.com>
2026-06-24 13:54:40 +0200

    spark/test: move DynamicParameter to a shared test-commons module (#4664)
    Extract the DynamicParameter test helper into a new test-commons module exposed
    as test fixtures, and rename the EMR property prefix from 'openlineage.test' to
    'openlineage.tests.emr'.
    
    BREAKING: external users setting EMR integration-test parameters via the
    'openlineage.test' system-property prefix must switch to 'openlineage.tests.emr'.
    
    Signed-off-by: Artur Owczarek <owczarek.artur@gmail.com>

--- integration/spark/app/src/test/java/io/openlineage/spark/agent/EmrIntegrationTest.java
+++ integration/spark/app/src/test/java/io/openlineage/spark/agent/EmrIntegrationTest.java
@@ -63,8 +63,8 @@ import org.junit.jupiter.api.Test;
  * <h2>Configuration</h2>
  *
  * <p>All infrastructure details and configuration parameters should be set using system properties.
- * For example: {@code -Dopenlineage.test.bucketName=my-bucket-name}. Most parameters have defaults.
- * For a full list of configurable parameters, see {@link EmrDynamicParameter}.
+ * For example: {@code -Dopenlineage.tests.emr.bucketName=my-bucket-name}. Most parameters have
+ * defaults. For a full list of configurable parameters, see {@link EmrDynamicParameter}.
  *
  * <h3>Note on JUnit and Gradle</h3>
  *
