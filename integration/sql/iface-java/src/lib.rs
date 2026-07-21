8ecf65dc43a7ba2c948f387ab0e5a0e9574f078e
Kacper Muda <mudakacper@gmail.com>
2026-07-21 14:18:13 +0200

    build(deps): bump sqlparser, jni, and pyo3 in integration/sql (#4742)
    Signed-off-by: Kacper Muda <mudakacper@gmail.com>

--- integration/sql/iface-java/src/lib.rs
+++ integration/sql/iface-java/src/lib.rs
@@ -6,26 +6,27 @@ extern crate openlineage_sql as rust_impl;
 
 use anyhow::Result;
 use jni::errors::Error;
-use jni::objects::{JClass, JList, JObject, JString, JValue};
+use jni::objects::{JClass, JList, JObject, JString, JValue, JValueOwned};
+use jni::signature::{MethodSignature, RuntimeMethodSignature};
+use jni::strings::JNIString;
 use jni::sys::{jobject, jstring};
-use jni::JNIEnv;
+use jni::{Env, EnvUnowned, Outcome};
 
 use rust_impl::{get_generic_dialect, parse_multiple_statements};
 
 trait AsJavaObject {
-    fn as_java_object<'a>(&self, env: &'a JNIEnv) -> Result<JObject<'a>> {
-        let classname = Self::java_class_name();
-        let java_class = env.find_class(classname)?;
-        let signature = Self::ctor_signature();
-        let args = self.ctor_arguments(env)?;
-
-        let obj = env.new_object(java_class, signature, &args)?;
+    fn as_java_object<'local>(&self, env: &mut Env<'local>) -> Result<JObject<'local>> {
+        let rt_sig = RuntimeMethodSignature::from_str(Self::ctor_signature())?;
+        let sig = MethodSignature::from(&rt_sig);
+        let args_owned = self.ctor_arguments(env)?;
+        let args: Vec<JValue<'_>> = args_owned.iter().map(|v| v.borrow()).collect();
+        let obj = env.new_object(JNIString::from(Self::java_class_name()), sig, &args)?;
         Ok(obj)
     }
 
     fn java_class_name() -> &'static str;
     fn ctor_signature() -> &'static str;
-    fn ctor_arguments<'a>(&self, env: &'a JNIEnv) -> Result<Box<[JValue<'a>]>>;
+    fn ctor_arguments<'local>(&self, env: &mut Env<'local>) -> Result<Vec<JValueOwned<'local>>>;
 }
 
 impl AsJavaObject for rust_impl::SqlMeta {
@@ -37,48 +38,44 @@ impl AsJavaObject for rust_impl::SqlMeta {
         "(Ljava/util/List;Ljava/util/List;Ljava/util/List;Ljava/util/List;)V"
     }
 
-    fn ctor_arguments<'a>(&self, env: &'a JNIEnv) -> Result<Box<[JValue<'a>]>> {
-        let array_list_class = env
-            .find_class("java/util/ArrayList")
-            .expect("Couldn't find the ArrayList class");
-        let ins = env
-            .new_object(array_list_class, "()V", &[])
-            .expect("Coudln't create a new ArrayList");
-        let outs = env
-            .new_object(array_list_class, "()V", &[])
-            .expect("Coudln't create a new ArrayList");
-        let columns = env
-            .new_object(array_list_class, "()V", &[])
-            .expect("Coudln't create a new ArrayList");
-        let errors = env
-            .new_object(array_list_class, "()V", &[])
-            .expect("Coudln't create a new ArrayList");
-        let ins = JList::from_env(env, ins).unwrap();
-        let outs = JList::from_env(env, outs).unwrap();
-        let columns = JList::from_env(env, columns).unwrap();
-        let errors = JList::from_env(env, errors).unwrap();
+    fn ctor_arguments<'local>(&self, env: &mut Env<'local>) -> Result<Vec<JValueOwned<'local>>> {
+        let empty_sig = RuntimeMethodSignature::from_str("()V")?;
+        let empty = MethodSignature::from(&empty_sig);
+        let array_list = JNIString::from("java/util/ArrayList");
+
+        let ins_raw = env.new_object(array_list.clone(), &empty, &[])?;
+        let outs_raw = env.new_object(array_list.clone(), &empty, &[])?;
+        let columns_raw = env.new_object(array_list.clone(), &empty, &[])?;
+        let errors_raw = env.new_object(array_list, &empty, &[])?;
+
+        let ins = env.cast_local::<JList>(ins_raw)?;
+        let outs = env.cast_local::<JList>(outs_raw)?;
+        let columns = env.cast_local::<JList>(columns_raw)?;
+        let errors = env.cast_local::<JList>(errors_raw)?;
 
         for e in &self.table_lineage.in_tables {
-            ins.add(e.as_java_object(env)?)?;
+            let obj = e.as_java_object(env)?;
+            ins.add(env, &obj)?;
         }
         for e in &self.table_lineage.out_tables {
-            outs.add(e.as_java_object(env)?)?;
+            let obj = e.as_java_object(env)?;
+            outs.add(env, &obj)?;
         }
-
         for e in &self.column_lineage {
-            columns.add(e.as_java_object(env)?)?;
+            let obj = e.as_java_object(env)?;
+            columns.add(env, &obj)?;
         }
-
         for e in &self.errors {
-            errors.add(e.as_java_object(env)?)?;
+            let obj = e.as_java_object(env)?;
+            errors.add(env, &obj)?;
         }
 
-        Ok(Box::new([
-            JValue::Object(ins.into()),
-            JValue::Object(outs.into()),
-            JValue::Object(columns.into()),
-            JValue::Object(errors.into()),
-        ]))
+        Ok(vec![
+            JValueOwned::Object(ins.into()),
+            JValueOwned::Object(outs.into()),
+            JValueOwned::Object(columns.into()),
+            JValueOwned::Object(errors.into()),
+        ])
     }
 }
 
@@ -91,25 +88,25 @@ impl AsJavaObject for rust_impl::QuoteStyle {
         "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V"
     }
 
-    fn ctor_arguments<'a>(&self, env: &'a JNIEnv) -> Result<Box<[JValue<'a>]>> {
-        let arg1 = match &self.database {
+    fn ctor_arguments<'local>(&self, env: &mut Env<'local>) -> Result<Vec<JValueOwned<'local>>> {
+        let arg1: JObject<'local> = match &self.database {
             Some(n) => env.new_string(n.to_string())?.into(),
             None => JObject::null(),
         };
-        let arg2 = match &self.schema {
+        let arg2: JObject<'local> = match &self.schema {
             Some(n) => env.new_string(n.to_string())?.into(),
             None => JObject::null(),
         };
-        let arg3 = match &self.name {
+        let arg3: JObject<'local> = match &self.name {
             Some(n) => env.new_string(n.to_string())?.into(),
             None => JObject::null(),
         };
 
-        Ok(Box::new([
-            JValue::Object(arg1),
-            JValue::Object(arg2),
-            JValue::Object(arg3),
-        ]))
+        Ok(vec![
+            JValueOwned::Object(arg1),
+            JValueOwned::Object(arg2),
+            JValueOwned::Object(arg3),
+        ])
     }
 }
 
@@ -122,28 +119,28 @@ impl AsJavaObject for rust_impl::DbTableMeta {
         "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Lio/openlineage/sql/QuoteStyle;)V"
     }
 
-    fn ctor_arguments<'a>(&self, env: &'a JNIEnv) -> Result<Box<[JValue<'a>]>> {
-        let arg1 = match &self.database {
+    fn ctor_arguments<'local>(&self, env: &mut Env<'local>) -> Result<Vec<JValueOwned<'local>>> {
+        let arg1: JObject<'local> = match &self.database {
             Some(d) => env.new_string(d)?.into(),
             None => JObject::null(),
         };
-        let arg2 = match &self.schema {
+        let arg2: JObject<'local> = match &self.schema {
             Some(s) => env.new_string(s)?.into(),
             None => JObject::null(),
         };
-        let arg3 = env.new_string(&self.name)?.into();
+        let arg3: JObject<'local> = env.new_string(&self.name)?.into();
 
-        let arg4 = match &self.quote_style {
-            Some(q) => q.as_java_object(env).unwrap(),
+        let arg4: JObject<'local> = match &self.quote_style {
+            Some(q) => q.as_java_object(env)?,
             None => JObject::null(),
         };
 
-        Ok(Box::new([
-            JValue::Object(arg1),
-            JValue::Object(arg2),
-            JValue::Object(arg3),
-            JValue::Object(arg4),
-        ]))
+        Ok(vec![
+            JValueOwned::Object(arg1),
+            JValueOwned::Object(arg2),
+            JValueOwned::Object(arg3),
+            JValueOwned::Object(arg4),
+        ])
     }
 }
 
@@ -156,14 +153,14 @@ impl AsJavaObject for rust_impl::ColumnMeta {
         "(Lio/openlineage/sql/DbTableMeta;Ljava/lang/String;)V"
     }
 
-    fn ctor_arguments<'a>(&self, env: &'a JNIEnv) -> Result<Box<[JValue<'a>]>> {
-        let arg1 = match &self.origin {
-            Some(d) => d.as_java_object(env).unwrap(),
+    fn ctor_arguments<'local>(&self, env: &mut Env<'local>) -> Result<Vec<JValueOwned<'local>>> {
+        let arg1: JObject<'local> = match &self.origin {
+            Some(d) => d.as_java_object(env)?,
             None => JObject::null(),
         };
-        let arg2 = env.new_string(&self.name)?.into();
+        let arg2: JObject<'local> = env.new_string(&self.name)?.into();
 
-        Ok(Box::new([JValue::Object(arg1), JValue::Object(arg2)]))
+        Ok(vec![JValueOwned::Object(arg1), JValueOwned::Object(arg2)])
     }
 }
 
@@ -176,26 +173,24 @@ impl AsJavaObject for rust_impl::ColumnLineage {
         "(Lio/openlineage/sql/ColumnMeta;Ljava/util/List;)V"
     }
 
-    fn ctor_arguments<'a>(&self, env: &'a JNIEnv) -> Result<Box<[JValue<'a>]>> {
-        let array_list_class = env
-            .find_class("java/util/ArrayList")
-            .expect("Couldn't find the ArrayList class");
+    fn ctor_arguments<'local>(&self, env: &mut Env<'local>) -> Result<Vec<JValueOwned<'local>>> {
+        let empty_sig = RuntimeMethodSignature::from_str("()V")?;
+        let empty = MethodSignature::from(&empty_sig);
 
-        let lineage = env
-            .new_object(array_list_class, "()V", &[])
-            .expect("Coudln't create a new ArrayList");
-        let lineage = JList::from_env(env, lineage).unwrap();
+        let lineage_raw = env.new_object(JNIString::from("java/util/ArrayList"), &empty, &[])?;
+        let lineage = env.cast_local::<JList>(lineage_raw)?;
 
         for e in &self.lineage {
-            lineage.add(e.as_java_object(env)?)?;
+            let obj = e.as_java_object(env)?;
+            lineage.add(env, &obj)?;
         }
 
         let descendant = self.descendant.as_java_object(env)?;
 
-        Ok(Box::new([
-            JValue::Object(descendant),
-            JValue::Object(lineage.into()),
-        ]))
+        Ok(vec![
+            JValueOwned::Object(descendant),
+            JValueOwned::Object(lineage.into()),
+        ])
     }
 }
 
@@ -208,67 +203,90 @@ impl AsJavaObject for rust_impl::ExtractionError {
         "(JLjava/lang/String;Ljava/lang/String;)V"
     }
 
-    fn ctor_arguments<'a>(&self, env: &'a JNIEnv) -> Result<Box<[JValue<'a>]>> {
-        let message = env.new_string(&self.message)?.into();
-        let origin_statement = env.new_string(&self.origin_statement)?.into();
+    fn ctor_arguments<'local>(&self, env: &mut Env<'local>) -> Result<Vec<JValueOwned<'local>>> {
+        let message: JObject<'local> = env.new_string(&self.message)?.into();
+        let origin_statement: JObject<'local> = env.new_string(&self.origin_statement)?.into();
 
-        Ok(Box::new([
-            JValue::Long(self.index as i64),
-            JValue::Object(message),
-            JValue::Object(origin_statement),
-        ]))
+        Ok(vec![
+            JValueOwned::Long(self.index as i64),
+            JValueOwned::Object(message),
+            JValueOwned::Object(origin_statement),
+        ])
     }
+}
+
+fn throw_anyhow(env: &mut Env<'_>, err: anyhow::Error) {
+    let _ = env.throw_new(
+        JNIString::from("java/lang/RuntimeException"),
+        JNIString::from(err.to_string()),
+    );
 }
 
 #[no_mangle]
 pub extern "system" fn Java_io_openlineage_sql_OpenLineageSql_parse(
-    env: JNIEnv,
+    mut env: EnvUnowned,
     _class: JClass,
     sql: JObject,
     dialect: JString,
     default_schema: JString,
 ) -> jobject {
-    let f = || -> Result<jobject> {
-        let sql = env.get_list(sql)?;
-        let mut vec_sql: Vec<String> = vec![];
-        let size = sql.size()?;
-        for i in 0..size {
-            let item = sql.get(i)?;
-            if let Some(i) = item {
-                let s: String = env.get_string(i.into())?.into();
+    let outcome = env.with_env(move |env| -> Result<jobject, Error> {
+        let result: anyhow::Result<jobject> = (|| {
+            let sql_list = env.cast_local::<JList>(sql)?;
+            let mut vec_sql: Vec<String> = vec![];
+            let size = sql_list.size(env)?;
+            for i in 0..size {
+                let item = sql_list.get(env, i)?;
+                if item.is_null() {
+                    continue;
+                }
+                let jstr = env.cast_local::<JString>(item)?;
+                let s = jstr.try_to_string(env)?;
                 vec_sql.push(s);
             }
+
+            let dialect_str: Option<String> = if dialect.as_raw().is_null() {
+                None
+            } else {
+                Some(dialect.try_to_string(env)?)
+            };
+            let dialect_obj = get_generic_dialect(dialect_str);
+
+            let default_schema_str: Option<String> = if default_schema.as_raw().is_null() {
+                None
+            } else {
+                Some(default_schema.try_to_string(env)?)
+            };
+
+            let parsed = parse_multiple_statements(vec_sql, dialect_obj, default_schema_str)?;
+            Ok(parsed.as_java_object(env)?.into_raw())
+        })();
+
+        match result {
+            Ok(obj) => Ok(obj),
+            Err(err) => {
+                throw_anyhow(env, err);
+                Ok(JObject::null().into_raw())
+            }
         }
+    });
 
-        let dialect: Option<String> = match env.get_string(dialect) {
-            Err(Error::NullPtr(_)) => None,
-            s => Some(s?.into()),
-        };
-        let dialect = get_generic_dialect(dialect);
-
-        let default_schema: Option<String> = match env.get_string(default_schema) {
-            Err(Error::NullPtr(_)) => None,
-            s => Some(s?.into()),
-        };
-
-        let parsed = parse_multiple_statements(vec_sql, dialect, default_schema)?;
-        Ok(parsed.as_java_object(&env)?.into_raw())
-    };
-
-    match f() {
-        Ok(obj) => obj,
-        Err(err) => {
-            let _ = env.throw(err.to_string());
-            JObject::null().into_raw()
-        }
+    match outcome.into_outcome() {
+        Outcome::Ok(obj) => obj,
+        Outcome::Err(_) | Outcome::Panic(_) => JObject::null().into_raw(),
     }
 }
 
 #[no_mangle]
 pub extern "system" fn Java_io_openlineage_sql_OpenLineageSql_provider(
-    env: JNIEnv,
+    mut env: EnvUnowned,
     _class: JClass,
 ) -> jstring {
-    let output = env.new_string("rust").unwrap();
-    output.into_raw()
+    match env
+        .with_env(|env| -> Result<jstring, Error> { Ok(env.new_string("rust")?.into_raw()) })
+        .into_outcome()
+    {
+        Outcome::Ok(s) => s,
+        Outcome::Err(_) | Outcome::Panic(_) => JObject::null().into_raw() as jstring,
+    }
 }
