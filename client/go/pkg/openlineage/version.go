328c836d3bb100254eddc2ea0c22cfbd2c3ccac4
Maciej Obuchowski <maciej.obuchowski@datadoghq.com>
2026-07-23 15:48:31 +0200

    Prepare next development version 1.53.0-SNAPSHOT
    Signed-off-by: Maciej Obuchowski <maciej.obuchowski@datadoghq.com>

--- client/go/pkg/openlineage/version.go
+++ client/go/pkg/openlineage/version.go
@@ -7,4 +7,4 @@ package openlineage
 
 // Version is the current version of the OpenLineage Go client.
 // This value is updated automatically during releases.
-const Version = "1.52.0"
+const Version = "1.53.0-SNAPSHOT"
