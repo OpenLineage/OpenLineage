62986ee025b52e56edbe052792b09ad048406d9d
merobi-hub <merobi@gmail.com>
2026-08-25 07:55:25 -0400

    Use payload datetimes and add docstrings
    Signed-off-by: merobi-hub <merobi@gmail.com>

--- integration/temporalio/temporal_harvester.py
+++ integration/temporalio/temporal_harvester.py
@@ -65,51 +65,43 @@ class TemporalOpenLineageAdapter:
 
 
 async def get_temporal_events(workflow_ids: list, t_client: Client) -> None:
-    """"""
+    """Get events from the Temporal API, process them, and pass them to the adapter."""
     adapter = TemporalOpenLineageAdapter()
 
     temporal_events = []
     for workflow_id in workflow_ids:
         try:
             description = await t_client.get_workflow_handle(workflow_id).describe()
-        except RPCError as e:
-            print("Description not found for workflow with ID %s", workflow_id)
+        except RPCError:
+            print(f"Description not found for workflow with id {workflow_id}")
             continue
-        start_event_name = description.raw_info.type.name
-        start_event_time = description.raw_info.start_time
-        base_dt = datetime.datetime.fromtimestamp(start_event_time.seconds, tz=timezone.utc)
-        start_final_dt = base_dt + datetime.timedelta(microseconds=start_event_time.nanos // 1000)
-        start_event_run_id = adapter.build_run_id(start_final_dt, start_event_name)
+        start_event_name = description.id
+        start_event_time = description.start_time
+        start_event_run_id = adapter.build_run_id(start_event_time, start_event_name)
 
         adapter.create_and_emit_task_event(
                                             start_event_run_id, 
                                             RunState.START, 
-                                            start_final_dt, 
+                                            start_event_time, 
                                             start_event_name
                                             )
 
         if description.raw_info.status == 2:
-            complete_event_name = description.raw_info.type.name
-            complete_event_time = description.raw_info.close_time
-            base_dt = datetime.datetime.fromtimestamp(complete_event_time.seconds, tz=timezone.utc)
-            complete_final_dt = base_dt + datetime.timedelta(microseconds=complete_event_time.nanos // 1000)
-
+            complete_event_name = description.id
+            complete_event_time = description.close_time
             adapter.create_and_emit_task_event(
                                                 start_event_run_id, 
                                                 RunState.COMPLETE, 
-                                                complete_final_dt, 
+                                                complete_event_time, 
                                                 complete_event_name
                                                 )
 
         elif description.raw_info.status == 3:
-            complete_event_name = description.raw_info.type.name
-            complete_event_time = description.raw_info.close_time
-            base_dt = datetime.datetime.fromtimestamp(complete_event_time.seconds, tz=timezone.utc)
-            complete_final_dt = base_dt + datetime.timedelta(microseconds=complete_event_time.nanos // 1000)
-
+            complete_event_name = description.id
+            complete_event_time = description.close_time
             adapter.create_and_emit_task_event(
                                                 start_event_run_id, 
                                                 RunState.FAIL, 
-                                                complete_final_dt, 
+                                                complete_event_time, 
                                                 complete_event_name
                                                 )
