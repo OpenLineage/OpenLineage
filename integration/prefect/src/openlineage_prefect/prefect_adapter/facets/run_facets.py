0e6e527d7ce8cec7209a526e2ac7ec5c379b5bcf
merobi-hub <merobi@gmail.com>
2026-09-21 07:33:37 -0400

    Remove unused _get_producer method from facet
    Signed-off-by: merobi-hub <merobi@gmail.com>

--- integration/prefect/src/openlineage_prefect/prefect_adapter/facets/run_facets.py
+++ integration/prefect/src/openlineage_prefect/prefect_adapter/facets/run_facets.py
@@ -22,9 +22,3 @@ class PrefectDeploymentRunFacet(BaseFacet):
     @staticmethod
     def _get_schema() -> str:
         return "https://raw.githubusercontent.com/OpenLineage/OpenLineage/main/integration/prefect/src/openlineage_prefect/prefect_adapter/facets/PrefectDeploymentRunFacet.json"
-
-    @staticmethod
-    def _get_producer() -> str:
-        return (
-            "https://github.com/OpenLineage/openlineage/integration/prefect"
-        )
